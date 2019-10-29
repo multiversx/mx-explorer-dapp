e505a2940686bf6e0388d2a7dccf3009899697fe
Tudor Morar <tudor.morar@elrond.com>
2019-10-29 15:30:37 +0200

    fixed highlights empty state

--- src/sharedComponents/Highlights/index.tsx
+++ src/sharedComponents/Highlights/index.tsx
@@ -37,7 +37,16 @@ const Hightlights: React.FC = () => {
 
   React.useEffect(() => {
     getStats(elasticUrl).then(data => {
-      if (ref.current !== null) setState(data);
+      const newState = {
+        blockNumber: parseInt(data.blockNumber).toLocaleString('en'),
+        nrOfNodes: parseInt(data.nrOfNodes).toLocaleString('en'),
+        nrOfShards: parseInt(data.nrOfShards).toLocaleString('en'),
+        roundNumber: parseInt(data.roundNumber).toLocaleString('en'),
+        liveTPS: parseInt(data.liveTPS).toLocaleString('en'),
+        peakTPS: parseInt(data.peakTPS).toLocaleString('en'),
+        totalProcessedTxCount: parseInt(data.totalProcessedTxCount).toLocaleString('en'),
+      };
+      if (ref.current !== null) setState(newState);
     });
   }, [elasticUrl]); // run the operation only once since the parameter does not change
 
@@ -51,54 +60,42 @@ const Hightlights: React.FC = () => {
                 <FontAwesomeIcon icon={faCube} />
               </span>
               <span className="highlight-label">CURRENT BLOCK</span>
-              <span className="highlight-value">
-                {parseInt(state.blockNumber).toLocaleString('en')}
-              </span>
+              <span className="highlight-value">{state.blockNumber}</span>
             </li>
             <li className="col-lg-2 col-6 mt-4 mb-4">
               <span className="highlight-icon">
                 <FontAwesomeIcon icon={faServer} />
               </span>
               <span className="highlight-label">NUMBER OF SHARDS</span>
-              <span className="highlight-value">
-                {parseInt(state.nrOfShards).toLocaleString('en')}
-              </span>
+              <span className="highlight-value">{state.nrOfShards}</span>
             </li>
             <li className="col-lg-2 col-6 mt-4 mb-4">
               <span className="highlight-icon">
                 <FontAwesomeIcon icon={faClock} />
               </span>
               <span className="highlight-label">CURRENT ROUND</span>
-              <span className="highlight-value">
-                {parseInt(state.roundNumber).toLocaleString('en')}
-              </span>
+              <span className="highlight-value">{state.roundNumber}</span>
             </li>
             <li className="col-lg-2 col-6 mt-4 mb-4">
               <span className="highlight-icon">
                 <FontAwesomeIcon icon={faExchangeAlt} />
               </span>
               <span className="highlight-label">TPS</span>
-              <span className="highlight-value">
-                {parseInt(state.liveTPS).toLocaleString('en')}
-              </span>
+              <span className="highlight-value">{state.liveTPS}</span>
             </li>
             <li className="col-lg-2 col-6 mt-4 mb-4">
               <span className="highlight-icon">
                 <FontAwesomeIcon icon={faChartBar} />
               </span>
               <span className="highlight-label">PEAK TPS</span>
-              <span className="highlight-value">
-                {parseInt(state.peakTPS).toLocaleString('en')}
-              </span>
+              <span className="highlight-value">{state.peakTPS}</span>
             </li>
             <li className="col-lg-2 col-6 mt-4 mb-4">
               <span className="highlight-icon">
                 <FontAwesomeIcon icon={faServer} />
               </span>
               <span className="highlight-label">TOTAL TX</span>
-              <span className="highlight-value">
-                {parseInt(state.totalProcessedTxCount).toLocaleString('en')}
-              </span>
+              <span className="highlight-value">{state.totalProcessedTxCount}</span>
             </li>
           </ul>
         </div>
