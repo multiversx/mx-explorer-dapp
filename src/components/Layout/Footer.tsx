d92ef0d4622a8231596fffdd94b7b0c5ff7429fc
Tudor Morar <tudor.morar@elrond.com>
2019-11-13 15:48:49 +0200

    no testnet in footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { useGlobalState } from '../../context';
 
 const Footer: React.FC = () => {
-  const {
-    activeTestnet: { name },
-  } = useGlobalState();
   return (
     <footer className="footer">
       <div className="container text-center text-muted">
         <small>
-          {name}
-          &nbsp; • &nbsp;
           <a target="_blank" rel="noopener noreferrer" href="https://elrond.com/">
             &copy; Elrond Network
           </a>
