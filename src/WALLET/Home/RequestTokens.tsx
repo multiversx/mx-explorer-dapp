459835bbef398c946ad298e4dd9da10e069753cc
Tudor Morar <tudor.morar@elrond.com>
2019-11-19 11:06:01 +0200

    request tokens working

---
+++ src/WALLET/Home/RequestTokens.tsx
@@ -0,0 +1,168 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+import ReCAPTCHA from 'react-google-recaptcha';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ReactComponent as ErdsSvg } from 'assets/img/erds.svg';
+import { useWalletState } from './../context';
+import { getTokens } from './helpers/asyncRequests';
+import { WalletHomeType } from './Home';
+
+interface WalletHeaderType {
+  populateDetails: Function;
+}
+
+const RequestTokens = (props: WalletHeaderType & WalletHomeType) => {
+  const { publicKey, faucet, timeout, nodeUrl, populateDetails } = props;
+  const { balance } = useWalletState();
+  let ref = React.useRef(null);
+  const [showModal, setShowModal] = React.useState(false);
+  const [currentBalance, setCurrentBalance] = React.useState(balance);
+  const [erdsClass, setErdsClass] = React.useState<'d-none' | 'show' | 'hide'>('d-none');
+  const [fundsRecieved, setFundsRecieved] = React.useState(false);
+  const [requestFailed, setRequestFailed] = React.useState(false);
+  const [requestDisabled, setRequestDisabled] = React.useState(true);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
+
+  const onRecaptchaChange = (value: any) => {
+    if (value) {
+      setRequestDisabled(false);
+    } else setRequestDisabled(true);
+  };
+
+  React.useEffect(() => {
+    setCurrentBalance(balance);
+  }, [balance]);
+
+  const onRequestClick = () => {
+    let intervalId: any = null;
+    function getNewBalance() {
+      populateDetails(props);
+      if (balance !== currentBalance) {
+        clearInterval(intervalId);
+      }
+    }
+
+    getTokens({ nodeUrl, timeout, publicKey }).then(success => {
+      if (success) {
+        intervalId = setInterval(getNewBalance, 2000);
+        setErdsClass('show');
+        setFundsRecieved(true);
+        setTimeout(() => {
+          setErdsClass('hide');
+          setTimeout(() => {
+            setErdsClass('d-none');
+            setShowModal(false);
+          }, 300);
+        }, 2000);
+      } else {
+        setRequestFailed(true);
+      }
+    });
+  };
+
+  const RequestSuccess = () => (
+    <div id="requestSuccess">
+      <div className="empty mt-4 mb-3">
+        <FontAwesomeIcon icon={faCheck} className="empty-icon text-success" />
+        <span className="h5 empty-heading text-success">Request Succeed</span>
+        <div className="empty-details empty-small">
+          <p className="mt-3 mb-3">
+            Please allow a few seconds for the transaction to be processed.
+          </p>
+        </div>
+        <button type="button" className="btn btn-outline-secondary" onClick={handleClose}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+
+  const RequestFailed = () => (
+    <div id="requestFailed">
+      <div className="empty mt-4 mb-3">
+        <FontAwesomeIcon icon={faTimes} className="empty-icon text-warning" />
+        <span className="h5 empty-heading text-warning">Request Failed</span>
+        <div className="empty-details empty-small">
+          <p className="mt-3 mb-3">The request has failed. Please try again later.</p>
+        </div>
+        <button type="button" className="btn btn-outline-secondary" onClick={handleClose}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+
+  return (
+    <>
+      {faucet && balance === '0' && (
+        <>
+          <button
+            type="button"
+            className="btn btn-outline-light btn-request"
+            onClick={handleShow}
+            id="requestInvoke"
+          >
+            Request Tokens
+          </button>
+          <Modal show={showModal} onHide={handleClose}>
+            <div className="modal-header">
+              <h5 className="modal-title" id="requestTokens">
+                ERD Faucet
+              </h5>
+              <button type="button" className="close" onClick={handleClose}>
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <Modal.Body>
+              {!requestFailed ? (
+                <>
+                  {!fundsRecieved ? (
+                    <div id="requestBox">
+                      <div className="empty mt-4 mb-3">
+                        <i className="fa fa-coins empty-icon" />
+                        <span className="h4 empty-heading">Request Tokens</span>
+                        <div className="empty-details">
+                          <p>Here you can request testnet ERD tokens.</p>
+                          <div className="d-flex justify-content-center">
+                            <ReCAPTCHA
+                              ref={ref}
+                              sitekey="***REMOVED***"
+                              onChange={onRecaptchaChange}
+                              theme="light"
+                            />
+                          </div>
+
+                          <button
+                            id="requestButton"
+                            type="submit"
+                            className="btn btn-primary mt-4"
+                            disabled={requestDisabled}
+                            onClick={onRequestClick}
+                          >
+                            Request Tokens
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  ) : (
+                    <RequestSuccess />
+                  )}
+                </>
+              ) : (
+                <RequestFailed />
+              )}
+            </Modal.Body>
+          </Modal>
+          <div id="erds" className={erdsClass}>
+            <ErdsSvg />
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default RequestTokens;
--- src/WALLET/Home/WalletHeader.tsx
+++ src/WALLET/Home/WalletHeader.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Modal } from 'react-bootstrap';
-import ReCAPTCHA from 'react-google-recaptcha';
-import { faCopy, faCoins, faWallet, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faCopy, faCoins, faWallet, faSignOutAlt, faSync } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Denominate } from 'sharedComponents';
-import { ReactComponent as ErdsSvg } from 'assets/img/erds.svg';
 import { useWalletState } from './../context';
-import { getTokens } from './helpers/asyncRequests';
 import { WalletHomeType } from './Home';
+import RequestTokens from './RequestTokens';
 
 interface WalletHeaderType {
   populateDetails: Function;
@@ -15,53 +12,18 @@ interface WalletHeaderType {
 }
 
 const WalletHeader = (props: WalletHeaderType & WalletHomeType) => {
-  const { publicKey, faucet, timeout, nodeUrl, logout, populateDetails } = props;
+  const { publicKey, logout, populateDetails, faucet } = props;
   const { balance } = useWalletState();
-  let ref = React.useRef(null);
   let publicKeyRef = React.useRef(null);
-  const [showModal, setShowModal] = React.useState(false);
-  const [currentBalance, setCurrentBalance] = React.useState(balance);
-  const [erdsClass, setErdsClass] = React.useState<'d-none' | 'show' | 'hide'>('d-none');
-  const [requestDisabled, setRequestDisabled] = React.useState(true);
-
-  const handleClose = () => setShowModal(false);
-  const handleShow = () => setShowModal(true);
-
-  const onRecaptchaChange = (value: any) => {
-    if (value) setRequestDisabled(false);
-  };
-
-  React.useEffect(() => {
-    setCurrentBalance(balance);
-  }, [balance]);
-
-  const onRequestClick = () => {
-    let intervalId: any = null;
-    function getNewBalance() {
-      populateDetails(props);
-      if (balance !== currentBalance) {
-        clearInterval(intervalId);
-      }
-    }
-
-    getTokens({ nodeUrl, timeout, publicKey }).then(success => {
-      if (success) {
-        intervalId = setInterval(getNewBalance, 2000);
-        setErdsClass('show');
-        setTimeout(() => {
-          setErdsClass('hide');
-          setTimeout(() => {
-            setErdsClass('d-none');
-            setShowModal(false);
-          }, 300);
-        }, 2000);
-      }
-    });
-  };
 
   const copyAddress = (e: React.SyntheticEvent) => {
     e.preventDefault();
     navigator.clipboard.writeText(publicKey);
+  };
+
+  const refreshBalance = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    populateDetails();
   };
 
   return (
@@ -98,19 +60,10 @@ const WalletHeader = (props: WalletHeaderType & WalletHomeType) => {
               <span className="highlight-value">
                 <Denominate value={balance} showAllDecimals />
               </span>
-              <a href="#" className="highlight-link" ng-click="refreshBalance()">
-                <i className="fa fa-sync" />
+              <a href="#" className="highlight-link" onClick={refreshBalance}>
+                <FontAwesomeIcon icon={faSync} />
               </a>
-              {faucet && balance === '0' && (
-                <button
-                  type="button"
-                  className="btn btn-outline-light btn-request"
-                  onClick={handleShow}
-                  id="requestInvoke"
-                >
-                  Request Tokens
-                </button>
-              )}
+              {faucet && balance === '0' && <RequestTokens {...props} />}
             </div>
             <div className="log_out">
               <a href="#" className="highlight-link" onClick={e => logout()}>
@@ -119,62 +72,6 @@ const WalletHeader = (props: WalletHeaderType & WalletHomeType) => {
             </div>
           </div>
         </div>
-      </div>
-      <Modal show={showModal} onHide={handleClose}>
-        <div className="modal-header">
-          <h5 className="modal-title" id="requestTokens">
-            ERD Faucet
-          </h5>
-          <button type="button" className="close" onClick={handleClose}>
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <Modal.Body>
-          <div id="requestBox">
-            <div className="empty mt-4 mb-3">
-              <i className="fa fa-coins empty-icon" />
-              <span className="h4 empty-heading">Request Tokens</span>
-              <div className="empty-details">
-                <p>Here you can request testnet ERD tokens.</p>
-                <div className="d-flex justify-content-center">
-                  <ReCAPTCHA
-                    ref={ref}
-                    sitekey="***REMOVED***"
-                    onChange={onRecaptchaChange}
-                    theme="light"
-                  />
-                </div>
-
-                <button
-                  id="requestButton"
-                  type="submit"
-                  className="btn btn-primary mt-4"
-                  disabled={requestDisabled}
-                  onClick={onRequestClick}
-                >
-                  Request Tokens
-                </button>
-              </div>
-            </div>
-          </div>
-          <div id="requestSuccess" className="d-none">
-            <div className="empty mt-4 mb-3">
-              <i className="fa fa-check empty-icon text-success" aria-hidden="true" />
-              <span className="h5 empty-heading text-success">Request Succeed</span>
-              <div className="empty-details empty-small">
-                <p className="mt-3 mb-3">
-                  Please allow a few seconds for the transaction to be processed.
-                </p>
-              </div>
-              <button type="button" className="btn btn-outline-secondary" data-dismiss="modal">
-                Close
-              </button>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
-      <div id="erds" className={erdsClass}>
-        <ErdsSvg />
       </div>
     </div>
   );
