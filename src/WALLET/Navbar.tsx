4d1ec5e449100dfafbf609f30cc53d6c5bd30c58
Tudor Morar <tudor.morar@elrond.com>
2019-11-22 21:55:55 +0200

    Unlock-pem test working

--- src/WALLET/Navbar.tsx
+++ src/WALLET/Navbar.tsx
@@ -1,10 +1,10 @@
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import elrondLogo from 'assets/img/elrond.svg';
 import React from 'react';
-import Navbar from '../components/Layout/Navbar/index';
+import { Navbar } from 'react-bootstrap';
+import { NavbarWrapper } from '../components/Layout/Navbar';
+import AppSwitcher from '../components/Layout/Navbar/AppSwitcher';
 import TestnetSwitcher from '../components/Layout/Navbar/TestnetSwitcher';
-import { TestnetLink } from '../sharedComponents';
 import { useWalletDispatch } from './context';
 
 export default function SiteNavbar() {
@@ -17,28 +17,19 @@ export default function SiteNavbar() {
   };
 
   return (
-    <Navbar collapseOnSelect expand="md" onToggle={onToggle} expanded={expanded}>
-      <div className="container">
-        <TestnetLink className="navbar-brand" to="/">
-          <div className="d-none d-md-block d-lg-block d-xl-block" style={{ marginTop: '-2px' }}>
-            <img src={elrondLogo} alt="Elrond logo" />
-          </div>
-          <div className="d-xs-block d-sm-block d-md-none d-lg-none d-xl-none">
-            <img src={elrondLogo} alt="Elrond logo" />
-          </div>
-        </TestnetLink>
-        <Navbar.Toggle aria-controls="navbars" style={{ color: 'black', border: 'none' }}>
-          {expanded ? <FontAwesomeIcon icon={faTimes} /> : <FontAwesomeIcon icon={faBars} />}
-        </Navbar.Toggle>
-        <Navbar.Collapse id="navbars">
-          <ul className="navbar-nav mr-auto">
-            <li className={`nav-item active`} onClick={() => onToggle(false)}>
-              wallet
-            </li>
-          </ul>
-          <TestnetSwitcher onToggle={onToggle} />
-        </Navbar.Collapse>
-      </div>
-    </Navbar>
+    <NavbarWrapper>
+      <AppSwitcher activeAppId="wallet" />
+      <Navbar.Toggle aria-controls="navbars" style={{ color: 'black', border: 'none' }}>
+        {expanded ? <FontAwesomeIcon icon={faTimes} /> : <FontAwesomeIcon icon={faBars} />}
+      </Navbar.Toggle>
+      <Navbar.Collapse id="navbars">
+        <ul className="navbar-nav mr-auto">
+          <li className={`nav-item active`} onClick={() => onToggle(false)}>
+            wallet
+          </li>
+        </ul>
+        <TestnetSwitcher onToggle={onToggle} />
+      </Navbar.Collapse>
+    </NavbarWrapper>
   );
 }
