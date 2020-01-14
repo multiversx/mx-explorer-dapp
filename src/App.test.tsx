b95ed40abadc047843ce5fd33f5188b42297d05c
Tudor Morar <tudor.morar@elrond.com>
2020-01-14 16:20:27 +0200

    Remove app.test.tsx

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import App from './App';
-
-it('shows validators in navbar', () => {
-  const { getByText } = render(<App />);
-  expect(getByText('validators')).toBeInTheDocument();
-});
