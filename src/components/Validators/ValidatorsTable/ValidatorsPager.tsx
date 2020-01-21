6a644f5c28423a0182cf18ba9d58422d663da1aa
Tudor Morar <tudor.morar@elrond.com>
2020-01-21 15:58:49 +0200

    Add ValidatorsPager component

---
+++ src/components/Validators/ValidatorsTable/ValidatorsPager.tsx
@@ -0,0 +1,75 @@
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+const ValidatorsPager = ({
+  total,
+  start,
+  end,
+  show,
+  page,
+  setPage,
+}: {
+  total: number | string;
+  start: number;
+  end: number;
+  show: boolean;
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+  const size = !isNaN(page as any) ? parseInt(page as any) : 1;
+  const prevPageNo = size === 2 ? 1 : size - 1;
+
+  const goToPrev = () => setPage(prevPageNo);
+  const goToNext = () => setPage(size + 1);
+
+  const startEnd = end === 1 ? 1 : `${start.toLocaleString('en')}-${end.toLocaleString('en')}`;
+
+  const PagerComponent = (
+    <div className="float-right mt-3">
+      <ul className="list-inline">
+        <li className="list-inline-item">
+          <span>
+            <span data-testid="pageInterval">{startEnd}</span>
+            &nbsp;of&nbsp;
+            <span data-testid="totalPages">{total.toLocaleString('en')}</span>
+          </span>
+        </li>
+        <li className="list-inline-item ml-2 mr-2">
+          {size === 1 ? (
+            <div className="pager">
+              <span data-testid="disabledPreviousPageButton">
+                <FontAwesomeIcon icon={faChevronLeft} /> Prev
+              </span>
+            </div>
+          ) : (
+            <div className="pager">
+              <p data-testid="previousPageButton" onClick={goToPrev}>
+                <FontAwesomeIcon icon={faChevronLeft} /> Prev
+              </p>
+            </div>
+          )}
+        </li>
+        <li className="ml-2 list-inline-item">
+          {end < total ? (
+            <div className="pager">
+              <p className="link" data-testid="nextPageButton" onClick={goToNext}>
+                Next <FontAwesomeIcon icon={faChevronRight} />
+              </p>
+            </div>
+          ) : (
+            <div className="pager">
+              <span data-testid="disabledNextPageButton">
+                Next <FontAwesomeIcon icon={faChevronRight} />
+              </span>
+            </div>
+          )}
+        </li>
+      </ul>
+    </div>
+  );
+
+  return show ? PagerComponent : null;
+};
+
+export default ValidatorsPager;
--- src/components/Validators/ValidatorsTable/index.tsx
+++ src/components/Validators/ValidatorsTable/index.tsx
@@ -6,6 +6,7 @@ import { ValidatorType } from './../index';
 import ValidatorStats from './Stats';
 import ValidatorTableHead from './Thead';
 import ValidatorTableRow from './Trow';
+import ValidatorsPager from './ValidatorsPager';
 
 export interface ComputedShard {
   shardID: string;
@@ -33,11 +34,19 @@ const ValidatorsTable = (
 ) => {
   const [includeObservers, setIncludeObsevers] = React.useState(false);
   const [sort, setSort] = React.useState<SortType>({ field: '', dir: 'none' });
+  const [page, setPage] = React.useState<number>(1);
   const [searchValue, setSearchValue] = React.useState<string>('');
   const [shardValue, setShardValue] = React.useState<string>('');
   const [statusValue, setStatusValue] = React.useState<string>('');
   const [validatorObserverValue, setValidatorObserverValue] = React.useState<string>('');
   const { validators, validatorsAndObservers, validatorDetails, shardData } = props;
+
+  const resetPager = (func: any) => (data: any) => {
+    if (page !== 1) {
+      setPage(1);
+    }
+    func(data);
+  };
 
   const mainFilter: { logic: string; filters: Array<{}> } = { logic: 'and', filters: [] };
 
@@ -75,11 +84,15 @@ const ValidatorsTable = (
 
   const data = includeObservers ? validatorsAndObservers : validators;
 
-  const dataSource = new kendo.data.DataSource({ data });
+  const pageSize = 50;
+
+  const dataSource = new kendo.data.DataSource({ data, pageSize });
 
   dataSource.filter(mainFilter);
 
   dataSource.sort([sort]);
+
+  dataSource.page(page);
 
   const newValidators: ValidatorType[] = dataSource.view();
 
@@ -91,26 +104,26 @@ const ValidatorsTable = (
             <ValidatorStats
               shownValidatorsLength={data.length}
               filteredValidatorsLength={newValidators.length}
-              setSearchValue={setSearchValue}
+              setSearchValue={resetPager(setSearchValue)}
               includeObservers={includeObservers}
-              setIncludeObsevers={setIncludeObsevers}
-              setShardValue={setShardValue}
-              setStatusValue={setStatusValue}
-              setValidatorObserverValue={setValidatorObserverValue}
+              setIncludeObsevers={resetPager(setIncludeObsevers)}
+              setShardValue={resetPager(setShardValue)}
+              setStatusValue={resetPager(setStatusValue)}
+              setValidatorObserverValue={resetPager(setValidatorObserverValue)}
             />
             <div className="table-responsive" style={{ minHeight: '290px' }}>
               <table className="table mt-4">
                 <ValidatorTableHead
                   includeObservers={includeObservers}
-                  sortBy={setSort}
+                  sortBy={resetPager(setSort)}
                   sort={sort}
                   shardData={shardData}
                   shardValue={shardValue}
-                  setShardValue={setShardValue}
+                  setShardValue={resetPager(setShardValue)}
                   statusValue={statusValue}
-                  setStatusValue={setStatusValue}
+                  setStatusValue={resetPager(setStatusValue)}
                   validatorObserverValue={validatorObserverValue}
-                  setValidatorObserverValue={setValidatorObserverValue}
+                  setValidatorObserverValue={resetPager(setValidatorObserverValue)}
                   validatorStatistics={props.validatorStatistics}
                 />
                 <tbody>
@@ -124,6 +137,19 @@ const ValidatorsTable = (
                   ))}
                 </tbody>
               </table>
+              <ValidatorsPager
+                setPage={setPage}
+                total={dataSource.total()}
+                page={page}
+                start={(page - 1) * pageSize + (page === 1 ? 1 : 0)}
+                end={
+                  (page - 1) * pageSize +
+                  (parseInt(newValidators.length.toString()) < pageSize
+                    ? parseInt(newValidators.length.toString())
+                    : pageSize)
+                }
+                show={newValidators.length > 0}
+              />
             </div>
           </div>
         </div>
