851d364fa175ebeab12860b0c34d79d109a331cc
Tudor Morar <tudor.morar@elrond.com>
2020-05-05 13:56:11 +0300

    Remove unused code

--- src/components/Validators/Tabs.tsx
+++ src/components/Validators/Tabs.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useLocation, useRouteMatch } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { validatorsRouteNames } from 'routes';
 import { TestnetLink } from 'sharedComponents';
 
