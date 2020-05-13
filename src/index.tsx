39b6e20a901ca42ebe4a5b358ae5f14e7de3f9f5
Tudor Morar <tudor.morar@elrond.com>
2020-05-13 11:10:53 +0300

    Remove old WALLET

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Wallet from 'WALLET';
 import App from './App';
 import './assets/sass/theme.scss';
 import * as serviceWorker from './serviceWorker';
@@ -11,11 +10,7 @@ if (process.env.NODE_ENV === 'development') {
   whyDidYouRender(React);
 }
 
-if (process.env.REACT_APP_WALLET) {
-  ReactDOM.render(<Wallet />, document.getElementById('root'));
-} else {
-  ReactDOM.render(<App />, document.getElementById('root'));
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
