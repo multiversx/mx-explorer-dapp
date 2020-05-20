68766a7cff5a70ab0868ba6f2dbaa9cd25871788
Tudor Morar <tudor.morar@elrond.com>
2020-05-20 14:10:47 +0300

    Remove Search from Hero and add it to Navbar on all pages

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -1,34 +1,18 @@
 import * as React from 'react';
-import { Highlights, Search } from './../../../sharedComponents';
 import Leaflet from './Leaflet';
-// import Map from './Map';
 
-const HeroHighlights: React.FC = () => {
-  return (
-    <>
-      <div className="bg-black">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-12 pl-0 pr-0">
-              <div className="canvas">
-                <Leaflet />
-              </div>
-            </div>
-            {/* <div className="canvas">{ChartJS}</div> */}
+const HeroHighlights = () => (
+  <div className="bg-black">
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 pl-0 pr-0">
+          <div className="canvas">
+            <Leaflet />
           </div>
-          {/* <div className="row">
-            <div className="col-12 mb-3">
-              <div className="form-search" role="search">
-                <div className="input-group input-group-seamless">
-                  <Search />
-                </div>
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
-    </>
-  );
-};
+    </div>
+  </div>
+);
 
 export default HeroHighlights;
