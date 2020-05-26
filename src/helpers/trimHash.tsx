d598aa739424b662b91b60c0aa94569358d7b852
Tudor Morar <tudor.morar@elrond.com>
2020-05-26 15:13:36 +0300

    Trim hash 10 characters

--- src/helpers/trimHash.tsx
+++ src/helpers/trimHash.tsx
@@ -1,5 +1,5 @@
 const trimHash = (hash: string) => {
-  const keep = 12;
+  const keep = 10;
   const start = hash.substring(0, keep);
   const end = hash.substring(hash.length - keep);
   return `${start}...${end}`;
