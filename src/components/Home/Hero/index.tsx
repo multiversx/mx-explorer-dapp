0ed9b93a465b1f429732c0a25d3c63dcb7d4a458
Tudor Morar <tudor.morar@elrond.com>
2020-05-27 13:43:36 +0300

    Sho map only on default testnet

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
+import { useGlobalState } from 'context';
 import Leaflet from './Leaflet';
 
-const HeroHighlights = () => (
-  <div className="bg-black">
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-12 pl-0 pr-0">
-          <div className="canvas">
-            <Leaflet />
+const HeroHighlights = () =>
+  useGlobalState().activeTestnet.default ? (
+    <div className="bg-black">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 pl-0 pr-0">
+            <div className="canvas">
+              <Leaflet />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  ) : null;
 
 export default HeroHighlights;
