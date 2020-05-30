23aba3df89e4638aaa0fb203e380c5f01dbe3808
Tudor Morar <tudor.morar@elrond.com>
2020-05-30 18:24:07 +0300

    Improve validatordetals page show logic

--- src/components/Validators/index.tsx
+++ src/components/Validators/index.tsx
@@ -100,9 +100,12 @@ const Validators = () => {
 
   React.useEffect(getData, [nodeUrl, timeout]);
 
-  let validator = validatorData.validatorsAndObservers.find(v => v.publicKey === hash);
-  if (hash && validator === undefined && validatorData.validatorsAndObservers.length > 0) {
-    validator = { peerType: 'observer' } as any;
+  let validator: ValidatorType | undefined;
+  if (hash) {
+    validator = validatorData.validatorsAndObservers.find(v => v.publicKey === hash);
+    if (validator === undefined && validatorData.validatorsAndObservers.length > 0) {
+      validator = { peerType: 'observer' } as any;
+    }
   }
 
   return useMemo(
