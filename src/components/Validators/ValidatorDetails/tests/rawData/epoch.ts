04532bae320d886a1b9f1e2303dd8c4d2aaee3c0
Tudor Morar <tudor.morar@elrond.com>
2020-06-03 10:16:26 +0300

    Fix tests

--- src/components/Validators/ValidatorDetails/tests/rawData/epoch.ts
+++ src/components/Validators/ValidatorDetails/tests/rawData/epoch.ts
@@ -1,11 +1,16 @@
 export default {
   message: {
-    epochData: {
+    status: {
       erd_current_round: 21801,
       erd_epoch_number: 36,
       erd_round_at_epoch_start: 21638,
       erd_rounds_passed_in_current_epoch: 163,
       erd_rounds_per_epoch: 600,
+
+      // not used
+      erd_nonce: 0,
+      erd_nonce_at_epoch_start: 0,
+      erd_nonces_passed_in_current_epoch: 0,
     },
   },
 };
