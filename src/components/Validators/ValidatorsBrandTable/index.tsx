769a67418b12df5111e7e8580c86ef0ea09932c5
Tudor Morar <tudor.morar@elrond.com>
2020-06-09 14:53:41 +0300

    Extract validator layout to separate component

--- src/components/Validators/ValidatorsBrandTable/index.tsx
+++ src/components/Validators/ValidatorsBrandTable/index.tsx
@@ -1,59 +1,14 @@
-import { faCogs } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useGlobalState } from 'context';
 import React from 'react';
-import { populateValidatorsTable } from './../helpers/validatorHelpers';
-import { Loader } from 'sharedComponents';
-import ShardsList from './../ShardsList';
-import BrandTable, { BrandDataType } from './BrandTable';
-import useSetValidatorsData from './../useSetValidatorsData';
-
-interface ValidatorsBrandTableType {
-  success: boolean;
-  validatorData: ReturnType<typeof populateValidatorsTable>;
-  showNodes: boolean;
-  validatorDetails: boolean | undefined;
-  brandData: BrandDataType[];
-}
+import BrandTable from './BrandTable';
+import ValidatorsLayout from './../ValidatorsLayout';
 
 const ValidatorsBrandTable = () => {
-  const {
-    activeTestnet: { validatorDetails },
-    validatorData,
-    brandData,
-  } = useGlobalState();
-  const success = useSetValidatorsData();
+  const { validatorData, brandData } = useGlobalState();
   return (
-    <div>
-      <div className="container pt-3 pb-3">
-        <div className="row">
-          <div className="col-12">
-            <h4 data-testid="title">{'Node Information'}</h4>
-          </div>
-        </div>
-        {success ? (
-          <>
-            {validatorData.validatorsAndObservers.length > 0 && brandData.length > 0 ? (
-              <>
-                <ShardsList shardData={validatorData.shardData} />
-                <BrandTable allValidators={validatorData.validators} brandData={brandData} />
-              </>
-            ) : (
-              <Loader />
-            )}
-          </>
-        ) : (
-          <div className="card">
-            <div className="card-body card-details" data-testid="errorScreen">
-              <div className="empty">
-                <FontAwesomeIcon icon={faCogs} className="empty-icon" />
-                <span className="h4 empty-heading">Unable to load validators</span>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
+    <ValidatorsLayout title="Node Information">
+      <BrandTable allValidators={validatorData.validators} brandData={brandData} />
+    </ValidatorsLayout>
   );
 };
 
--- src/components/Validators/ValidatorsTable/index.tsx
+++ src/components/Validators/ValidatorsTable/index.tsx
@@ -1,58 +1,18 @@
-import { faCogs } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useGlobalState } from 'context';
 import React from 'react';
-import { populateValidatorsTable } from './../helpers/validatorHelpers';
-import { Loader } from 'sharedComponents';
-import ShardsList from './../ShardsList';
 import Table from './Table';
-import useSetValidatorsData from './../useSetValidatorsData';
-
-interface ValidatorsTableType {
-  success: boolean;
-  validatorData: ReturnType<typeof populateValidatorsTable>;
-  validatorDetails: boolean | undefined;
-}
+import ValidatorsLayout from './../ValidatorsLayout';
 
 const ValidatorsTable = () => {
   const {
     activeTestnet: { validatorDetails },
     validatorData,
   } = useGlobalState();
-  const success = useSetValidatorsData();
 
   return (
-    <div>
-      <div className="container pt-3 pb-3">
-        <div className="row">
-          <div className="col-12">
-            <h4 data-testid="title">{'Validators'}</h4>
-          </div>
-        </div>
-        {success ? (
-          <>
-            {validatorData.validatorsAndObservers.length > 0 ? (
-              <>
-                <ShardsList shardData={validatorData.shardData} />
-                <Table {...validatorData} validatorDetails={validatorDetails || false} />
-                )}
-              </>
-            ) : (
-              <Loader />
-            )}
-          </>
-        ) : (
-          <div className="card">
-            <div className="card-body card-details" data-testid="errorScreen">
-              <div className="empty">
-                <FontAwesomeIcon icon={faCogs} className="empty-icon" />
-                <span className="h4 empty-heading">Unable to load validators</span>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
+    <ValidatorsLayout title="Validators">
+      <Table {...validatorData} validatorDetails={validatorDetails || false} />
+    </ValidatorsLayout>
   );
 };
 
