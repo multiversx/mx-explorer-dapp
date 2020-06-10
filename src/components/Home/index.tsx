f73f76122d95b6c965f850e3dedb6926e9d9fc57
Tudor Morar <tudor.morar@elrond.com>
2020-06-10 14:04:02 +0300

    Fetch validators in Home for performance

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
+import { useGlobalState } from 'context';
 import Hero from './Hero';
 import LatestBlocks from './LatestBlocks';
 import LatestTransactions from './LatestTransactions';
+import useSetValidatorsData from 'components/Validators/useSetValidatorsData';
 
-const TransactionDetails = () => {
+const FetchValidatorsComponent = () => {
+  const success = useSetValidatorsData();
+  return success ? <></> : null;
+};
+
+const Home = () => {
+  const [fetchValidators, setFetchValidators] = React.useState(true);
+  const { brandData } = useGlobalState();
+
+  React.useEffect(() => {
+    setFetchValidators(false);
+  }, []);
+
   return (
     <div>
+      {fetchValidators && brandData.length === 0 && <FetchValidatorsComponent />}
       <Hero />
       <div className="container pt-3 pb-3">
         <div className="row">
@@ -21,4 +36,4 @@ const TransactionDetails = () => {
   );
 };
 
-export default TransactionDetails;
+export default Home;
