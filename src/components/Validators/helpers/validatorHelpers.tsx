772162b4f49ff673b3fc89d696c3e4efb24bc741
Tudor Morar <tudor.morar@elrond.com>
2020-06-10 12:36:02 +0300

    Move computeShardStatus to separate file

--- src/components/Validators/helpers/validatorHelpers.tsx
+++ src/components/Validators/helpers/validatorHelpers.tsx
@@ -2,6 +2,7 @@ import { validatorFunctions } from 'helpers';
 import { ValidatorType } from 'context/validators';
 import { validatorIssues } from './../RowIcon';
 import getPeerType from './getPeerType';
+import computeShardStatus from './computeShardStatus';
 
 interface ShardDataType {
   [key: string]: {
@@ -256,19 +257,6 @@ export function populateValidatorsTable({
     validators,
     validatorsAndObservers,
   };
-}
-
-function computeShardStatus(allActiveValidators: number, allValidators: number) {
-  const danger = Math.ceil(allValidators * (2 / 3)) + 1;
-  const warning = Math.ceil(allValidators - (allValidators - danger) / 2);
-  switch (true) {
-    case allActiveValidators >= warning:
-      return 'success';
-    case danger <= allActiveValidators && allActiveValidators < warning:
-      return 'warning';
-    default:
-      return 'danger';
-  }
 }
 
 export type DirectioinsType = 'none' | 'desc' | 'asc';
