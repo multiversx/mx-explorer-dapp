18d23ed26159235c03abcf63b13f32c96c327c35
Tudor Morar <tudor.morar@elrond.com>
2020-07-22 18:04:21 +0300

    Move Validators Brand tests inside Validator tests to resolve warning

--- src/components/Validators/tests/Validators.test.tsx
+++ src/components/Validators/tests/Validators.test.tsx
@@ -144,3 +144,35 @@ describe('Validators links', () => {
     });
   });
 });
+
+const goToBrandDetailsPage = () => {
+  mockGet();
+  return renderWithRouter({
+    route: '/validators/elrondcom',
+    optionalConfig,
+  });
+};
+
+describe('Validators brand', () => {
+  test('Validators brand page is displaying', async () => {
+    const render = goToBrandDetailsPage();
+    expect(document.title).toEqual('Validator Details • Elrond Explorer');
+    const title = await waitForElement(() => render.getByTestId('title'));
+    expect(title.innerHTML).toBe('Validator Details');
+    const name = render.getByText('Elrond Foundational Nodes');
+    expect(name).toBeDefined();
+  });
+  test('Validators brand page loading', async () => {
+    const render = goToBrandDetailsPage();
+    const loader = await waitForElement(() => render.getByTestId('loader'));
+    expect(loader).toBeDefined();
+  });
+  test('Validators brand page failed', async () => {
+    const render = renderWithRouter({
+      route: '/validators/elrondcom1',
+      optionalConfig,
+    });
+    const name = await render.findByText('/validators/elrondcom1');
+    expect(name).toBeDefined();
+  });
+});
