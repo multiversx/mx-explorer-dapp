e9e1b9d0e16efb7c4592beeda3ffd6e10eb04976
Tudor Morar <tudor.morar@elrond.com>
2020-07-30 18:48:02 +0300

    Change map provider to Carto

--- src/components/Home/Hero/Leaflet/Map.tsx
+++ src/components/Home/Hero/Leaflet/Map.tsx
@@ -37,9 +37,12 @@ export default function MapDisplay({
 
   const radiusByCity = getRadius(groupedCities);
 
-  const tiles = 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png';
+  const tiles = 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png';
   const attribution =
-    '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
+  // const tiles = 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png';
+  // const attribution =
+  //   '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 
   const style = (publicKey: string, offset = 0) => ({
     dangerouslySetInnerHTML: {
