4497e375355844f5dc47aabff0bd3dc8e7c44d58
Tudor Morar <tudor.morar@elrond.com>
2020-08-20 12:09:48 +0300

    Remove validator search tests

--- src/sharedComponents/Search/tests/Search.test.tsx
+++ src/sharedComponents/Search/tests/Search.test.tsx
@@ -5,7 +5,6 @@ import {
   wait,
   config as optionalConfig,
   meta,
-  act,
 } from 'utils/test-utils';
 import {
   transactions as transactionsResponse,
@@ -29,13 +28,13 @@ const beforeAll = (fail = ['']) => {
         return Promise.resolve({ data: meta });
       case url.includes(`/node/heartbeatstatus`):
         return Promise.resolve({ data: { data: heartbeatstatus, code: 'successful' } });
-      case url.includes('/validator/statistics'):
-        return Promise.resolve({ data: { data: statistics, code: 'successful' } });
-      case url.endsWith('/validators'):
-        return Promise.resolve({ data: validators });
+      // case url.includes('/validator/statistics'):
+      //   return Promise.resolve({ data: { data: statistics, code: 'successful' } });
+      // case url.endsWith('/validators'):
+      //   return Promise.resolve({ data: validators });
       // --- page load ---
-      case url.includes('validators'):
-        return Promise.resolve({ data: validatorsdoc });
+      // case url.includes('validators'):
+      //   return Promise.resolve({ data: validatorsdoc });
       case url.includes('network/status'):
         return Promise.resolve({ data: { data: epoch, code: 'successful' } });
       case url.includes('/ratingshistory/'):
@@ -59,28 +58,28 @@ const beforeAll = (fail = ['']) => {
 };
 
 describe('Search', () => {
-  test('Seach finds validator', async () => {
-    const render = beforeAll();
+  // test('Seach finds validator', async () => {
+  //   const render = beforeAll();
 
-    const search = render.getByTestId('search');
-    const data = {
-      target: {
-        value:
-          '000523677f6c7f594de2452960c67411cb3503925d65124002edb2aefe125593c9bf588687655a04e4592ab4ade9911257581a71c44fa223752b2cfb7b8d43f1f9ef675d9513f3aa3ca6003118343e05269974848582e5141932be2c31aab582',
-      },
-    };
-    fireEvent.change(search, data);
+  //   const search = render.getByTestId('search');
+  //   const data = {
+  //     target: {
+  //       value:
+  //         '000523677f6c7f594de2452960c67411cb3503925d65124002edb2aefe125593c9bf588687655a04e4592ab4ade9911257581a71c44fa223752b2cfb7b8d43f1f9ef675d9513f3aa3ca6003118343e05269974848582e5141932be2c31aab582',
+  //     },
+  //   };
+  //   fireEvent.change(search, data);
 
-    const searchButton = render.getByTestId('searchButton');
-    await wait(async () => {
-      expect(searchButton).toBeEnabled();
-    });
-    fireEvent.click(searchButton);
+  //   const searchButton = render.getByTestId('searchButton');
+  //   await wait(async () => {
+  //     expect(searchButton).toBeEnabled();
+  //   });
+  //   fireEvent.click(searchButton);
 
-    await wait(async () => {
-      expect(document.title).toEqual('Node Details • Elrond Explorer');
-    });
-  });
+  //   await wait(async () => {
+  //     expect(document.title).toEqual('Node Details • Elrond Explorer');
+  //   });
+  // });
   test('Search finds block', async () => {
     const render = beforeAll();
 
