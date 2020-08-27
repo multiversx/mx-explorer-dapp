b8f4e9625d00070c6c16096dd0bb4633ef162ee9
Tudor Morar <tudor.morar@elrond.com>
2020-08-27 15:18:58 +0300

    Change testing config

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -21,7 +21,8 @@ const config: ConfigType = {
       numInitCharactersForScAddress: 20,
       nodeUrl: 'https://api.elrond.com',
       refreshRate: 6000,
-      elasticUrl: 'https://api-facade.elrond.com',
+      // TODO: check axios not working inf wrong address ex: api-facade
+      elasticUrl: 'https://api.elrond.com',
       adapter: 'api',
       decimals: 4,
       denomination: 18,
