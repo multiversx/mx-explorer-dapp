a4c557f598026adc94c823c438b864a211757c78
Tudor Morar <tudor.morar@elrond.com>
2020-08-27 13:17:06 +0300

    Adapter methods

--- src/sharedComponents/Adapter/index.ts
+++ src/sharedComponents/Adapter/index.ts
@@ -1,22 +1,75 @@
 import { useGlobalState } from 'context';
 import elastic from './elastic';
+import api from './api';
+
+const providers = {
+  api,
+  elastic,
+};
 
 export default function useAdapter() {
   const {
-    activeTestnet: { elasticUrl },
+    activeTestnet: { elasticUrl, adapter },
     timeout,
   } = useGlobalState();
 
-  // const useApi = elasticUrl.includes('.elrond.com');
+  const provider = providers[adapter];
 
   return {
-    getBlocks: () =>
-      elastic({
-        path: `/blocks`,
-        queryStringParameters: {
+    getLatestBlocks: async () => {
+      try {
+        const params = {
           size: 25,
-        },
-        elasticUrl,
-      }).then((data) => console.log(1111, data)),
+        };
+
+        const { data } = await provider({
+          elasticUrl,
+          url: `/blocks`,
+          params,
+          timeout,
+        });
+
+        const blocks = data.map((block: any) => ({ hash: block.id, ...block }));
+
+        return {
+          data: blocks,
+          blocksFetched: data.length > 0,
+        };
+      } catch {
+        return {
+          data: [],
+          blocksFetched: false,
+        };
+      }
+    },
+    getLatestTransactions: async () => {
+      try {
+        const params = {
+          size: 20,
+        };
+
+        const { data } = await provider({
+          elasticUrl,
+          url: `/transactions`,
+          params,
+          timeout,
+        });
+
+        const transactions = data.map((transaction: any) => ({
+          hash: transaction.id,
+          ...transaction,
+        }));
+
+        return {
+          data: transactions,
+          transactionsFetched: data.length > 0,
+        };
+      } catch (e) {
+        return {
+          data: [],
+          transactionsFetched: false,
+        };
+      }
+    },
   };
 }
