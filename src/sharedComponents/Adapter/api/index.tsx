1935714c57b3d42935d5e4e9c3c4edd127c71343
Tudor Morar <tudor.morar@elrond.com>
2020-08-27 17:18:53 +0300

    Refactor to axios.get for testing

--- src/sharedComponents/Adapter/api/index.tsx
+++ src/sharedComponents/Adapter/api/index.tsx
@@ -2,13 +2,7 @@ import axios from 'axios';
 import { ProviderType } from './../functions';
 
 const api: ProviderType = ({ elasticUrl, url, params, timeout }) => {
-  return axios({
-    method: 'GET',
-    baseURL: elasticUrl,
-    url,
-    params,
-    timeout,
-  });
+  return axios.get(`${elasticUrl}${url}`, { params, timeout });
 };
 
 export default api;
