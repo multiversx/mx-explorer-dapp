2dc3295d3aee963c776004b0975e39db455dfa82
Tudor Morar <tudor.morar@elrond.com>
2020-08-28 12:04:04 +0300

    Move block details to adapter

--- src/sharedComponents/Adapter/index.ts
+++ src/sharedComponents/Adapter/index.ts
@@ -5,6 +5,7 @@ import {
   getHighlights,
   getLatestBlocks,
   getLatestTransactions,
+  getBlock,
   getBlocks,
   GetBlocksType,
   getBlocksCount,
@@ -13,6 +14,14 @@ import {
   getMiniBlock,
   getMiniBlockTransactions,
   getMiniBlockTransactionsCount,
+  TransactionsType,
+  getTransactions,
+  getTransactionsCount,
+  getAddressDetails,
+  getRewards,
+  isBlock,
+  isAddress,
+  isTransaction,
 } from './functions';
 
 // TODO: daca pun ruta de elastic default
@@ -38,6 +47,8 @@ export default function useAdapter() {
   const { provider, proxyUrl } = providers[adapter];
 
   return {
+    /* Homepage */
+
     getHighlights: () =>
       getHighlights({ provider, proxyUrl, elasticUrl, timeout, metaChainShardId }),
 
@@ -45,17 +56,26 @@ export default function useAdapter() {
 
     getLatestTransactions: () => getLatestTransactions({ provider, elasticUrl, timeout }),
 
+    /* Blocks */
+
+    getBlock: ({ blockId }: { blockId: string }) =>
+      getBlock({ provider, elasticUrl, blockId, timeout }),
+
     getBlocks: ({ size, shardId, epochId }: GetBlocksType) =>
       getBlocks({ provider, elasticUrl, size, shardId, epochId, timeout }),
 
     getBlocksCount: ({ size, shardId, epochId }: GetBlocksType) =>
       getBlocksCount({ provider, elasticUrl, size, shardId, epochId, timeout }),
 
+    /* Transaction */
+
     getTransaction: ({ transactionId }: { transactionId: string }) =>
       getTransaction({ provider, elasticUrl, transactionId, timeout }),
 
     getPendingTransaction: ({ transactionId }: { transactionId: string }) =>
       getPendingTransaction({ proxyUrl, transactionId, timeout }),
+
+    /* Miniblocks */
 
     getMiniBlock: ({ miniBlockHash }: { miniBlockHash: string }) =>
       getMiniBlock({ provider, elasticUrl, miniBlockHash, timeout }),
@@ -65,5 +85,28 @@ export default function useAdapter() {
 
     getMiniBlockTransactionsCount: ({ miniBlockHash }: { miniBlockHash: string }) =>
       getMiniBlockTransactionsCount({ provider, miniBlockHash, timeout, elasticUrl }),
+
+    /* Transactions */
+
+    getTransactions: ({ size, addressId, shardId, shardType }: TransactionsType) =>
+      getTransactions({ provider, elasticUrl, timeout, addressId, size, shardId, shardType }),
+
+    getTransactionsCount: ({ size, addressId, shardId, shardType }: TransactionsType) =>
+      getTransactionsCount({ provider, elasticUrl, timeout, addressId, size, shardId, shardType }),
+
+    getAddressDetails: ({ addressId }: { addressId: string }) =>
+      getAddressDetails({ proxyUrl, timeout, addressId }),
+
+    getRewards: ({ addressId }: { addressId: string }) =>
+      getRewards({ proxyUrl, timeout, addressId }),
+
+    /* Search */
+
+    isBlock: ({ hash }: { hash: string }) => isBlock({ provider, elasticUrl, hash, timeout }),
+
+    isAddress: ({ hash }: { hash: string }) => isAddress({ proxyUrl, hash, timeout }),
+
+    isTransaction: ({ hash }: { hash: string }) =>
+      isTransaction({ provider, elasticUrl, proxyUrl, hash, timeout }),
   };
 }
