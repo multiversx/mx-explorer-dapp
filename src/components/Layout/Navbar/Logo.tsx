a2307af649f04b956072eefe511b2ec149534a4b
Dan Popa <popa.ioan.dan@gmail.com>
2020-10-09 11:59:02 +0300

    Updates Logo component

--- src/components/Layout/Navbar/Logo.tsx
+++ src/components/Layout/Navbar/Logo.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import { useGlobalState } from 'context';
 import { ReactComponent as ElrondLogo } from 'assets/img/logo.svg';
 import { ReactComponent as ElrondSymbol } from 'assets/img/symbol.svg';
 
 const Logo = () => {
+  const { activeNetworkId } = useGlobalState();
+  const title = activeNetworkId === 'mainnet' ? 'Explorer' : 'Testnet Explorer';
+
   return (
     <Link to="/" className="d-flex align-items-center navbar-brand mr-0 ml-3">
-      <ElrondSymbol className="main-symbol d-sm-none" />
-      <ElrondLogo className="main-logo d-none d-sm-block" />
-      <span className="text-secondary text-truncate">Explorer</span>
+      <ElrondLogo
+        className={`main-logo flex-shrink-0 ${
+          activeNetworkId === 'mainnet' ? 'd-md-none d-xl-block' : 'd-none'
+        }`}
+      />
+      <ElrondSymbol
+        className={`main-symbol flex-shrink-0 ${
+          activeNetworkId === 'mainnet' ? 'd-none d-md-block d-xl-none' : ''
+        }`}
+      />
+      <span className="text-secondary text-truncate">{title}</span>
     </Link>
   );
 };
