c20ec7bcae1d992d62837382411f46157ed00533
Dan Popa <popa.ioan.dan@gmail.com>
2020-10-22 15:49:43 +0300

    Centralises transaction statuses

--- src/sharedComponents/TransactionStatus/index.tsx
+++ src/sharedComponents/TransactionStatus/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { faCheckCircle, faBan, faHourglass, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import txStatus from '../../components/TransactionDetails/txStatus';
 
 interface TransactionStatusType {
   status: string;
@@ -11,19 +12,21 @@ const capitalizeFirstLetter = (str: string) => {
 };
 
 const TransactionStatus = ({ status }: TransactionStatusType) => {
+  const statusIs = (compateTo: string) => status.toLowerCase() === compateTo.toLowerCase();
+
   let Icon = () => <></>;
-  switch (status.toLowerCase()) {
-    case 'not executed':
+  switch (true) {
+    case statusIs(txStatus.notExecuted):
       Icon = () => <FontAwesomeIcon icon={faBan} className="mr-2 text-danger" />;
       break;
-    case 'fail':
-    case 'failed':
+    case statusIs(txStatus.fail):
+    case statusIs(txStatus.failed):
       Icon = () => <FontAwesomeIcon icon={faTimes} className="mr-2 text-danger" />;
       break;
-    case 'success':
+    case statusIs(txStatus.success):
       Icon = () => <FontAwesomeIcon icon={faCheckCircle} className="mr-2 text-success" />;
       break;
-    case 'invalid':
+    case statusIs(txStatus.invalid):
       Icon = () => <FontAwesomeIcon icon={faBan} className="mr-2 text-danger" />;
       break;
     default:
