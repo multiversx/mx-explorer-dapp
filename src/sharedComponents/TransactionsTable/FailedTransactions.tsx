f93628d7d41872a4ec697294669711d331817fe9
Tudor Morar <tudor.morar@elrond.com>
2020-10-28 16:27:42 +0200

    Update faield and no transactions components

--- src/sharedComponents/TransactionsTable/FailedTransactions.tsx
+++ src/sharedComponents/TransactionsTable/FailedTransactions.tsx
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { faExchangeAlt } from '@fortawesome/pro-regular-svg-icons/faExchangeAlt';
 import { PageState } from 'sharedComponents';
 
-export default function NoTransactions() {
+export default function FailedTransactions() {
   return (
     <div className="card">
       <div className="card-body">
         <PageState
           icon={faExchangeAlt}
-          title="No transactions"
+          title={'Unable to load transactions'}
           className="py-spacer d-flex h-100 align-items-center justify-content-center"
+          data-testid="errorScreen"
         />
       </div>
     </div>
