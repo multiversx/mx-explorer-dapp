e3bbeebcb71d6a8306288bb629e2611540559d9e
Tudor Morar <tudor.morar@elrond.com>
2020-10-28 16:56:02 +0200

    Add senderShard receiverShard to urlBuilder

--- src/helpers/urlBuilder.tsx
+++ src/helpers/urlBuilder.tsx
@@ -1,5 +1,7 @@
 const urlBuilder = {
   shard: (shard: number | string) => `/blocks?shard=${shard}`,
+  receiverShard: (shard: number | string) => `/transactions?receivershard=${shard}`,
+  senderShard: (shard: number | string) => `/transactions?sendershard=${shard}`,
 };
 
 export default urlBuilder;
