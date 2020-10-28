0005e07e466937d7ff5fc170006be5b21cc110de
Dan Popa <popa.ioan.dan@gmail.com>
2020-10-28 19:32:07 +0200

    Styles EmptySearch and PageNotFound components

--- src/components/PageNotFoud/index.tsx
+++ src/components/PageNotFoud/index.tsx
@@ -1,21 +1,23 @@
 import * as React from 'react';
 import { useLocation } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/pro-regular-svg-icons/faTimes';
+import { PageState } from 'sharedComponents';
 
 const PageNotFound = () => {
   const { pathname } = useLocation();
   return (
-    <div className="container pt-3 pb-3">
+    <div className="container py-spacer">
       <div className="row">
         <div className="col-12">
-          <div className="card">
-            <div className="card-body card-details">
-              <div className="empty">
-                <FontAwesomeIcon icon={faTimes} className="empty-icon" />
-                <span className="h4 empty-heading">Page not found</span>
-                <span className="empty-details">{pathname}</span>
-              </div>
+          <div className="card card-small">
+            <div className="card-body">
+              <PageState
+                icon={faTimes}
+                title="Page not found"
+                description={pathname}
+                className="py-spacer d-flex h-100 align-items-center justify-content-center"
+                dataTestId="errorScreen"
+              />
             </div>
           </div>
         </div>
