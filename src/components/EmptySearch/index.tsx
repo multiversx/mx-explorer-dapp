221127066d2a77d764e26bcdbd55d48c571ec696
Dan Popa <popa.ioan.dan@gmail.com>
2020-10-28 19:32:07 +0200

    Styles EmptySearch and PageNotFound components

--- src/components/EmptySearch/index.tsx
+++ src/components/EmptySearch/index.tsx
@@ -1,25 +1,25 @@
 import { faSearch } from '@fortawesome/pro-regular-svg-icons/faSearch';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 import { useParams } from 'react-router-dom';
+import { PageState } from 'sharedComponents';
 
 const EmptySearch = () => {
   const { query } = useParams() as any;
 
   return (
     <>
-      <div className="container pt-3 pb-3">
+      <div className="container py-spacer">
         <div className="row">
           <div className="col-12">
-            <div className="card">
-              <div className="card-body card-details">
-                <div className="empty">
-                  <FontAwesomeIcon icon={faSearch} className="empty-icon" />
-                  <span className="h4 empty-heading">
-                    Your search does not match anything we've got
-                  </span>
-                  <span className="empty-details">{query}</span>
-                </div>
+            <div className="card card-small">
+              <div className="card-body">
+                <PageState
+                  icon={faSearch}
+                  title="Your search does not match anything we've got"
+                  description={query}
+                  className="py-spacer d-flex h-100 align-items-center justify-content-center"
+                  dataTestId="errorScreen"
+                />
               </div>
             </div>
           </div>
