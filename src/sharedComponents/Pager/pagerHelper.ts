534e001258f9a2188e31bdb94ab5e203b627ea40
Tudor Morar <tudor.morar@elrond.com>
2020-10-28 13:10:20 +0200

    PagerHelper logic

--- src/sharedComponents/Pager/pagerHelper.ts
+++ src/sharedComponents/Pager/pagerHelper.ts
@@ -1,18 +1,21 @@
 import { isValidInteger } from 'helpers';
 
 interface PagerHelperType {
-  total: number;
+  total: number | '...';
   itemsPerPage: number;
   page: string;
 }
 
 export default function pagerHelper({ total, itemsPerPage, page }: PagerHelperType) {
+  const numericTotal = total === '...' ? 0 : total;
   const size = isValidInteger(page) ? parseInt(page) : 1;
   const start = (size - 1) * itemsPerPage + (size === 1 ? 1 : 0);
-  const end = (size - 1) * itemsPerPage + (total < itemsPerPage ? total : itemsPerPage);
+  const end =
+    (size - 1) * itemsPerPage +
+    (numericTotal < itemsPerPage && total !== '...' ? numericTotal : itemsPerPage);
   const last = !isNaN(parseInt(total.toString())) ? Math.min(end, parseInt(total.toString())) : end;
   const correction = size >= 2 ? 0 : 1;
-  const lastPage = Math.ceil(total / (end - start + correction));
+  const lastPage = Math.ceil(numericTotal / (end - start + correction));
 
   return {
     size,
