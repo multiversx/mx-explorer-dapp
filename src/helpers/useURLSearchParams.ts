ecb94f288108b23ba294584c23a563bce8856ed4
Tudor Morar <tudor.morar@elrond.com>
2020-10-28 16:15:31 +0200

    Url parameters accept lowercase shards

--- src/helpers/useURLSearchParams.ts
+++ src/helpers/useURLSearchParams.ts
@@ -7,8 +7,11 @@ export default function useURLSearchParams() {
   const query = new URLSearchParams(useLocation().search);
   const page = query.get('page') ? String(query.get('page')) : '';
   const shard = query.get('shard') ? String(query.get('shard')) : '';
-  const senderShard = query.get('senderShard') ? String(query.get('senderShard')) : '';
-  const receiverShard = query.get('receiverShard') ? String(query.get('receiverShard')) : '';
+
+  let senderShard = query.get('senderShard') ? String(query.get('senderShard')) : '';
+  senderShard = query.get('sendershard') ? String(query.get('sendershard')) : senderShard;
+  let receiverShard = query.get('receiverShard') ? String(query.get('receiverShard')) : '';
+  receiverShard = query.get('receivershard') ? String(query.get('receivershard')) : receiverShard;
 
   return {
     page: checkValue(page),
