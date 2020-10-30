a0b19c6bcab60f8ae2ac50753f3a53d084909aff
Dan Popa <popa.ioan.dan@gmail.com>
2020-10-30 20:05:13 +0200

    Adds style for groups of hashes on BlockDetails page

--- src/sharedComponents/DetailItem/index.tsx
+++ src/sharedComponents/DetailItem/index.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
 
-const DetailItem = ({ children, title }: { children: React.ReactNode; title: string }) => (
-  <div className="row py-3 border-bottom detail-item">
+const DetailItem = ({
+  children,
+  title,
+  className = '',
+}: {
+  children: React.ReactNode;
+  title: string;
+  className?: string;
+}) => (
+  <div className={`row py-3 border-bottom detail-item ${className}`}>
     <div className="col-lg-2 text-secondary text-lg-right">{title}</div>
     <div className="col">{children}</div>
   </div>
