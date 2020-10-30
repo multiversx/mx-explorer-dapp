a6f7ec4c3529cb314007bf4534420438b47248a9
Tudor Morar <tudor.morar@elrond.com>
2020-10-30 13:59:00 +0200

    Simplify transaction tests

--- src/components/TransactionDetails/tests/TransactionDetails.test.tsx
+++ src/components/TransactionDetails/tests/TransactionDetails.test.tsx
@@ -1,44 +1,11 @@
-import axios from 'axios';
-import { renderWithRouter, wait } from 'utils/test-utils';
-import optionalConfig from 'utils/config';
-import {
-  transactions as doc,
-  heartbeatstatus,
-  statistics,
-  validators,
-  validatorsdoc,
-  meta,
-} from 'utils/rawData';
+import { wait, beforeAll } from 'utils/test-utils';
+import { transactions as doc } from 'utils/rawData';
 
-const beforeAll = (success = true) => {
-  const mockGet = jest.spyOn(axios, 'get');
-  mockGet.mockImplementation((url: string): any => {
-    switch (true) {
-      case url.includes('/tps/meta'):
-        return Promise.resolve({ data: meta });
-      case url.includes(`/node/heartbeatstatus`):
-        return Promise.resolve({ data: { data: heartbeatstatus, code: 'successful' } });
-      case url.includes('/validator/statistics'):
-        return Promise.resolve({ data: { data: statistics, code: 'successful' } });
-      case url.endsWith('/validators'):
-        return Promise.resolve({ data: validators });
-      case url.includes('validators/1_36'):
-        return Promise.resolve({ data: validatorsdoc });
-      case url.includes(`/transactions/${doc.txHash}`) && success:
-        return Promise.resolve({ data: doc });
-      default:
-        return Promise.resolve(new Error('error'));
-    }
-  });
-  return renderWithRouter({
-    route: `/transactions/${doc.txHash}`,
-    optionalConfig,
-  });
-};
-
-describe('Transaction Details', () => {
+describe('Transaction Details Page', () => {
   test('Transaction Details page is displaying', async () => {
-    const render = beforeAll();
+    const render = beforeAll({
+      route: `/transactions/${doc.txHash}`,
+    });
     expect(document.title).toEqual('Transaction Details • Elrond Explorer');
     await wait(async () => {
       expect(render.getByText(doc.txHash)).toBeInTheDocument();
@@ -46,13 +13,20 @@ describe('Transaction Details', () => {
   });
 
   test('Transaction Details loading state', async () => {
-    const render = beforeAll();
+    const render = beforeAll({
+      route: `/transactions/${doc.txHash}`,
+    });
     const loader = await render.findByTestId('loader');
     expect(loader.innerHTML).toBeDefined();
   });
 
   test('Transaction Details failed state', async () => {
-    const render = beforeAll(false);
+    const render = beforeAll({
+      route: `/transactions/${doc.txHash}`,
+      networkRequests: {
+        transaction: () => Promise.resolve(new Error('error')),
+      },
+    });
     const failedScreen = await render.findByText('Unable to locate this transaction hash');
     expect(failedScreen.innerHTML).toBeDefined();
   });
