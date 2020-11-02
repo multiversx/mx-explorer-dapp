783a296583c80a650940d1448be357c49b024552
Constantin <constantin.tovisi@gmail.com>
2020-11-02 21:27:31 +0200

    outdatedVersion helper

--- src/helpers/outdatedVersion.tsx
+++ src/helpers/outdatedVersion.tsx
@@ -1,6 +1,7 @@
 const outdatedVersion = (validator: string, network: string): boolean => {
   const compare = validator.split('-')[0];
 
+  // TODO: remove this exception
   if (compare === 'v1.1.6.1') {
     return false;
   }
