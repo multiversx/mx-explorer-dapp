ca19610cf74677e54ed9ee1b6334a7a5387611cf
Dan Popa <popa.ioan.dan@gmail.com>
2020-11-02 10:29:32 +0200

    Fixes Logo component activeNetworkId bug

--- src/components/Layout/Navbar/Logo.tsx
+++ src/components/Layout/Navbar/Logo.tsx
@@ -5,19 +5,21 @@ import { ReactComponent as ElrondLogo } from 'assets/images/logo.svg';
 import { ReactComponent as ElrondSymbol } from 'assets/images/symbol.svg';
 
 const Logo = () => {
-  const { activeNetworkId } = useGlobalState();
-  const title = activeNetworkId === 'mainnet' ? 'Explorer' : 'Testnet Explorer';
+  const {
+    activeNetwork: { id },
+  } = useGlobalState();
+  const title = id === 'mainnet' ? 'Explorer' : 'Testnet Explorer';
 
   return (
     <Link to="/" className="d-flex align-items-center navbar-brand mr-0">
       <ElrondLogo
         className={`main-logo flex-shrink-0 ${
-          activeNetworkId === 'mainnet' ? 'd-md-none d-xl-block' : 'd-none'
+          id === 'mainnet' ? 'd-md-none d-xl-block' : 'd-none'
         }`}
       />
       <ElrondSymbol
         className={`main-symbol flex-shrink-0 ${
-          activeNetworkId === 'mainnet' ? 'd-none d-md-block d-xl-none' : ''
+          id === 'mainnet' ? 'd-none d-md-block d-xl-none' : ''
         }`}
       />
       <span className="text-secondary text-truncate">{title}</span>
