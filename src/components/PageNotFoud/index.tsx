518e6a477597599204540aea394df0577d22a479
Dan Popa <popa.ioan.dan@gmail.com>
2020-11-02 10:36:56 +0200

    Removes card from PageNotFound component

--- src/components/PageNotFoud/index.tsx
+++ src/components/PageNotFoud/index.tsx
@@ -6,23 +6,13 @@ import { PageState } from 'sharedComponents';
 const PageNotFound = () => {
   const { pathname } = useLocation();
   return (
-    <div className="container py-spacer">
-      <div className="row">
-        <div className="col-12">
-          <div className="card card-small">
-            <div className="card-body">
-              <PageState
-                icon={faTimes}
-                title="Page not found"
-                description={pathname}
-                className="py-spacer d-flex h-100 align-items-center justify-content-center"
-                dataTestId="errorScreen"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <PageState
+      icon={faTimes}
+      title="Page not found"
+      description={pathname}
+      className="py-spacer m-auto"
+      dataTestId="errorScreen"
+    />
   );
 };
 
