019acb4b55e213385878d51196a98190b297679d
Tudor Morar <tudor.morar@elrond.com>
2020-11-09 16:19:39 +0200

    Remove unused propreties from config

--- public/config.production.js
+++ public/config.production.js
@@ -1,34 +1,4 @@
 const CONFIG = {
-  metaChainShardId: 4294967295,
-  erdLabel: 'eGLD',
-  elrondApps: [
-    {
-      id: 'main-site',
-      name: 'Main site',
-      to: 'https://elrond.com/',
-    },
-    {
-      id: 'wallet',
-      name: 'Wallet',
-      to: 'https://wallet.elrond.com/',
-    },
-    {
-      id: 'explorer',
-      name: 'Explorer',
-      to: 'https://explorer.elrond.com/',
-    },
-    {
-      id: 'bridge',
-      name: 'Bridge',
-      to: 'https://bridge.elrond.com/',
-    },
-    {
-      id: 'docs',
-      name: 'Docs',
-      to: 'https://docs.elrond.com/',
-    },
-  ],
-  explorerApi: 'https://explorer-api.elrond.com',
   /*
       Possbile flags:
         validators: (default) true
