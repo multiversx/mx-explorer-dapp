0c7989f30988431481fea2d26fcc5dd5eb7c434a
Dan Popa <popa.ioan.dan@gmail.com>
2020-11-09 12:43:35 +0200

    Adds rounds card page states

--- src/components/NodeDetails/Rounds/index.tsx
+++ src/components/NodeDetails/Rounds/index.tsx
@@ -1,52 +1,80 @@
 import * as React from 'react';
 import { faCogs } from '@fortawesome/pro-regular-svg-icons/faCogs';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { PageState } from 'sharedComponents';
 
 export interface RoundType {
   key: string;
   value: boolean;
 }
 
-const Rounds = ({ rounds }: { rounds: RoundType[] }) => {
+interface RoundsType {
+  data: RoundType[];
+  success: boolean | undefined;
+  peerType: string;
+}
+
+const Rounds = ({
+  data,
+  success,
+  peerType,
+}: {
+  data: RoundType[];
+  success: boolean | undefined;
+  peerType: string;
+}) => {
   return (
     <>
       <div className="card" data-testid="roundsContainer">
-        {/* {roundsFetched === false && (
+        {success === false && (
           <PageState
             icon={faCogs}
-            title={isWaiting ? 'Validator not in consensus' : 'Unable to load rounds'}
+            title="Unable to load rounds"
             className="page-state-sm d-flex h-100 align-items-center justify-content-center"
             dataTestId="roundsErrorScreen"
           />
-        )} */}
+        )}
 
-        <>
-          <div className="card-header">
-            <div className="card-header-item">
-              <h6 className="m-0">Last Consensus Rounds</h6>
+        {success === true && data.length === 0 && (
+          <PageState
+            icon={faCogs}
+            title={`${
+              peerType === 'waiting' ? 'Validator not in consensus' : 'Unable to load rounds'
+            }`}
+            className="page-state-sm d-flex h-100 align-items-center justify-content-center"
+            dataTestId="roundsErrorScreen"
+          />
+        )}
+
+        {success === true && data.length > 0 && (
+          <>
+            <div className="card-header">
+              <div className="card-header-item">
+                <h6 className="m-0">Last Consensus Rounds</h6>
+              </div>
             </div>
-          </div>
-          <div className="card-body">
-            <div className="squares" data-testid="rounds">
-              {rounds.length &&
-                rounds.map((round: any) => (
-                  <OverlayTrigger
-                    key={round.key}
-                    placement="top"
-                    delay={{ show: 250, hide: 400 }}
-                    overlay={(props: any) => (
-                      <Tooltip id={round.key} {...props}>
-                        Block {round.value ? ' ' : ' not '} proposed{' '}
-                        {round.key.indexOf('_') > 0 ? round.key.split('_').pop() : round.key}
-                      </Tooltip>
-                    )}
-                  >
-                    <div className={round.value ? 'full square-block' : 'square-block'} />
-                  </OverlayTrigger>
-                ))}
+            <div className="card-body">
+              <div className="squares" data-testid="rounds">
+                {data.length &&
+                  data.map((round: any) => (
+                    <OverlayTrigger
+                      key={round.key}
+                      placement="top"
+                      delay={{ show: 250, hide: 400 }}
+                      overlay={(props: any) => (
+                        <Tooltip id={round.key} {...props}>
+                          Block {round.value ? ' ' : ' not '} proposed{' '}
+                          {round.key.indexOf('_') > 0 ? round.key.split('_').pop() : round.key}
+                        </Tooltip>
+                      )}
+                    >
+                      <div className={round.value ? 'full square-block' : 'square-block'} />
+                    </OverlayTrigger>
+                  ))}
+              </div>
             </div>
-          </div>
-        </>
+          </>
+        )}
       </div>
     </>
   );
