66b1997710768eb9a86b0dae6ac2170c4b1d09f4
Tudor Morar <tudor.morar@elrond.com>
2020-11-09 15:13:28 +0200

    Change delegation chart to chart.js

--- src/components/Address/DelegationDetails/index.tsx
+++ src/components/Address/DelegationDetails/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Chart from './Chart';
+import DelegationChart from './DelegationChart';
 import { useGlobalState } from 'context';
 import { AddressDetailsType } from './../AddressDetails';
 
@@ -14,33 +14,29 @@ const DelegationDetails = (props: AddressDetailsType) => {
   const stake = props.stake.toLocaleString('en', { minimumFractionDigits: 4 });
   const rewards = props.claimableRewards.toLocaleString('en', { minimumFractionDigits: 4 });
 
-  return props.addressId ? (
-    <>
-      {props.stake > 0 && (
-        <div className="card chart">
-          <div className="card-body bg-primary d-flex align-items-center">
-            <div className="chart-container mr-4">
-              <div>
-                <Chart stake={props.stake} claimableRewards={props.claimableRewards} />
-              </div>
-            </div>
-            <div className="text-white ml-1">
-              <p className="font-weight-bold mb-1">
-                <span className="delegation-label">Total:</span> {total} {erdLabel}{' '}
-              </p>
-              <p className="mb-1">
-                <span className={`badge badge-pill badge-status badge-delegation`}>&nbsp;</span>
-                &nbsp;<span className="delegation-label">Delegation:</span> {stake}
-              </p>
-              <p className="mb-0">
-                <span className={`badge badge-pill badge-status badge-rewards`}>&nbsp;</span>
-                &nbsp;<span className="delegation-label">Rewards:</span> {rewards}
-              </p>
-            </div>
+  return props.addressId && props.stake > 0 ? (
+    <div className="card chart">
+      <div className="card-body bg-primary d-flex align-items-center">
+        <div className="chart-container mr-4">
+          <div>
+            <DelegationChart stake={props.stake} claimableRewards={props.claimableRewards} />
           </div>
         </div>
-      )}
-    </>
+        <div className="text-white ml-1">
+          <p className="font-weight-bold mb-1">
+            <span className="delegation-label">Total:</span> {total} {erdLabel}{' '}
+          </p>
+          <p className="mb-1">
+            <span className={`badge badge-pill badge-status badge-delegation`}>&nbsp;</span>
+            &nbsp;<span className="delegation-label">Delegation:</span> {stake}
+          </p>
+          <p className="mb-0">
+            <span className={`badge badge-pill badge-status badge-rewards`}>&nbsp;</span>
+            &nbsp;<span className="delegation-label">Rewards:</span> {rewards}
+          </p>
+        </div>
+      </div>
+    </div>
   ) : null;
 };
 
