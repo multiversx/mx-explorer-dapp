f08f6a9e47dff7da226024223dfc644945ab9da8
Dan Popa <popa.ioan.dan@gmail.com>
2020-11-09 19:44:14 +0200

    Fixes identity details link on node details page; Fixes page states on Identity details page;

--- src/components/IdentityDetails/index.tsx
+++ src/components/IdentityDetails/index.tsx
@@ -34,7 +34,7 @@ const IdentityDetails = () => {
   React.useEffect(fetchData, []);
 
   return (
-    <div ref={ref}>
+    <>
       {dataReady === undefined && <Loader />}
       {dataReady === false && (
         <PageState
@@ -44,119 +44,121 @@ const IdentityDetails = () => {
           dataTestId="errorScreen"
         />
       )}
-      {dataReady === true && identity && (
-        <div className="container py-spacer">
-          <div className="row page-header">
-            <div className="col-12">
-              <h3 className="page-title mb-4">
-                <span data-testid="title">Validator Details</span>
-              </h3>
+      <div ref={ref}>
+        {dataReady === true && identity && (
+          <div className="container py-spacer">
+            <div className="row page-header">
+              <div className="col-12">
+                <h3 className="page-title mb-4">
+                  <span data-testid="title">Validator Details</span>
+                </h3>
+              </div>
             </div>
-          </div>
-          <div className="row " data-testid="identityDetailsContainer">
-            <div className="col-12 col-md-6 mb-spacer">
-              <div className="card">
-                <div className="card-header">
-                  <div className="card-header-item p-0">
-                    <div className="identity-header-item px-lg-spacer">
-                      <img
-                        className={`mr-3 avatar rounded-circle shadow-sm ${
-                          identity.avatar ? '' : 'gray'
-                        }`}
-                        src={identity.avatar ? identity.avatar : '/validators/default-avatar.svg'}
-                        alt={identity.name}
-                        height="42"
-                      />
+            <div className="row " data-testid="identityDetailsContainer">
+              <div className="col-12 col-md-6 mb-spacer">
+                <div className="card">
+                  <div className="card-header">
+                    <div className="card-header-item p-0">
+                      <div className="identity-header-item px-lg-spacer">
+                        <img
+                          className={`mr-3 avatar rounded-circle shadow-sm ${
+                            identity.avatar ? '' : 'gray'
+                          }`}
+                          src={identity.avatar ? identity.avatar : '/validators/default-avatar.svg'}
+                          alt={identity.name}
+                          height="42"
+                        />
 
-                      {identity.name ? identity.name : 'N/A'}
+                        {identity.name ? identity.name : 'N/A'}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card-body p-0">
+                    <div className="container-fluid">
+                      <DetailItem title="Location" colWidth="3">
+                        {identity.location ? (
+                          identity.location
+                        ) : (
+                          <span className="text-muted">N/A</span>
+                        )}
+                      </DetailItem>
+
+                      <DetailItem title="Twitter" colWidth="3">
+                        {identity.twitter ? (
+                          <a target={`_blank`} href={identity.twitter}>
+                            {identity.twitter.split('/').pop()}
+                          </a>
+                        ) : (
+                          <span className="text-muted">N/A</span>
+                        )}
+                      </DetailItem>
+
+                      <DetailItem title="Web" colWidth="3">
+                        {identity.website ? (
+                          <a target={`_blank`} href={identity.website}>
+                            {identity.website}
+                          </a>
+                        ) : (
+                          <span className="text-muted">N/A</span>
+                        )}
+                      </DetailItem>
                     </div>
                   </div>
                 </div>
-                <div className="card-body p-0">
-                  <div className="container-fluid">
-                    <DetailItem title="Location" colWidth="3">
-                      {identity.location ? (
-                        identity.location
-                      ) : (
-                        <span className="text-muted">N/A</span>
-                      )}
-                    </DetailItem>
+              </div>
+              <div className="col-12 col-md-6 mb-spacer">
+                <div className="card">
+                  <div className="card-body p-0">
+                    <div className="container-fluid">
+                      <DetailItem title="Stake" colWidth="4">
+                        {identity.stake.toLocaleString('en')}
+                      </DetailItem>
 
-                    <DetailItem title="Twitter" colWidth="3">
-                      {identity.twitter ? (
-                        <a target={`_blank`} href={identity.twitter}>
-                          {identity.twitter.split('/').pop()}
-                        </a>
-                      ) : (
-                        <span className="text-muted">N/A</span>
-                      )}
-                    </DetailItem>
+                      <DetailItem title="Stake percent" colWidth="4">
+                        {Math.round(identity.stakePercent) > 0
+                          ? Math.round(identity.stakePercent)
+                          : '< 1'}
+                        %
+                      </DetailItem>
 
-                    <DetailItem title="Web" colWidth="3">
-                      {identity.website ? (
-                        <a target={`_blank`} href={identity.website}>
-                          {identity.website}
-                        </a>
-                      ) : (
-                        <span className="text-muted">N/A</span>
-                      )}
-                    </DetailItem>
+                      <DetailItem title="Nodes" colWidth="4">
+                        {identity.validators.toLocaleString('en')}
+                      </DetailItem>
+
+                      <DetailItem title="Score" colWidth="4">
+                        {Math.floor(identity.score).toLocaleString('en')}
+                      </DetailItem>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="col-12 col-md-6 mb-spacer">
-              <div className="card">
-                <div className="card-body p-0">
-                  <div className="container-fluid">
-                    <DetailItem title="Stake" colWidth="4">
-                      {identity.stake.toLocaleString('en')}
-                    </DetailItem>
+            <div className="row">
+              <div className="col-12">
+                <div className="card">
+                  <div className="card-header">
+                    <div className="card-header-item">
+                      <h6 className="m-0" data-testid="title">
+                        Nodes
+                      </h6>
+                    </div>
+                  </div>
 
-                    <DetailItem title="Stake percent" colWidth="4">
-                      {Math.round(identity.stakePercent) > 0
-                        ? Math.round(identity.stakePercent)
-                        : '< 1'}
-                      %
-                    </DetailItem>
-
-                    <DetailItem title="Nodes" colWidth="4">
-                      {identity.validators.toLocaleString('en')}
-                    </DetailItem>
-
-                    <DetailItem title="Score" colWidth="4">
-                      {Math.floor(identity.score).toLocaleString('en')}
-                    </DetailItem>
+                  <div className="card-body p-0">
+                    <NodesTable>
+                      <NodesTable.Body nodes={nodes} />
+                    </NodesTable>
+                  </div>
+                  <div className="card-footer">
+                    <Pager itemsPerPage={25} page={String(size)} total={totalNodes} show />
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="row">
-            <div className="col-12">
-              <div className="card">
-                <div className="card-header">
-                  <div className="card-header-item">
-                    <h6 className="m-0" data-testid="title">
-                      Nodes
-                    </h6>
-                  </div>
-                </div>
-
-                <div className="card-body p-0">
-                  <NodesTable>
-                    <NodesTable.Body nodes={nodes} />
-                  </NodesTable>
-                </div>
-                <div className="card-footer">
-                  <Pager itemsPerPage={25} page={String(size)} total={totalNodes} show />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
 
