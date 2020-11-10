b9b70b91cd488963f6d22fc39b3c4ee470987e18
Tudor Morar <tudor.morar@elrond.com>
2020-11-10 18:14:42 +0200

    Make timeAgo hide zero seconds

--- src/sharedComponents/TimeAgo/timeAgo.tsx
+++ src/sharedComponents/TimeAgo/timeAgo.tsx
@@ -1,36 +1,40 @@
 import moment from 'moment';
 
-export default function timeAgo(timestamp: number) {
-  function dhms(ms: number) {
-    let days = Math.floor(ms / (24 * 60 * 60 * 1000));
-    let daysms = ms % (24 * 60 * 60 * 1000);
-    let hrs = Math.floor(daysms / (60 * 60 * 1000));
-    let hrsms = daysms % (60 * 60 * 1000);
-    let mins = Math.floor(hrsms / (60 * 1000));
-    let minsms = hrsms % (60 * 1000);
-    let secs = Math.floor(minsms / 1000);
+export function dhms(ms: number) {
+  let days = Math.floor(ms / (24 * 60 * 60 * 1000));
+  let daysms = ms % (24 * 60 * 60 * 1000);
+  let hrs = Math.floor(daysms / (60 * 60 * 1000));
+  let hrsms = daysms % (60 * 60 * 1000);
+  let mins = Math.floor(hrsms / (60 * 1000));
+  let minsms = hrsms % (60 * 1000);
+  let secs = Math.floor(minsms / 1000);
 
-    // let diff = ' ago';
-    let diff = '';
-    let secsString = secs + ' sec';
-    let minsString = mins + ' min';
-    let hrsString = hrs + ' hr';
-    let daysString = days + ' day';
+  // let diff = ' ago';
+  let diff = '';
+  let secsString = secs + ' sec';
+  let minsString = mins + ' min';
+  let hrsString = hrs + ' hr';
+  let daysString = days + ' day';
 
-    if (secs > 1) secsString = secs + ' secs';
-    if (mins > 1) minsString = mins + ' mins';
-    if (hrs > 1) hrsString = hrs + ' hrs';
-    if (days > 1) daysString = days + ' days';
+  if (secs > 1) secsString = secs + ' secs';
+  if (mins > 1) minsString = mins + ' mins';
+  if (hrs > 1) hrsString = hrs + ' hrs';
+  if (days > 1) daysString = days + ' days';
 
-    if (days >= 1) return daysString + ' ' + hrsString + diff;
-    if (hrs >= 1) return hrsString + ' ' + minsString + diff;
-    if (mins >= 1) return minsString + ' ' + secsString + diff;
-
-    return secsString + diff;
+  if (days >= 1) return daysString + ' ' + hrsString + diff;
+  if (hrs >= 1) return hrsString + ' ' + minsString + diff;
+  if (mins >= 1) {
+    const secString = secs === 0 ? '' : ' ' + secsString + diff;
+    return minsString + secString;
   }
 
-  let dateNow = moment.utc();
-  let txtTime = moment.utc(timestamp);
-  let diffInMs = dateNow.diff(txtTime);
+  return secsString + diff;
+}
+
+export default function timeAgo(timestamp: number) {
+  const dateNow = moment.utc();
+  const txtTime = moment.utc(timestamp);
+  const diffInMs = dateNow.diff(txtTime);
+
   return dhms(diffInMs);
 }
