0a172ca38633fa51f2e1544e84b7231748390323
Tudor Morar <tudor.morar@elrond.com>
2020-11-10 18:14:42 +0200

    Make timeAgo hide zero seconds

---
+++ src/sharedComponents/TimeAgo/tests/timeAgo.test.tsx
@@ -0,0 +1,12 @@
+import moment from 'moment';
+import { dhms } from '../timeAgo';
+
+describe('TimeAgo tests', () => {
+  test('Display only minutes when seconds are zero', async () => {
+    const txTime = moment();
+    const time = moment().add(5, 'minutes');
+    const diffInMs = time.diff(txTime);
+
+    expect(dhms(diffInMs)).toBe('5 mins');
+  });
+});
