6630818bfd0d9e0ebb1dd3702d94df02c014eb14
Dan Popa <popa.ioan.dan@gmail.com>
2020-11-12 12:17:51 +0200

    Adds unable to load page state to delegation box on address details

--- src/components/Address/DelegationDetails/index.tsx
+++ src/components/Address/DelegationDetails/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import DelegationChart from './DelegationChart';
 import { useGlobalState } from 'context';
 import { AddressDetailsType } from './../AddressDetails';
+import { PageState } from 'sharedComponents';
+import { faCoins } from '@fortawesome/pro-regular-svg-icons/faCoins';
 
 const DelegationDetails = (props: AddressDetailsType) => {
   const {
@@ -14,28 +16,43 @@ const DelegationDetails = (props: AddressDetailsType) => {
   const stake = props.stake.toLocaleString('en', { minimumFractionDigits: 4 });
   const rewards = props.claimableRewards.toLocaleString('en', { minimumFractionDigits: 4 });
 
-  return props.address && props.stake > 0 ? (
-    <div className="card chart">
-      <div className="card-body bg-primary d-flex align-items-center">
-        <div className="mr-4">
-          <DelegationChart stake={props.stake} claimableRewards={props.claimableRewards} />
+  return (
+    <>
+      {props.address && props.rewardsFetched === false && (
+        <div className="card py-3">
+          <PageState
+            icon={faCoins}
+            title="Unable to load delegation"
+            className="delegation-page-state page-state-sm"
+            dataTestId="delegationErrorScreen"
+          />
         </div>
-        <div className="text-white ml-1">
-          <p className="font-weight-bold mb-1">
-            <span className="delegation-label">Total:</span> {total} {erdLabel}{' '}
-          </p>
-          <p className="mb-1">
-            <span className={`badge badge-pill badge-status badge-delegation`}>&nbsp;</span>
-            &nbsp;<span className="delegation-label">Delegation:</span> {stake}
-          </p>
-          <p className="mb-0">
-            <span className={`badge badge-pill badge-status badge-rewards`}>&nbsp;</span>
-            &nbsp;<span className="delegation-label">Rewards:</span> {rewards}
-          </p>
+      )}
+
+      {props.address && props.rewardsFetched && props.stake > 0 && (
+        <div className="card chart">
+          <div className="card-body bg-primary d-flex align-items-center">
+            <div className="mr-4">
+              <DelegationChart stake={props.stake} claimableRewards={props.claimableRewards} />
+            </div>
+            <div className="text-white ml-1">
+              <p className="font-weight-bold mb-1">
+                <span className="delegation-label">Total:</span> {total} {erdLabel}{' '}
+              </p>
+              <p className="mb-1">
+                <span className={`badge badge-pill badge-status badge-delegation`}>&nbsp;</span>
+                &nbsp;<span className="delegation-label">Delegation:</span> {stake}
+              </p>
+              <p className="mb-0">
+                <span className={`badge badge-pill badge-status badge-rewards`}>&nbsp;</span>
+                &nbsp;<span className="delegation-label">Rewards:</span> {rewards}
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  ) : null;
+      )}
+    </>
+  );
 };
 
 export default DelegationDetails;
