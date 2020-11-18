e3f12662487be5de4f6571b8944ce1e5ca97f47c
Dan Popa <popa.ioan.dan@gmail.com>
2020-11-18 19:09:31 +0200

    Fixes rounds and blocks cards states on node details page

--- src/sharedComponents/BlocksTable/NoBlocks.tsx
+++ src/sharedComponents/BlocksTable/NoBlocks.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
-import { faExchangeAlt } from '@fortawesome/pro-regular-svg-icons/faExchangeAlt';
+import { faCube } from '@fortawesome/pro-regular-svg-icons/faCube';
 import { PageState } from 'sharedComponents';
 
 export default function NoBlocks({ title }: { title?: string }) {
   return (
-    <PageState
-      icon={faExchangeAlt}
-      title={title ? title : 'No blocks'}
-      className="py-spacer my-auto"
-    />
+    <PageState icon={faCube} title={title ? title : 'No blocks'} className="py-spacer my-auto" />
   );
 }
