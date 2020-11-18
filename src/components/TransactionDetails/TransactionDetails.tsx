b6d35feb2402126a13a4878c1b82378200f21661
Dan Popa <popa.ioan.dan@gmail.com>
2020-11-18 16:02:30 +0200

    Shows all returnMessages below To field on transaction details page

--- src/components/TransactionDetails/TransactionDetails.tsx
+++ src/components/TransactionDetails/TransactionDetails.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { faClock } from '@fortawesome/pro-regular-svg-icons/faClock';
+import { faAngleDown } from '@fortawesome/pro-regular-svg-icons/faAngleDown';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import BigNumber from 'bignumber.js';
 import { addressIsBech32, dateFormatted, urlBuilder } from 'helpers';
@@ -44,7 +45,23 @@ const getFee = (transaction: TransactionType) => {
   return output;
 };
 
+const getScResultsMessages = (transaction: TransactionType) => {
+  const messages: string[] = [];
+
+  if (transaction.scResults) {
+    transaction.scResults.forEach((result) => {
+      if (result.returnMessage) {
+        messages.push(result.returnMessage);
+      }
+    });
+  }
+
+  return messages;
+};
+
 const Details = ({ transaction }: { transaction: TransactionType }) => {
+  const scResultsMessages = getScResultsMessages(transaction);
+
   return (
     <div className="card">
       <div className="card-body p-0">
@@ -118,23 +135,37 @@ const Details = ({ transaction }: { transaction: TransactionType }) => {
           </DetailItem>
 
           <DetailItem title="To">
-            <div className="d-flex align-items-center">
-              <ScAddressIcon initiator={transaction.receiver} />
-              <NetworkLink
-                to={urlBuilder.accountDetails(transaction.receiver)}
-                className="trim-wrapper"
-              >
-                <Trim text={transaction.receiver} />
-              </NetworkLink>
-              &nbsp;
-              {!isNaN(transaction.receiverShard) && (
+            <div className="d-flex flex-column">
+              <div className="d-flex align-items-center">
+                <ScAddressIcon initiator={transaction.receiver} />
                 <NetworkLink
-                  to={urlBuilder.receiverShard(transaction.receiverShard)}
-                  className="flex-shrink-0"
+                  to={urlBuilder.accountDetails(transaction.receiver)}
+                  className="trim-wrapper"
                 >
-                  (<ShardSpan shard={transaction.receiverShard} />)
+                  <Trim text={transaction.receiver} />
                 </NetworkLink>
-              )}
+                &nbsp;
+                {!isNaN(transaction.receiverShard) && (
+                  <NetworkLink
+                    to={urlBuilder.receiverShard(transaction.receiverShard)}
+                    className="flex-shrink-0"
+                  >
+                    (<ShardSpan shard={transaction.receiverShard} />)
+                  </NetworkLink>
+                )}
+              </div>
+              {scResultsMessages.map((msg, i) => (
+                <div key={i} className="d-flex ml-1">
+                  <FontAwesomeIcon
+                    icon={faAngleDown}
+                    className="text-secondary"
+                    style={{ marginTop: '2px' }}
+                    transform={{ rotate: 45 }}
+                  />
+                  &nbsp;
+                  <small className="text-danger ml-1"> {msg}</small>
+                </div>
+              ))}
             </div>
           </DetailItem>
 
