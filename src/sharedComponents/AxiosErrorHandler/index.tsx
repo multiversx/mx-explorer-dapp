be320087c22ca36260dc7bc016f84f9760576196
Tudor Morar <tudor.morar@elrond.com>
2020-12-15 13:15:51 +0200

    Configure token WIP

--- src/sharedComponents/AxiosErrorHandler/index.tsx
+++ src/sharedComponents/AxiosErrorHandler/index.tsx
@@ -3,12 +3,28 @@ import axios from 'axios';
 import { analytics } from 'helpers';
 
 const AxiosErrorHandler = ({ children }: { children: React.ReactNode }) => {
-  const [initialize, setInitialize] = React.useState(false);
+  const timeoutRef = React.useRef<any>();
+  const [interceptorsReady, setInterceptorsReady] = React.useState(false);
+  const [requestId, setRequestId] = React.useState(-1);
+  const [responseId, setResponseId] = React.useState(-1);
+  const [token, setToken] = React.useState('');
   const explorerVersion = process.env.REACT_APP_CACHE_BUST;
   const ignoreList: string[] = [];
 
-  const intiAxios = () => {
-    axios.interceptors.response.use(
+  const setInterceptors = (newToken: string) => {
+    const newRequestId = axios.interceptors.request.use(
+      async (config) => {
+        config.headers = {
+          'x-access': `${newToken}`,
+        };
+        return config;
+      },
+      (error) => {
+        Promise.reject(error);
+      }
+    );
+    setRequestId(newRequestId);
+    const newResponseId = axios.interceptors.response.use(
       (response) => {
         return response;
       },
@@ -19,14 +35,50 @@ const AxiosErrorHandler = ({ children }: { children: React.ReactNode }) => {
         if (explorerVersion !== undefined && logError) {
           analytics.sendEvent({ action: 'failed-request', label: reqUrl, explorerVersion });
         }
+
         return Promise.reject(error);
       }
     );
-    setInitialize(true);
+    setResponseId(newResponseId);
   };
 
-  React.useEffect(intiAxios, []);
-  return initialize ? <>{children}</> : null;
+  const fetchToken = () => {
+    const instance = axios.create();
+    instance
+      .post('***REMOVED***/access', '')
+      .then((response) => {
+        const newToken = response.headers['x-access'];
+        setToken(newToken);
+        setInterceptors(newToken);
+        setInterceptorsReady(true);
+      })
+      .catch((err) => {
+        console.error(err);
+        setInterceptorsReady(true);
+      });
+  };
+
+  const configureAxios = () => {
+    if (!token) {
+      fetchToken();
+    } else {
+      const timestamp = token.split('-').pop();
+      const tokenTimestamp = timestamp ? parseInt(timestamp) : 0;
+      const now = Math.floor(Date.now() / 1000);
+      const fetchNextTokenSec = tokenTimestamp - now - 115;
+      timeoutRef.current = setTimeout(() => {
+        axios.interceptors.request.eject(requestId);
+        axios.interceptors.request.eject(responseId);
+        fetchToken();
+      }, fetchNextTokenSec * 1000);
+    }
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
+  };
+
+  React.useEffect(configureAxios, [token]);
+  return interceptorsReady ? <>{children}</> : null;
 };
 
 export default AxiosErrorHandler;
