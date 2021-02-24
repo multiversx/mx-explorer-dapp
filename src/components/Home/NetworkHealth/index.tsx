10dec37516b93873ce6584b3079bbafc3fac5174
Dan Popa <popa.ioan.dan@gmail.com>
2021-02-24 13:12:17 +0200

    Displays NetworkHealth data on svg animation

--- src/components/Home/NetworkHealth/index.tsx
+++ src/components/Home/NetworkHealth/index.tsx
@@ -1,25 +1,96 @@
 import * as React from 'react';
+import { useGlobalState } from 'context';
+import { adapter } from 'sharedComponents';
 import { ReactComponent as Gear } from 'assets/images/network-health/gear.svg';
 import { ReactComponent as CenterGear } from 'assets/images/network-health/center-gear.svg';
 import { ReactComponent as LayoutGear } from 'assets/images/network-health/layout-gear.svg';
 import ProgressRing from './ProgressRing';
+import moment from 'moment';
+
+export interface StateType {
+  shards: string;
+  blocks: string;
+  accounts: string;
+  transactions: string;
+}
+
+const initialState = {
+  shards: '...',
+  blocks: '...',
+  accounts: '...',
+  transactions: '...',
+  epoch: '...',
+  epochPercentage: 0,
+  epochTotalTime: '...',
+  epochTimeElapsed: '...',
+  epochTimeRemaining: '...',
+};
 
 const NetworkHealth = () => {
-  const [epochProgress, setEpochProgress] = React.useState(0);
-  const [blockTimeProgress, setBlockTimeProgress] = React.useState(0);
-  const progressStep = 10;
+  const {
+    activeNetworkId,
+    refresh: { timestamp },
+  } = useGlobalState();
+
+  const { getStats } = adapter();
+
+  const [state, setState] = React.useState({
+    [activeNetworkId]: initialState,
+  });
+  const [oldTestnetId, setOldTestnetId] = React.useState('');
+  const ref = React.useRef(null);
 
   React.useEffect(() => {
-    const intervalEpoch = setInterval(() => {
-      setEpochProgress((epochProgress) => {
-        if (epochProgress >= 100) {
-          setEpochProgress(0);
+    setOldTestnetId(activeNetworkId);
+  }, [activeNetworkId]);
+
+  const getData = () => {
+    if (ref.current !== null) {
+      getStats().then((statsData) => {
+        const { data, success } = statsData;
+        const check = success ? data.roundsPerEpoch >= data.roundsPassed : false;
+        const newState = success
+          ? {
+              shards: parseInt(data.shards).toLocaleString('en'),
+              blocks: parseInt(data.blocks).toLocaleString('en'),
+              accounts: parseInt(data.accounts).toLocaleString('en'),
+              transactions: parseInt(data.transactions).toLocaleString('en'),
+              epoch: data.epoch.toLocaleString('en'),
+              epochPercentage: check ? (100 * data.roundsPassed) / data.roundsPerEpoch : 0,
+              epochTotalTime: check
+                ? moment.utc(data.refreshRate * data.roundsPerEpoch).format('HH:mm')
+                : '...',
+              epochTimeElapsed: check
+                ? moment.utc(data.refreshRate * data.roundsPassed).format('HH:mm')
+                : '...',
+              epochTimeRemaining: check
+                ? moment
+                    .utc(data.refreshRate * (data.roundsPerEpoch - data.roundsPassed))
+                    .format('HH:mm')
+                : '...',
+            }
+          : initialState;
+
+        if (ref.current !== null) {
+          const sameTestnet = oldTestnetId === activeNetworkId;
+          if (success || (!success && !sameTestnet)) {
+            setState((state) => ({
+              ...state,
+              [activeNetworkId]: newState,
+            }));
+          }
         }
-        return epochProgress + progressStep;
       });
-    }, 1000);
-    return () => clearInterval(intervalEpoch);
-  }, []);
+    }
+  };
+
+  React.useEffect(getData, [timestamp, activeNetworkId]);
+
+  const { shards, blocks, accounts, transactions, epoch, epochPercentage, epochTimeRemaining } =
+    activeNetworkId in state ? state[activeNetworkId] : initialState;
+
+  const [blockTimeProgress, setBlockTimeProgress] = React.useState(0);
+  const progressStep = 10;
 
   React.useEffect(() => {
     const intervalBlockTime = setInterval(() => {
@@ -34,7 +105,7 @@ const NetworkHealth = () => {
   }, []);
 
   return (
-    <div className="card network-health">
+    <div ref={ref} className="card network-health">
       <div className="card-header">
         <div className="card-header-item d-flex justify-content-between align-items-center">
           <h6 className="m-0">Network Health</h6>
@@ -47,29 +118,33 @@ const NetworkHealth = () => {
           <div className="gear-container top-left">
             <Gear className="gear rotate" />
             <div className="gear-content">
-              <small>Accounts</small>
+              {accounts}
+              <small className="text-secondary">Accounts</small>
             </div>
           </div>
 
           <div className="gear-container top-right">
             <Gear className="gear rotate" />
             <div className="gear-content">
-              <small>Transactions</small>
+              {transactions}
+              <small className="text-secondary">Transactions</small>
             </div>
           </div>
 
           <div className="gear-container center">
             <CenterGear className="gear" />
             <div className="gear-content">
-              <ProgressRing radius={60} stroke={3} progress={epochProgress} />
-              <small>Epoch</small>
+              <ProgressRing radius={60} stroke={3} progress={epochPercentage} />
+              {epochTimeRemaining}
+              <small className="text-secondary">Epoch {epoch}</small>
             </div>
           </div>
 
           <div className="gear-container bottom-left">
             <Gear className="gear rotate" />
             <div className="gear-content">
-              <small>Block Height</small>
+              {blocks}
+              <small className="text-secondary">Block Height</small>
             </div>
           </div>
 
@@ -82,7 +157,7 @@ const NetworkHealth = () => {
                 progress={blockTimeProgress}
                 dotted={progressStep}
               />
-              <small>Block Time</small>
+              <small className="text-secondary">Block Time</small>
             </div>
           </div>
         </div>
