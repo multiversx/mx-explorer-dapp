8c714e617363d3f376c10a851a0368b8d0059acb
Dan Popa <popa.ioan.dan@gmail.com>
2021-02-25 15:43:16 +0200

    BlockTime loop refactor and other small improvements

--- src/components/Home/NetworkHealth/index.tsx
+++ src/components/Home/NetworkHealth/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import moment from 'moment';
 import { useGlobalState } from 'context';
 import { adapter } from 'sharedComponents';
 import { ReactComponent as Gear } from 'assets/images/network-health/gear.svg';
@@ -6,8 +7,7 @@ import { ReactComponent as BigGear } from 'assets/images/network-health/big-gear
 import { ReactComponent as CenterGear } from 'assets/images/network-health/center-gear.svg';
 import { ReactComponent as LayoutGear } from 'assets/images/network-health/layout-gear.svg';
 import ProgressRing from './ProgressRing';
-import moment from 'moment';
-import { number } from 'yup';
+import { refreshRate } from 'appConfig';
 
 export interface StateType {
   shards: string;
@@ -28,87 +28,94 @@ const initialState = {
   epochTimeRemaining: '...',
 };
 
+const processStats = (statsData: any) => {
+  const { data, success } = statsData;
+  const check = success ? data.roundsPerEpoch >= data.roundsPassed : false;
+  const newState = success
+    ? {
+        shards: parseInt(data.shards).toLocaleString('en'),
+        blocks: parseInt(data.blocks).toLocaleString('en'),
+        accounts: parseInt(data.accounts).toLocaleString('en'),
+        transactions: parseInt(data.transactions).toLocaleString('en'),
+        epoch: data.epoch.toLocaleString('en'),
+        epochPercentage: check ? (100 * data.roundsPassed) / data.roundsPerEpoch : 0,
+        epochTotalTime: check
+          ? moment.utc(data.refreshRate * data.roundsPerEpoch).format('HH:mm')
+          : '...',
+        epochTimeElapsed: check
+          ? moment.utc(data.refreshRate * data.roundsPassed).format('HH:mm')
+          : '...',
+        epochTimeRemaining: check
+          ? moment.utc(data.refreshRate * (data.roundsPerEpoch - data.roundsPassed)).format('HH:mm')
+          : '...',
+      }
+    : initialState;
+
+  return newState;
+};
+
 const NetworkHealth = () => {
   const {
     activeNetworkId,
     refresh: { timestamp },
   } = useGlobalState();
-
   const { getStats } = adapter();
 
-  const [state, setState] = React.useState({
-    [activeNetworkId]: initialState,
-  });
-  const [oldTestnetId, setOldTestnetId] = React.useState('');
   const ref = React.useRef(null);
+  const [stateBuffer, setStateBuffer] = React.useState<typeof initialState | undefined>();
+  const [state, setState] = React.useState(initialState);
 
+  const [oldTestnetId, setOldTestnetId] = React.useState(activeNetworkId);
   React.useEffect(() => {
     setOldTestnetId(activeNetworkId);
   }, [activeNetworkId]);
 
-  const getData = () => {
-    if (ref.current !== null) {
-      getStats().then((statsData) => {
-        const { data, success } = statsData;
-        const check = success ? data.roundsPerEpoch >= data.roundsPassed : false;
-        const newState = success
-          ? {
-              shards: parseInt(data.shards).toLocaleString('en'),
-              blocks: parseInt(data.blocks).toLocaleString('en'),
-              accounts: parseInt(data.accounts).toLocaleString('en'),
-              transactions: parseInt(data.transactions).toLocaleString('en'),
-              epoch: data.epoch.toLocaleString('en'),
-              epochPercentage: check ? (100 * data.roundsPassed) / data.roundsPerEpoch : 0,
-              epochTotalTime: check
-                ? moment.utc(data.refreshRate * data.roundsPerEpoch).format('HH:mm')
-                : '...',
-              epochTimeElapsed: check
-                ? moment.utc(data.refreshRate * data.roundsPassed).format('HH:mm')
-                : '...',
-              epochTimeRemaining: check
-                ? moment
-                    .utc(data.refreshRate * (data.roundsPerEpoch - data.roundsPassed))
-                    .format('HH:mm')
-                : '...',
-            }
-          : initialState;
-
-        if (ref.current !== null) {
-          const sameTestnet = oldTestnetId === activeNetworkId;
-          if (success || (!success && !sameTestnet)) {
-            setState((state) => ({
-              ...state,
-              [activeNetworkId]: newState,
-            }));
-          }
-        }
-        setBlockTimeProgress(0);
-      });
-    }
+  const initStates = (stats: typeof initialState) => {
+    setState(stats);
+    setStateBuffer(stats);
+    blockTimeInterval();
   };
 
+  const getData = () => {
+    getStats().then((statsData) => {
+      const { success } = statsData;
+      const newStats = processStats(statsData);
+      const sameTestnet = oldTestnetId === activeNetworkId;
+
+      if (ref.current !== null && success) {
+        if (stateBuffer === undefined || !sameTestnet) {
+          // fresh page load or active testnet changed
+          initStates(newStats);
+        } else if (sameTestnet) {
+          setStateBuffer(newStats);
+        }
+      }
+    });
+  };
   React.useEffect(getData, [timestamp, activeNetworkId]);
 
-  const { blocks, accounts, transactions, epoch, epochPercentage, epochTimeRemaining } =
-    activeNetworkId in state ? state[activeNetworkId] : initialState;
-
-  const [blockTimeProgress, setBlockTimeProgress] = React.useState<number | undefined>();
-  const intervalInSec = 6;
+  const [blockTimeProgress, setBlockTimeProgress] = React.useState(0);
+  const intervalInSec = refreshRate / 1000;
 
   const blockTimeInterval = () => {
     const intervalBlockTime = setInterval(() => {
-      if (ref.current !== null) {
-        setBlockTimeProgress((blockTimeProgress) => {
-          return blockTimeProgress != undefined && blockTimeProgress < intervalInSec
-            ? blockTimeProgress + 1
-            : blockTimeProgress;
-        });
+      if (ref.current !== null && !document.hidden) {
+        setBlockTimeProgress((blockTimeProgress) =>
+          blockTimeProgress === intervalInSec ? 0 : blockTimeProgress + 1
+        );
       }
     }, 1000);
     return () => clearInterval(intervalBlockTime);
   };
 
-  React.useEffect(blockTimeInterval, [activeNetworkId]);
+  const updateSate = () => {
+    if (blockTimeProgress === intervalInSec && stateBuffer !== undefined) {
+      setState(stateBuffer);
+    }
+  };
+  React.useEffect(updateSate, [blockTimeProgress]);
+
+  const { blocks, accounts, transactions, epoch, epochPercentage, epochTimeRemaining } = state;
 
   return (
     <div ref={ref} className="card network-health">
@@ -158,12 +165,8 @@ const NetworkHealth = () => {
           <div className="gear-container bottom-right">
             <Gear className="gear" />
             <div className="gear-content">
-              <ProgressRing
-                progress={
-                  blockTimeProgress !== undefined ? (blockTimeProgress * 100) / intervalInSec : 0
-                }
-              />
-              {blockTimeProgress}
+              <ProgressRing progress={(blockTimeProgress * 100) / intervalInSec} />
+              {stateBuffer !== undefined ? blockTimeProgress : '...'}
               <small>Block Time</small>
             </div>
           </div>
