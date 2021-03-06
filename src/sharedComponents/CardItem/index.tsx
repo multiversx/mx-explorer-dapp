e18f2e0a8cd8b0c95189633603568655e299bac5
Dan Popa <popa.ioan.dan@gmail.com>
2021-03-06 23:22:38 +0200

    Node information card style update WIP

--- src/sharedComponents/CardItem/index.tsx
+++ src/sharedComponents/CardItem/index.tsx
@@ -5,17 +5,20 @@ const CardItem = ({
   children,
   title,
   icon,
+  customIcon,
 }: {
   children: React.ReactNode;
   title: string;
-  icon: any;
+  icon?: any;
+  customIcon?: React.ReactNode;
 }) => (
   <div
     className="card-item flex-fill p-2 d-flex align-items-center"
     style={{ maxWidth: '15rem', minWidth: '10rem' }}
   >
     <div className="right-angle-icon mr-2">
-      <FontAwesomeIcon icon={icon} />
+      {icon && <FontAwesomeIcon icon={icon} />}
+      {customIcon && <>{customIcon}</>}
     </div>
 
     <div className="card-icon-body min-w-0">
