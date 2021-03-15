fad463a5469d150e631ceed9cf493f21d66e3bf3
Dan Popa <popa.ioan.dan@gmail.com>
2021-03-15 11:08:54 +0200

    Buttons use primary-light color

--- src/components/ProviderDetails/ProviderStats.tsx
+++ src/components/ProviderDetails/ProviderStats.tsx
@@ -30,7 +30,7 @@ const ProviderStats = ({ provider }: { provider: types.ProviderType | undefined 
 
           {website && (
             <a
-              className="btn btn-sm btn-primary"
+              className="btn btn-sm btn-primary-light"
               target={`_blank`}
               rel={`noreferrer nofollow`}
               href={website}
