3ae65e83e64b104cfe9c0e3fac1cbbfd2373a4d2
Dan Popa <popa.ioan.dan@gmail.com>
2021-03-17 11:18:07 +0200

    Adds link to Provider Details page on Account Details page if it is the case

--- src/components/AccountDetails/AccountInfo/index.tsx
+++ src/components/AccountDetails/AccountInfo/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CardItem, CopyButton, Denominate } from 'sharedComponents';
+import { CardItem, CopyButton, Denominate, NetworkLink, adapter } from 'sharedComponents';
 import { useGlobalState } from 'context';
 import { ReactComponent as ElrondSymbol } from 'assets/images/elrond-symbol-chart.svg';
 import { faDollarSign } from '@fortawesome/pro-solid-svg-icons/faDollarSign';
@@ -8,25 +8,57 @@ import { faCoins } from '@fortawesome/pro-solid-svg-icons/faCoins';
 import { LockedAmountType } from '../AccountLayout';
 import UsdValue from './UsdValue';
 import LockedAmountCardItem from './LockedAmountCardItem';
+import { isContract, urlBuilder } from 'helpers';
 
 const AccountInfo = ({ lockedAmount }: { lockedAmount: LockedAmountType }) => {
+  const ref = React.useRef(null);
   const {
-    activeNetwork: { id, adapter },
+    activeNetwork: { id, adapter: networkAdapter },
     accountDetails,
     accountTokens,
   } = useGlobalState();
 
+  const { getProvider } = adapter();
+
   const { address, balance, nonce } = accountDetails;
-  const tokensActive = id !== 'mainnet' && adapter === 'api';
+  const tokensActive = id !== 'mainnet' && networkAdapter === 'api';
   const cardItemClass = tokensActive ? 'n5' : '';
 
+  const [isProvider, setIsProvider] = React.useState(false);
+  const fetchProviderDetails = () => {
+    if (isContract(accountDetails.address)) {
+      setIsProvider(true);
+      // getProvider(accountDetails.address).then(({ success, data }) => {
+      //   if (ref.current !== null) {
+      //     if (success && data !== undefined) {
+      //       setIsProvider(true);
+      //     }
+      //   }
+      // });
+    }
+  };
+
+  React.useEffect(() => {
+    fetchProviderDetails();
+  }, [id, accountDetails.address]);
+
   return address !== '' ? (
-    <div className="row account-info">
+    <div ref={ref} className="row account-info">
       <div className="col mb-spacer">
         <div className="card">
           <div className="card-header">
             <div className="card-header-item">
-              <h6 data-testid="title">Account Details</h6>
+              <div className="d-flex align-items-center justify-content-between">
+                <h6 data-testid="title">Account Details</h6>
+                {isProvider && (
+                  <NetworkLink
+                    to={urlBuilder.providerDetails(accountDetails.address)}
+                    className="btn btn-sm btn-primary-light"
+                  >
+                    Contract Details
+                  </NetworkLink>
+                )}
+              </div>
             </div>
             <div className="card-header-item compact d-flex">
               Address:
