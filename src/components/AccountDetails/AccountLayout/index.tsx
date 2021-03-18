591b5cb11ae0ea79804fe78c1cb7c1fff1c721fa
Dan Popa <popa.ioan.dan@gmail.com>
2021-03-18 16:19:22 +0200

    Moves Locked Amount api calls in own component. Fixes test.

--- src/components/AccountDetails/AccountLayout/index.tsx
+++ src/components/AccountDetails/AccountLayout/index.tsx
@@ -5,21 +5,8 @@ import { Loader, adapter } from 'sharedComponents';
 import FailedAccount from './FailedAccount';
 import { addressIsBech32, useNetworkRoute, useSize } from 'helpers';
 import AccountInfo from './AccountInfo';
-
 import { useRouteMatch } from 'react-router-dom';
 import { accountRoutes } from 'routes';
-
-export interface LockedAmountType {
-  stakeFetched?: boolean | undefined;
-  totalStaked?: string;
-  delegationFetched?: boolean | undefined;
-  userActiveStake?: string;
-  userDeferredPaymentStake?: string;
-  userUnstakedStake?: string;
-  userWaitingStake?: string;
-  userWithdrawOnlyStake?: string;
-  usd?: number;
-}
 
 const AccountLayout = ({ children }: { children: React.ReactNode }) => {
   const ref = React.useRef(null);
@@ -27,25 +14,14 @@ const AccountLayout = ({ children }: { children: React.ReactNode }) => {
   const { firstPageTicker } = useSize();
   const { activeNetwork, accountDetails } = useGlobalState();
   const dispatch = useGlobalDispatch();
-  const {
-    getAccount,
-    getAccountTokens,
-    getAccountDelegation,
-    getAccountStake,
-    getEgldPrice,
-  } = adapter();
+  const { getAccount, getAccountTokens } = adapter();
   const networkRoute = useNetworkRoute();
 
   const tokensActive = activeNetwork.id !== 'mainnet' && activeNetwork.adapter === 'api';
   const isOldAddressRoute = pathname.includes('/address/');
   const match: any = useRouteMatch(accountRoutes.index);
   const address = match ? match.params.hash : undefined;
-
   const [dataReady, setDataReady] = React.useState<boolean | undefined>();
-  const [lockedAmount, setLockedAmount] = React.useState<LockedAmountType>({
-    stakeFetched: undefined,
-    delegationFetched: undefined,
-  });
 
   const fetchBalanceAndCount = () => {
     if (!document.hidden) {
@@ -78,31 +54,6 @@ const AccountLayout = ({ children }: { children: React.ReactNode }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [firstPageTicker, activeNetwork.id, address]);
 
-  const fetchLockedAmountAndPrice = () => {
-    if (!document.hidden) {
-      Promise.all([getAccountDelegation(address), getAccountStake(address), getEgldPrice()]).then(
-        ([delegationData, stakeData, priceData]) => {
-          if (ref.current !== null) {
-            const delegationFetched = delegationData.success ? delegationData.data : {};
-            const stakeFetched = stakeData.success ? stakeData.data : {};
-            const usd =
-              priceData.success && priceData.data.length > 0
-                ? priceData.data[priceData.data.length - 1].value
-                : undefined;
-
-            setLockedAmount({
-              ...(delegationFetched ? delegationData.data : {}),
-              ...(stakeFetched ? stakeData.data : {}),
-              usd,
-              delegationFetched,
-              stakeFetched,
-            });
-          }
-        }
-      );
-    }
-  };
-
   const fetchAccountTokens = () => {
     if (tokensActive) {
       getAccountTokens(address).then(({ success, data }) => {
@@ -121,7 +72,6 @@ const AccountLayout = ({ children }: { children: React.ReactNode }) => {
 
   React.useEffect(() => {
     if (!isOldAddressRoute) {
-      fetchLockedAmountAndPrice();
       fetchAccountTokens();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -144,7 +94,7 @@ const AccountLayout = ({ children }: { children: React.ReactNode }) => {
       <div ref={ref}>
         {!loading && !failed && (
           <div className="container pt-spacer">
-            <AccountInfo lockedAmount={lockedAmount} />
+            <AccountInfo />
             {children}
           </div>
         )}
