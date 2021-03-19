c246ead6cc7567e69030a5155cab9fe68dbe82af
Dan Popa <popa.ioan.dan@gmail.com>
2021-03-19 13:47:32 +0200

    Display contracts tab on testnet

--- src/components/Nodes/NodesLayout/NodesTabs.tsx
+++ src/components/Nodes/NodesLayout/NodesTabs.tsx
@@ -1,47 +1,48 @@
+import { useIsMainnet } from 'helpers';
 import * as React from 'react';
 import { useLocation } from 'react-router-dom';
 import { validatorsRoutes } from 'routes';
 import { NetworkLink } from 'sharedComponents';
 
 const NodesTabs = () => {
+  const isMainnet = useIsMainnet();
   const activePath = useLocation().pathname;
 
   return (
     <div className="nodes-tabs card-header-item">
       <div className="d-flex">
-        <div>
-          {activePath.includes(validatorsRoutes.index) ? (
-            <div className="mr-3">
-              <h6>Validators</h6>
-            </div>
-          ) : (
-            <NetworkLink to={validatorsRoutes.index} className="tab-link mr-3">
-              <h6>Validators</h6>
-            </NetworkLink>
-          )}
-        </div>
-        <div>
-          {activePath.includes(validatorsRoutes.providers) ? (
-            <div className="mx-3">
-              <h6>Contracts</h6>
-            </div>
-          ) : (
-            <NetworkLink to={validatorsRoutes.providers} className="tab-link mx-3">
-              <h6>Contracts</h6>
-            </NetworkLink>
-          )}
-        </div>
-        <div>
-          {activePath.includes(validatorsRoutes.nodes) ? (
-            <div className="mx-3">
-              <h6>Nodes</h6>
-            </div>
-          ) : (
-            <NetworkLink to={validatorsRoutes.nodes} className="tab-link ml-3">
-              <h6>Nodes</h6>
-            </NetworkLink>
-          )}
-        </div>
+        {isMainnet && (
+          <>
+            {activePath.includes(validatorsRoutes.index) ? (
+              <div className="mr-3">
+                <h6>Validators</h6>
+              </div>
+            ) : (
+              <NetworkLink to={validatorsRoutes.index} className="tab-link mr-3">
+                <h6>Validators</h6>
+              </NetworkLink>
+            )}
+          </>
+        )}
+
+        {activePath.includes(validatorsRoutes.providers) ? (
+          <div className="mx-3">
+            <h6>Contracts</h6>
+          </div>
+        ) : (
+          <NetworkLink to={validatorsRoutes.providers} className="tab-link mx-3">
+            <h6>Contracts</h6>
+          </NetworkLink>
+        )}
+        {activePath.includes(validatorsRoutes.nodes) ? (
+          <div className="mx-3">
+            <h6>Nodes</h6>
+          </div>
+        ) : (
+          <NetworkLink to={validatorsRoutes.nodes} className="tab-link ml-3">
+            <h6>Nodes</h6>
+          </NetworkLink>
+        )}
       </div>
     </div>
   );
