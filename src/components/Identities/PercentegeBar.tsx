26cd9c307535555a2db8d73e7b7764dc49fe5379
Dan Popa <popa.ioan.dan@gmail.com>
2021-03-23 18:17:03 +0200

    Refactor identity percentage bar

--- src/components/Identities/PercentegeBar.tsx
+++ src/components/Identities/PercentegeBar.tsx
@@ -2,40 +2,38 @@ import * as React from 'react';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 const PercentegeBar = ({
-  totalUpTimePercentege,
-  totalDownTimePercentege,
-  totalUpTimeLabel,
-  totalDownTimeLabel,
+  overallPercent,
+  fillPercent,
+  fillPercentLabel,
 }: {
-  totalUpTimePercentege: number;
-  totalDownTimePercentege: number;
-  totalUpTimeLabel: string;
-  totalDownTimeLabel: string;
+  overallPercent: number;
+  fillPercent: number;
+  fillPercentLabel: string;
 }) => (
   <div className="d-flex h-100 align-items-center">
-    {totalUpTimePercentege + totalDownTimePercentege > 0 ? (
+    {overallPercent + fillPercent > 0 ? (
       <div className="progress progress-sm w-100 my-2">
         <div
           className="progress-bar bg-success"
           data-testid="progresUpTimeBar"
-          id={totalUpTimeLabel + totalUpTimePercentege.toString()}
-          style={{ width: totalUpTimePercentege + '%' }}
+          id={overallPercent.toString()}
+          style={{ width: overallPercent + '%' }}
         />
 
         <OverlayTrigger
           placement="top"
           delay={{ show: 0, hide: 400 }}
           overlay={(props: any) => (
-            <Tooltip id={totalDownTimeLabel} {...props} show={props.show.toString()}>
-              {totalDownTimeLabel}
+            <Tooltip id={fillPercentLabel} {...props} show={props.show.toString()}>
+              {fillPercentLabel}
             </Tooltip>
           )}
         >
           <div
             className="progress-bar bg-danger"
             data-testid="progresDownTimeBar"
-            id={totalDownTimeLabel + totalDownTimePercentege.toString()}
-            style={{ width: totalDownTimePercentege + '%' }}
+            id={fillPercentLabel + fillPercent.toString()}
+            style={{ width: fillPercent + '%' }}
           />
         </OverlayTrigger>
       </div>
