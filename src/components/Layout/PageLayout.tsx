5b4c56a33ebb6f249829254505955a920bcf5960
Dan Popa <popa.ioan.dan@gmail.com>
2021-04-06 13:27:49 +0300

    Fixes devnet urls matching bug

--- src/components/Layout/PageLayout.tsx
+++ src/components/Layout/PageLayout.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import { useMatchPath } from 'helpers';
+import { useMatchPath, useNetworkRoute } from 'helpers';
 import { validatorsRoutes, accountRoutes, providerRoutes } from 'routes';
 import NodesLayout from 'components/Nodes/NodesLayout';
 import AccountLayout from 'components/AccountDetails/AccountLayout';
 import ProviderLayout from 'components/ProviderDetails/ProviderLayout';
 
 export default function PageLayout({ children }: { children: React.ReactNode }) {
+  const networkRoute = useNetworkRoute();
   const matchPath = useMatchPath();
 
   switch (true) {
-    case matchPath(validatorsRoutes.index) !== null:
-    case matchPath(validatorsRoutes.nodes) !== null:
-    case matchPath(validatorsRoutes.providers) !== null:
+    case matchPath(networkRoute(validatorsRoutes.index)) !== null:
+    case matchPath(networkRoute(validatorsRoutes.nodes)) !== null:
+    case matchPath(networkRoute(validatorsRoutes.providers)) !== null:
       return <NodesLayout>{children}</NodesLayout>;
 
-    case matchPath(accountRoutes.index) !== null:
-    case matchPath(accountRoutes.code) !== null:
-    case matchPath(accountRoutes.tokens) !== null:
-    case matchPath(accountRoutes.oldAccountDetails) !== null:
+    case matchPath(networkRoute(accountRoutes.index)) !== null:
+    case matchPath(networkRoute(accountRoutes.code)) !== null:
+    case matchPath(networkRoute(accountRoutes.tokens)) !== null:
+    case matchPath(networkRoute(accountRoutes.oldAccountDetails)) !== null:
       return <AccountLayout>{children}</AccountLayout>;
 
-    case matchPath(providerRoutes.index) !== null:
-    case matchPath(providerRoutes.transactions) !== null:
+    case matchPath(networkRoute(providerRoutes.index)) !== null:
+    case matchPath(networkRoute(providerRoutes.transactions)) !== null:
       return <ProviderLayout>{children}</ProviderLayout>;
 
     default:
