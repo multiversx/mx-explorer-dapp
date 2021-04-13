b448448beaa0d55803ff36c13807833ec5406fb7
Dan Popa <popa.ioan.dan@gmail.com>
2021-04-13 10:57:01 +0300

    Identity Avatar does not deform now

--- src/sharedComponents/SharedIdentity/IdentityAvatar/index.tsx
+++ src/sharedComponents/SharedIdentity/IdentityAvatar/index.tsx
@@ -9,7 +9,9 @@ interface IdentityAvatarType {
 const IdentityAvatar = ({ identity }: { identity: IdentityAvatarType }) => {
   return (
     <img
-      className={`identity-avatar rounded-circle mr-2 ${!identity.avatar ? 'p-1 bg-light' : ''}`}
+      className={`identity-avatar rounded-circle flex-shrink-0 mr-2 ${
+        !identity.avatar ? 'p-1 bg-light' : ''
+      }`}
       src={identity.avatar ? identity.avatar : require('../../../assets/images/default-avatar.svg')}
       alt="img"
       height="42"
