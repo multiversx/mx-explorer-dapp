ce2d3144ae23b775685472739f3203c44da244ea
Tudor Morar <tudor.morar@elrond.com>
2021-08-05 15:08:34 +0300

    Work on modal link

--- src/components/TransactionDetails/TransactionInfo/DataField/index.tsx
+++ src/components/TransactionDetails/TransactionInfo/DataField/index.tsx
@@ -1,15 +1,44 @@
 import * as React from 'react';
 import { Anchorme, LinkComponentProps } from 'react-anchorme';
+import { Modal } from 'react-bootstrap';
 import { DetailItem } from 'sharedComponents';
 import useScamDetect from './helpres/useScamDetect';
 
-const CustomLink = (props: LinkComponentProps) => {
+// http://localhost:3000/transactions/e6d98b3ed9cfbe50651ea355e11440e40b910df763edba3489392128d088c531
+
+const ModalLink = (props: LinkComponentProps) => {
+  const [show, setShow] = React.useState(false);
+
   const onClick = (e: React.MouseEvent) => {
     e.preventDefault();
-    console.log('click');
+    console.log(props);
+
+    setShow(true);
   };
 
-  return <a {...props} onClick={onClick} />;
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  return (
+    <>
+      <a {...props} onClick={onClick} />
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <button className="btn btn-default" onClick={handleClose}>
+            Close
+          </button>
+          <a href="/#" className="btn">
+            Leave site
+          </a>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
 
 const DataField = ({ data }: { data?: string }) => {
@@ -28,7 +57,7 @@ const DataField = ({ data }: { data?: string }) => {
       {showData ? (
         <>
           <div className="textarea form-control col cursor-text mt-1">
-            <Anchorme linkComponent={CustomLink} target="_blank" rel="noreferrer noopener">
+            <Anchorme linkComponent={ModalLink} target="_blank" rel="noreferrer noopener">
               {dataString}
             </Anchorme>
           </div>
