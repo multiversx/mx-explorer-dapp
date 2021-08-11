53677d6dafe908be44d2e5173617e672a32c0cad
Tudor Morar <tudor.morar@elrond.com>
2021-08-11 15:30:49 +0300

    Update scamDetect

--- src/components/TransactionDetails/TransactionInfo/DataField/helpres/tests/scamDetect.test.ts
+++ src/components/TransactionDetails/TransactionInfo/DataField/helpres/tests/scamDetect.test.ts
@@ -1,25 +1,33 @@
 import { scamDetect } from '../useScamDetect';
 
 describe('scamDetect tests', () => {
-  const blacklist = ['lottery', 'l0ttery', '🅻🅾🆃🆃🅴🆁🆈'];
-  const output = '[Message hidden due to suspicious content]';
-  const strings: { [key: string]: string } = {
-    '👉 link.com': '👉 http://link.com',
-    'first-link.com or 🎉 second-link.com 🎉':
-      'http://first-link.com or 🎉 http://second-link.com 🎉',
-    'http://google.com 🎉': 'http://google.com 🎉',
-    '👉 https://linkedin.com 🎉': '👉 https://linkedin.com 🎉',
-    'http://google.com?asd=true': 'http://google.com?asd=true',
-    'http://www1.google.com': 'http://www1.google.com',
-    'http://www.google.ceva.com': 'http://www.google.ceva.com',
-    '[...] 🅻🅾🆃🆃🅴🆁🆈': '[...] 🅻🅾🆃🆃🅴🆁🆈',
+  const blacklist = {
+    lottery: 'Scam report',
+    l0ttery: 'Scam report',
+    '🅻🅾🆃🆃🅴🆁🆈': 'Lottery scam report',
+    won: 'Scam report',
+    '🇼‌🇴‌🇳‌': 'Scam report',
+    '🇱‌🇴‌͏🇹‌͏🇹‌͏🇪‌͏🇷‌͏🇾‌͏': 'Scam report',
+    ʟᴏᴛᴛᴇʀʏ: 'Scam report',
+  };
+  const output = '[Message hidden due to suspicious content';
+  const strings: { [key: string]: string[] } = {
+    '👉 link.com': ['👉 link.com', ''],
+    'first-link.com or 🎉 second-link.com 🎉': ['first-link.com or 🎉 second-link.com 🎉', ''],
+    'http://google.com 🎉': ['http://google.com 🎉', ''],
+    '👉 https://linkedin.com 🎉': ['👉 https://linkedin.com 🎉', ''],
+    'http://google.com?asd=true': ['http://google.com?asd=true', ''],
+    'http://www1.google.com': ['http://www1.google.com', ''],
+    'http://www.google.ceva.com': ['http://www.google.ceva.com', ''],
+    'access: 👉 www.lottery-elrond.com': ['access: 👉 www.lottery-elrond.com', ' - Scam report'],
+    '[...] 🅻🅾🆃🆃🅴🆁🆈': ['[...] 🅻🅾🆃🆃🅴🆁🆈', ' - Lottery scam report'],
   };
   for (let i = 0; i < Object.keys(strings).length; i++) {
     const input = Object.keys(strings)[i];
-    const msg = strings[input];
+    const [msg, reason] = strings[input];
     test(`anonymize ${input} -> ${msg}`, () => {
       const { output: result, stringWithLinks } = scamDetect(blacklist)(input);
-      expect(result).toEqual(output);
+      expect(result).toEqual(output + reason + ']');
       expect(stringWithLinks).toEqual(msg);
     });
   }
