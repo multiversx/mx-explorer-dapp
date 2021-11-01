e9d2e84a46d800f4ded0abae9cfdc650270f8bcf
Radu Mojic <radu.mojic@elrond.com>
2021-11-01 18:57:13 +0200

    updated nft preview

--- src/components/NftDetails/NftPreview/index.tsx
+++ src/components/NftDetails/NftPreview/index.tsx
@@ -4,9 +4,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinnerThird } from '@fortawesome/pro-regular-svg-icons/faSpinnerThird';
 import { types } from 'helpers';
 
-const NftPreview = ({ token }: { token: types.NftType }) => {
+const Thumbnail = ({ token, children }: { token: types.NftType; children: any }) => {
   const [loaded, setLoaded] = React.useState(false);
+  return token.thumbnailUrl ? (
+    <OverlayTrigger
+      placement="top"
+      delay={{ show: 250, hide: 400 }}
+      onExited={() => {
+        setLoaded(false);
+      }}
+      overlay={(props) => (
+        <Tooltip id="nft-tooltip" {...props}>
+          {!loaded && (
+            <FontAwesomeIcon
+              icon={faSpinnerThird}
+              size="2x"
+              className="text-white fa-spin fast-spin"
+            />
+          )}
+          <img
+            src={token.thumbnailUrl}
+            alt={token.name}
+            height={90}
+            onLoad={() => {
+              setLoaded(true);
+            }}
+          />
+        </Tooltip>
+      )}
+    >
+      {children}
+    </OverlayTrigger>
+  ) : (
+    <>{children}</>
+  );
+};
 
+const NftPreview = ({ token }: { token: types.NftType }) => {
   return token.uris ? (
     <div className="nft-details d-flex flex-column text-left">
       <ul className="list-unstyled mb-0">
@@ -15,44 +49,16 @@ const NftPreview = ({ token }: { token: types.NftType }) => {
             const link = Buffer.from(String(uri), 'base64').toString();
             return (
               <li key={i}>
-                <OverlayTrigger
-                  placement="top"
-                  delay={{ show: 250, hide: 400 }}
-                  onExited={() => {
-                    setLoaded(false);
-                  }}
-                  overlay={(props) => (
-                    <Tooltip id="nft-tooltip" {...props}>
-                      {!loaded && (
-                        <FontAwesomeIcon
-                          icon={faSpinnerThird}
-                          size="2x"
-                          className="text-white fa-spin fast-spin"
-                        />
-                      )}
-
-                      <img
-                        src={link.replace(
-                          'https://ipfs.io/ipfs',
-                          'http://media.elrond.com/nfts/asset'
-                        )}
-                        alt="img"
-                        height={90}
-                        onLoad={() => {
-                          setLoaded(true);
-                        }}
-                      />
-                    </Tooltip>
-                  )}
-                >
+                <Thumbnail token={token}>
                   <a
                     href={link}
                     {...{ target: '_blank' }}
                     data-testid={`${token.identifier}-link-${i}`}
+                    className="text-break"
                   >
                     {link}
                   </a>
-                </OverlayTrigger>
+                </Thumbnail>
               </li>
             );
           } else return null;
