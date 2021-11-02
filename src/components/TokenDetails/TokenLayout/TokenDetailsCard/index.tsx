540b161bd23c3c89021edb8429956ad31a8f9cf5
Radu Mojic <radu.mojic@elrond.com>
2021-11-02 20:28:48 +0200

    removed tx details from transaction list

--- src/components/TokenDetails/TokenLayout/TokenDetailsCard/index.tsx
+++ src/components/TokenDetails/TokenLayout/TokenDetailsCard/index.tsx
@@ -23,8 +23,8 @@ const SmallDetailItem = ({
   title: string | React.ReactNode;
 }) => (
   <div className="row py-3 border-bottom detail-item">
-    <div className="col-lg-3 text-secondary pl-lg-spacer">{title}</div>
-    <div className="col pr-lg-spacer">{children}</div>
+    <div className="col-lg-3 text-secondary pl-lg-spacer pr-lg-0">{title}</div>
+    <div className="col pr-lg-spacer pl-lg-0">{children}</div>
   </div>
 );
 
