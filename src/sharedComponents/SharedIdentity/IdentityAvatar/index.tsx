77685578bf350b710a2c2b33ad192444deccecbd
Radu Mojic <radu.mojic@elrond.com>
2021-11-05 20:24:51 +0200

    fixed identity logos

--- src/sharedComponents/SharedIdentity/IdentityAvatar/index.tsx
+++ src/sharedComponents/SharedIdentity/IdentityAvatar/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-
+import { ReactComponent as DefaultAvatar } from 'assets/images/default-avatar.svg';
 interface IdentityAvatarType {
   name?: string;
   avatar?: string;
@@ -8,14 +8,23 @@ interface IdentityAvatarType {
 
 const IdentityAvatar = ({ identity }: { identity: IdentityAvatarType }) => {
   return (
-    <img
-      className={`identity-avatar rounded-circle flex-shrink-0 mr-2 ${
-        !identity.avatar ? 'border-0' : ''
-      }`}
-      src={identity.avatar ? identity.avatar : require('../../../assets/images/default-avatar.svg')}
-      alt="img"
-      height="42"
-    />
+    <>
+      {identity.avatar ? (
+        <img
+          className={`identity-avatar rounded-circle flex-shrink-0 mr-2 ${
+            !identity.avatar ? 'border-0' : ''
+          }`}
+          src={identity.avatar}
+          alt="img"
+          height="42"
+        />
+      ) : (
+        <DefaultAvatar
+          className="identity-avatar border-0 flex-shrink-0 mr-2"
+          style={{ width: '42px', height: '42px' }}
+        />
+      )}
+    </>
   );
 };
 
