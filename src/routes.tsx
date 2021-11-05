0193d1be5a443864c3c54f46c02d1a46edfa363d
Radu Mojic <radu.mojic@elrond.com>
2021-11-05 19:33:58 +0200

    added sc results

--- src/routes.tsx
+++ src/routes.tsx
@@ -26,6 +26,7 @@ import ProviderDetails from 'components/ProviderDetails';
 import AccountContractCode from './components/AccountDetails/AccountContractCode';
 import AccountTokens from 'components/AccountDetails/AccountTokens';
 import AccountNfts from 'components/AccountDetails/AccountNfts';
+import AccountScResults from 'components/AccountDetails/AccountScResults';
 import ProviderTransactions from 'components/ProviderDetails/ProviderTransactions';
 import NodesStatistics from 'components/NodesStatistics';
 import NodesQueue from 'components/NodesQueue';
@@ -61,6 +62,7 @@ export const accountsRoutes = {
   accountCode: `/accounts/:hash/code`,
   accountTokens: `/accounts/:hash/tokens`,
   accountNfts: `/accounts/:hash/nfts`,
+  accountScResults: `/accounts/:hash/sc-results`,
   oldAccountDetails: `/address/:hash`,
 };
 
@@ -192,6 +194,11 @@ const routes: RouteType[] = [
     path: accountsRoutes.accountNfts,
     title: 'Account Details',
     component: AccountNfts,
+  },
+  {
+    path: accountsRoutes.accountScResults,
+    title: 'Account Details',
+    component: AccountScResults,
   },
   {
     path: tokensRoutes.tokens,
