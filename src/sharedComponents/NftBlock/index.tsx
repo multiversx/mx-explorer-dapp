e0d624174f7570e0e549d76bf066ff0d4cd586cb
Radu Mojic <radu.mojic@elrond.com>
2021-12-08 17:13:01 +0200

    showLastNonZeroDecimal on token operations

--- src/sharedComponents/NftBlock/index.tsx
+++ src/sharedComponents/NftBlock/index.tsx
@@ -24,6 +24,7 @@ const NftBlock = ({ value, operationToken }: NftBlockType) => {
                     value={value}
                     showLabel={false}
                     denomination={operationToken.decimals}
+                    showLastNonZeroDecimal={true}
                   />
                 ) : (
                   Number(value).toLocaleString('en')
