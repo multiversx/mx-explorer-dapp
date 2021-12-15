9c7ed9bb514d405320cb0e6d9671ee08725cba8f
Radu Mojic <radu.mojic@elrond.com>
2021-12-15 17:56:36 +0200

    updated tests

--- src/sharedComponents/TransactionDescription/tests/unwrappers.test.tsx
+++ src/sharedComponents/TransactionDescription/tests/unwrappers.test.tsx
@@ -194,4 +194,86 @@ describe('Tx Description unwrapper tests', () => {
     };
     expect(unwrapper(action)).toEqual(['Reinvest rewards', action.arguments.token]);
   });
+  test('Swap tokens', () => {
+    const { action } = {
+      action: {
+        category: 'mex',
+        name: TxActionsEnum.swap,
+        description: 'Swap 1 WEGLD for 281066.754391919467235791 MEX',
+        arguments: {
+          token1: {
+            name: 'WrappedEGLD',
+            collection: 'WEGLD-bd4d79',
+            identifier: 'WEGLD-bd4d79',
+            ticker: 'WEGLD',
+            value: '1000000000000000000',
+          },
+          token2: {
+            name: 'MEX',
+            collection: 'MEX-455c57',
+            identifier: 'MEX-455c57',
+            ticker: 'MEX',
+            value: '281066754391919467235791',
+          },
+        },
+      },
+    };
+    expect(unwrapper(action)).toEqual([
+      'Swap',
+      action.arguments.token1,
+      'for',
+      action.arguments.token2,
+    ]);
+  });
+  test('Add liquidity', () => {
+    const { action } = {
+      action: {
+        category: 'mex',
+        name: TxActionsEnum.addLiquidity,
+        description:
+          'Added liquidity for 0.309893556225986569 WEGLD and 88246.97734239037166522 MEX',
+        arguments: {
+          token1: {
+            name: 'WrappedEGLD',
+            collection: 'WEGLD-bd4d79',
+            identifier: 'WEGLD-bd4d79',
+            ticker: 'WEGLD',
+            value: '309893556225986569',
+          },
+          token2: {
+            name: 'MEX',
+            collection: 'MEX-455c57',
+            identifier: 'MEX-455c57',
+            ticker: 'MEX',
+            value: '88246977342390371665220',
+          },
+        },
+      },
+    };
+    expect(unwrapper(action)).toEqual([
+      'Added liquidity for',
+      action.arguments.token1,
+      'and',
+      action.arguments.token2,
+    ]);
+  });
+  test('Remove liquidity', () => {
+    const { action } = {
+      action: {
+        category: 'mex',
+        name: TxActionsEnum.removeLiquidity,
+        description: 'Removed liquidity 0.297 EGLDMEX',
+        arguments: {
+          token: {
+            name: 'EGLDMEXLP',
+            collection: 'EGLDMEX-0be9e5',
+            identifier: 'EGLDMEX-0be9e5',
+            ticker: 'EGLDMEX',
+            value: '297000000000000000',
+          },
+        },
+      },
+    };
+    expect(unwrapper(action)).toEqual(['Removed liquidity', action.arguments.token]);
+  });
 });
