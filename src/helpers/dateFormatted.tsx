6ac77b8215752c6dda7323db3c66ed7d2a9c9a33
Radu Mojic <radu.mojic@elrond.com>
2022-01-18 12:32:04 +0200

    show the UTC times for dates

--- src/helpers/dateFormatted.tsx
+++ src/helpers/dateFormatted.tsx
@@ -1,5 +1,8 @@
 import moment from 'moment';
 
-export default function dateFormatted(value: number, noSeconds?: boolean) {
+export default function dateFormatted(value: number, noSeconds?: boolean, utc?: boolean) {
+  if (utc) {
+    return moment.utc(value * 1000).format(`MMM DD, YYYY HH:mm${noSeconds ? '' : ':ss'} A UTC`);
+  }
   return moment(value * 1000).format(`MMM DD, YYYY HH:mm${noSeconds ? '' : ':ss'} A`);
 }
