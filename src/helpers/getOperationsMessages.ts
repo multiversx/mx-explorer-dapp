6eadbd7124dbc1f4ab139ea89df003093670dc14
Radu Mojic <radu.mojic@elrond.com>
2022-02-25 15:01:19 +0200

    use maps

--- src/helpers/getOperationsMessages.ts
+++ src/helpers/getOperationsMessages.ts
@@ -1,11 +1,10 @@
 import { TransactionType } from 'helpers/types';
 
 export default function getOperationsMessages(transaction: TransactionType) {
-  if (transaction.operations) {
-    const messages = transaction.operations.map((result) => result.message);
+  const messages =
+    transaction?.operations
+      ?.map((result) => result.message)
+      .filter((messages): messages is string => Boolean(messages)) ?? [];
 
-    return messages.filter((messages): messages is string => Boolean(messages));
-  }
-
-  return [];
+  return messages;
 }
