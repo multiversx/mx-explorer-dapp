21b8ea0f0193ecc07765d81ac7deef38c0f4598e
Radu Mojic <radu.mojic@elrond.com>
2022-04-06 17:17:44 +0300

    [*] use accountDetails.txCount + accountDetails.scrCount for pagination total for /transfers

--- src/utils/rawData/account.ts
+++ src/utils/rawData/account.ts
@@ -2,5 +2,6 @@ export default {
   address: 'erd1wh9c0sjr2xn8hzf02lwwcr4jk2s84tat9ud2kaq6zr7xzpvl9l5q8awmex',
   nonce: 15,
   balance: '62803693501081701872',
-  txCount: 1933
+  txCount: 1933,
+  scrCount: 0,
 };
