58408bdb278a1860c9a13eb4f46984d9c61f23e8
Radu Mojic <radu.mojic@elrond.com>
2022-07-17 19:34:28 +0300

    [+] moved tokens table to separate component

--- src/components/Tokens/index.tsx
+++ src/components/Tokens/index.tsx
@@ -1,23 +1,14 @@
 import * as React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDiamond } from '@fortawesome/pro-regular-svg-icons/faDiamond';
 import { useLocation } from 'react-router-dom';
-import BigNumber from 'bignumber.js';
 
-import { Loader, adapter, NetworkLink, Denominate, Pager } from 'sharedComponents';
+import { Loader, adapter, NetworkLink, Pager } from 'sharedComponents';
 import NoTokens from './NoTokens';
 import FailedTokens from './FailedTokens';
-import {
-  urlBuilder,
-  useFilters,
-  useURLSearchParams,
-  types,
-  useActiveRoute,
-  amountWithoutRounding,
-} from 'helpers';
+import { useFilters, useURLSearchParams, types, useActiveRoute } from 'helpers';
 import Filters from './Filters';
 
 import { tokensRoutes } from 'routes';
+import TokensTable from './TokensTable';
 
 const Tokens = () => {
   const ref = React.useRef(null);
@@ -107,100 +98,7 @@ const Tokens = () => {
                   {tokens && tokens.length > 0 ? (
                     <>
                       <div className="card-body border-0 p-0">
-                        <div className="table-wrapper">
-                          <table className="table">
-                            <thead>
-                              <tr>
-                                <th>Token</th>
-                                <th>Name</th>
-                                <th>Holders</th>
-                                <th>Transactions</th>
-                                <th>Price</th>
-                                <th>Circulating Supply</th>
-                                <th>Market Cap</th>
-                              </tr>
-                            </thead>
-                            <tbody data-testid="tokensTable">
-                              {tokens.map((token, i) => (
-                                <tr key={token.identifier}>
-                                  <td>
-                                    <div className="token-identity d-flex flex-row">
-                                      <div className="d-flex align-items-center mr-3">
-                                        <NetworkLink
-                                          to={urlBuilder.tokenDetails(token.identifier)}
-                                          data-testid={`tokensLink${i}`}
-                                          className="token-link"
-                                        >
-                                          {token.assets && token.assets.svgUrl ? (
-                                            <img
-                                              src={token.assets.svgUrl}
-                                              alt={token.name}
-                                              className="token-icon"
-                                            />
-                                          ) : (
-                                            <div className="bg-light token-icon d-flex align-items-center justify-content-center">
-                                              <FontAwesomeIcon icon={faDiamond} />
-                                            </div>
-                                          )}
-                                        </NetworkLink>
-                                      </div>
-                                      <div className="d-flex flex-column justify-content-center">
-                                        <NetworkLink
-                                          to={urlBuilder.tokenDetails(token.identifier)}
-                                          data-testid={`tokensLink${i}`}
-                                          className="d-block token-ticker"
-                                        >
-                                          {token.ticker}
-                                        </NetworkLink>
-                                        {token.assets && token.assets.description && (
-                                          <div
-                                            className="token-description text-wrap text-secondary small d-none d-md-block"
-                                            title={token.assets.description}
-                                          >
-                                            {token.assets.description}
-                                          </div>
-                                        )}
-                                      </div>
-                                    </div>
-                                  </td>
-                                  <td>{token.name}</td>
-                                  <td>
-                                    {token.accounts ? new BigNumber(token.accounts).toFormat() : 0}
-                                  </td>
-                                  <td>
-                                    {token.transactions
-                                      ? new BigNumber(token.transactions).toFormat()
-                                      : 0}
-                                  </td>
-                                  <td>
-                                    {token.price && (
-                                      <>${amountWithoutRounding(token.price.toString())}</>
-                                    )}
-                                  </td>
-                                  <td>
-                                    {token.circulatingSupply && (
-                                      <Denominate
-                                        showLabel={false}
-                                        value={
-                                          token.circulatingSupply
-                                            ? String(token.circulatingSupply)
-                                            : '0'
-                                        }
-                                        denomination={token.decimals}
-                                        decimals={0}
-                                      />
-                                    )}
-                                  </td>
-                                  <td>
-                                    {token.marketCap && (
-                                      <>${amountWithoutRounding(token.marketCap.toString())}</>
-                                    )}
-                                  </td>
-                                </tr>
-                              ))}
-                            </tbody>
-                          </table>
-                        </div>
+                        <TokensTable tokens={tokens} />
                       </div>
 
                       <div className="card-footer d-flex justify-content-end">
