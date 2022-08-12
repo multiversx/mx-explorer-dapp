2712724567111eecdd3c24b5d8f8d844527e1c06
Radu Mojic <radu.mojic@elrond.com>
2022-08-12 19:00:51 +0300

    [+] added publicKey and stats to node useEffect deps

--- src/components/NodeDetails/index.tsx
+++ src/components/NodeDetails/index.tsx
@@ -23,180 +23,202 @@ const initialState = {
 };
 
 const NodeDetails = () => {
-  const ref = React.useRef(null);
-  const { stats } = useGlobalState();
-  const { hash: publicKey } = useParams() as any;
-  const { search } = useLocation();
-  const { getNode, getIdentity, getRounds, getBlocks } = adapter();
-  const isMainnet = useIsMainnet();
+                            const ref = React.useRef(null);
+                            const { stats } = useGlobalState();
+                            const { hash: publicKey } = useParams() as any;
+                            const { search } = useLocation();
+                            const { getNode, getIdentity, getRounds, getBlocks } = adapter();
+                            const isMainnet = useIsMainnet();
 
-  const [dataReady, setDataReady] = React.useState<boolean | undefined>(true);
-  const [node, setNode] = React.useState<NodeDetailType<NodeType>>(initialState);
-  const [identity, setIdentity] = React.useState<NodeDetailType<IdentityType>>(initialState);
-  const [rounds, setRounds] = React.useState<NodeDetailType<RoundType[]>>(initialState);
-  const [blocks, setBlocks] = React.useState<NodeDetailType<BlockType[]>>(initialState);
+                            const [dataReady, setDataReady] = React.useState<boolean | undefined>(
+                              true
+                            );
+                            const [node, setNode] = React.useState<NodeDetailType<NodeType>>(
+                              initialState
+                            );
+                            const [identity, setIdentity] = React.useState<
+                              NodeDetailType<IdentityType>
+                            >(initialState);
+                            const [rounds, setRounds] = React.useState<NodeDetailType<RoundType[]>>(
+                              initialState
+                            );
+                            const [blocks, setBlocks] = React.useState<NodeDetailType<BlockType[]>>(
+                              initialState
+                            );
 
-  const fetchNodes = () => {
-    setDataReady(undefined);
-    getNode(publicKey).then((nodeData) => {
-      if (ref.current !== null) {
-        if (nodeData.success) {
-          const fetchIdentity = isMainnet && nodeData.data.identity !== undefined;
-          const hasExtendedInfo =
-            nodeData.data.type !== 'observer' && nodeData.data.status !== 'queued';
+                            const fetchNodes = () => {
+                              setDataReady(undefined);
+                              getNode(publicKey).then((nodeData) => {
+                                if (ref.current !== null) {
+                                  if (nodeData.success) {
+                                    const fetchIdentity =
+                                      isMainnet && nodeData.data.identity !== undefined;
+                                    const hasExtendedInfo =
+                                      nodeData.data.type !== 'observer' &&
+                                      nodeData.data.status !== 'queued';
 
-          const epoch = stats.epoch ?? undefined;
-          const shard = nodeData.data.shard;
+                                    const epoch = stats.epoch ?? undefined;
+                                    const shard = nodeData.data.shard;
 
-          if (hasExtendedInfo) {
-            const promises = [
-              getRounds({ validator: publicKey, shard, epoch }),
-              getBlocks({ proposer: publicKey, shard, epoch }),
-              ...(fetchIdentity ? [getIdentity(nodeData.data.identity)] : []),
-            ];
-            Promise.all(promises).then((response) => {
-              const [roundsData, blocksData, identityData] = response;
-              if (ref.current !== null) {
-                setNode(nodeData);
+                                    if (hasExtendedInfo) {
+                                      const promises = [
+                                        getRounds({ validator: publicKey, shard, epoch }),
+                                        getBlocks({ proposer: publicKey, shard, epoch }),
+                                        ...(fetchIdentity
+                                          ? [getIdentity(nodeData.data.identity)]
+                                          : []),
+                                      ];
+                                      Promise.all(promises).then((response) => {
+                                        const [roundsData, blocksData, identityData] = response;
+                                        if (ref.current !== null) {
+                                          setNode(nodeData);
 
-                setBlocks({
-                  data: blocksData.success ? blocksData.data.blocks : [],
-                  success: blocksData.success,
-                });
+                                          setBlocks({
+                                            data: blocksData.success ? blocksData.data.blocks : [],
+                                            success: blocksData.success,
+                                          });
 
-                setRounds({
-                  data: roundsData.success
-                    ? (roundsData as any).data.map((round: any) => ({
-                        key: round.round,
-                        value: round.blockWasProposed,
-                      }))
-                    : [],
-                  success: roundsData.success,
-                });
+                                          setRounds({
+                                            data: roundsData.success
+                                              ? (roundsData as any).data.map((round: any) => ({
+                                                  key: round.round,
+                                                  value: round.blockWasProposed,
+                                                }))
+                                              : [],
+                                            success: roundsData.success,
+                                          });
 
-                if (isMainnet && identityData) {
-                  setIdentity(identityData);
-                }
+                                          if (isMainnet && identityData) {
+                                            setIdentity(identityData);
+                                          }
 
-                setDataReady(nodeData.success);
-              }
-            });
-          } else {
-            if (isMainnet && nodeData.data.identity !== undefined) {
-              getIdentity(nodeData.data.identity).then((identityData) => {
-                if (ref.current !== null) {
-                  if (isMainnet && identityData) {
-                    setIdentity(identityData);
-                    setNode(nodeData);
-                    setDataReady(true);
-                  }
-                }
-              });
-            } else {
-              setNode(nodeData);
-              setDataReady(true);
-            }
-          }
-        } else {
-          setDataReady(false);
-        }
-      }
-    });
-  };
+                                          setDataReady(nodeData.success);
+                                        }
+                                      });
+                                    } else {
+                                      if (isMainnet && nodeData.data.identity !== undefined) {
+                                        getIdentity(nodeData.data.identity).then((identityData) => {
+                                          if (ref.current !== null) {
+                                            if (isMainnet && identityData) {
+                                              setIdentity(identityData);
+                                              setNode(nodeData);
+                                              setDataReady(true);
+                                            }
+                                          }
+                                        });
+                                      } else {
+                                        setNode(nodeData);
+                                        setDataReady(true);
+                                      }
+                                    }
+                                  } else {
+                                    setDataReady(false);
+                                  }
+                                }
+                              });
+                            };
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  React.useEffect(fetchNodes, [search]);
+                            // eslint-disable-next-line react-hooks/exhaustive-deps
+                            React.useEffect(fetchNodes, [search, publicKey, stats]);
 
-  const showIdentity =
-    identity.success === false || (identity.success && identity.data !== undefined);
+                            const showIdentity =
+                              identity.success === false ||
+                              (identity.success && identity.data !== undefined);
 
-  const showExtendedInfo =
-    node.data !== undefined && node.data.type !== 'observer' && node.data.status !== 'queued';
+                            const showExtendedInfo =
+                              node.data !== undefined &&
+                              node.data.type !== 'observer' &&
+                              node.data.status !== 'queued';
 
-  return (
-    <>
-      {dataReady === undefined && <Loader />}
-      {dataReady === false && (
-        <PageState
-          icon={faCogs}
-          title="Unable to locate this node"
-          className="py-spacer my-auto"
-          dataTestId="errorScreen"
-        />
-      )}
-      <div ref={ref}>
-        {dataReady === true && node.data !== undefined && (
-          <div className="container page-content">
-            {showIdentity && (
-              <div className="row">
-                <div className="col mb-spacer">
-                  <SharedIdentity.Summary identity={identity.data} />
-                </div>
-              </div>
-            )}
+                            return (
+                              <>
+                                {dataReady === undefined && <Loader />}
+                                {dataReady === false && (
+                                  <PageState
+                                    icon={faCogs}
+                                    title="Unable to locate this node"
+                                    className="py-spacer my-auto"
+                                    dataTestId="errorScreen"
+                                  />
+                                )}
+                                <div ref={ref}>
+                                  {dataReady === true && node.data !== undefined && (
+                                    <div className="container page-content">
+                                      {showIdentity && (
+                                        <div className="row">
+                                          <div className="col mb-spacer">
+                                            <SharedIdentity.Summary identity={identity.data} />
+                                          </div>
+                                        </div>
+                                      )}
 
-            <div className="row">
-              <div className="mb-spacer col">
-                <NodeInformation nodeData={node.data} />
-              </div>
-            </div>
+                                      <div className="row">
+                                        <div className="mb-spacer col">
+                                          <NodeInformation nodeData={node.data} />
+                                        </div>
+                                      </div>
 
-            {showExtendedInfo && (
-              <>
-                <div className="row">
-                  <div className="mb-spacer col">
-                    <ValidatorDetails nodeData={node.data} />
-                  </div>
-                </div>
+                                      {showExtendedInfo && (
+                                        <>
+                                          <div className="row">
+                                            <div className="mb-spacer col">
+                                              <ValidatorDetails nodeData={node.data} />
+                                            </div>
+                                          </div>
 
-                <div className="row">
-                  <div className="mb-spacer col-md-6">
-                    <NetworkMetrics node={node.data} />
-                  </div>
-                  <div className="col-md-6 mb-spacer">
-                    <Rounds rounds={rounds} node={node.data} />
-                  </div>
-                </div>
+                                          <div className="row">
+                                            <div className="mb-spacer col-md-6">
+                                              <NetworkMetrics node={node.data} />
+                                            </div>
+                                            <div className="col-md-6 mb-spacer">
+                                              <Rounds rounds={rounds} node={node.data} />
+                                            </div>
+                                          </div>
 
-                <div className="row">
-                  <div className="col-12">
-                    <div className="card">
-                      {blocks.success === false && <FailedBlocks />}
-                      {blocks.success && blocks.data && (
-                        <>
-                          {blocks.data.length === 0 && (
-                            <NoBlocks
-                              title={`${
-                                node.data.status === 'eligible'
-                                  ? 'No blocks'
-                                  : 'Validator not in consensus'
-                              }`}
-                            />
-                          )}
-                          {blocks.data.length > 0 && (
-                            <>
-                              <div className="card-header">
-                                <div className="card-header-item">
-                                  <h6 className="m-0">Latest Proposed Blocks</h6>
+                                          <div className="row">
+                                            <div className="col-12">
+                                              <div className="card">
+                                                {blocks.success === false && <FailedBlocks />}
+                                                {blocks.success && blocks.data && (
+                                                  <>
+                                                    {blocks.data.length === 0 && (
+                                                      <NoBlocks
+                                                        title={`${
+                                                          node.data.status === 'eligible'
+                                                            ? 'No blocks'
+                                                            : 'Validator not in consensus'
+                                                        }`}
+                                                      />
+                                                    )}
+                                                    {blocks.data.length > 0 && (
+                                                      <>
+                                                        <div className="card-header">
+                                                          <div className="card-header-item">
+                                                            <h6 className="m-0">
+                                                              Latest Proposed Blocks
+                                                            </h6>
+                                                          </div>
+                                                        </div>
+                                                        <div className="card-body p-0">
+                                                          <BlocksTable
+                                                            blocks={blocks.data}
+                                                            shard={undefined}
+                                                          />
+                                                        </div>
+                                                      </>
+                                                    )}
+                                                  </>
+                                                )}
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </>
+                                      )}
+                                    </div>
+                                  )}
                                 </div>
-                              </div>
-                              <div className="card-body p-0">
-                                <BlocksTable blocks={blocks.data} shard={undefined} />
-                              </div>
-                            </>
-                          )}
-                        </>
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </>
-            )}
-          </div>
-        )}
-      </div>
-    </>
-  );
-};
+                              </>
+                            );
+                          };
 
 export default NodeDetails;
