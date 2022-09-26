30c638e9845549e447c34d4de7aedf93ec9838f5
Radu Mojic <74196626+radumojic@users.noreply.github.com>
2022-09-26 17:42:33 +0300

    Staking & Analytics Pages (#343)
    [+] added the Account Analytics tab, show a chart for the last 100 entries from the /history endpoint
    [+] added the Account Staking tab, show a list of all staking options ( delegation / legacy delegation / staking ) and a donut chart with the percentage from total
    [+] replaced chart.js with the recharts library
    [+] updated the charts from the Home page with the new functionality
    [*] various fixes and improvements

---
+++ src/sharedComponents/Chart/ChartBar.tsx
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import moment from 'moment';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Cell, ResponsiveContainer } from 'recharts';
+
+import { ChartProps } from './helpers/types';
+import CustomTooltip from './helpers/CustomTooltip';
+import formatYAxis from './helpers/formatYAxis';
+import getChartMergedData from './helpers/getChartMergedData';
+import StartEndTick from './helpers/StartEndTick';
+
+const ChartBar = ({
+  config,
+  data,
+  dateFormat,
+  filter,
+  category,
+  currency,
+  size,
+  tooltip,
+  hasOnlyStartEndTick,
+}: ChartProps) => {
+  const [focusBar, setFocusBar] = useState<any>(null);
+
+  const chartData = getChartMergedData({ config, data, filter, category });
+
+  const docStyle = window.getComputedStyle(document.documentElement);
+  const primaryColor = docStyle.getPropertyValue('--primary');
+
+  return (
+    <div
+      className={`chart-bar mb-n3 ${size ? `chart-bar-${size}` : ''} ${
+        hasOnlyStartEndTick ? 'has-only-start-end-tick' : ''
+      }`}
+    >
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          data={chartData}
+          onMouseMove={(state) => {
+            if (state.isTooltipActive) {
+              setFocusBar(state.activeTooltipIndex);
+            } else {
+              setFocusBar(null);
+            }
+          }}
+        >
+          <defs>
+            <linearGradient id="transparent" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="100%" stopColor="transparent" stopOpacity={0} />
+            </linearGradient>
+            <linearGradient id="defaultGradient" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={primaryColor} stopOpacity={0.25} />
+              <stop offset="35%" stopColor={primaryColor} stopOpacity={0.4} />
+              <stop offset="95%" stopColor={primaryColor} stopOpacity={0} />
+            </linearGradient>
+            {config.map((chartConfig) => {
+              if (chartConfig.gradient) {
+                return (
+                  <linearGradient
+                    key={chartConfig.gradient}
+                    id={chartConfig.gradient}
+                    x1="0"
+                    y1="0"
+                    x2="0"
+                    y2="1"
+                  >
+                    <stop offset="5%" stopColor={`#${chartConfig.gradient}`} stopOpacity={0.25} />
+                    <stop offset="95%" stopColor={`#${chartConfig.gradient}`} stopOpacity={0} />
+                  </linearGradient>
+                );
+              }
+
+              return null;
+            })}
+          </defs>
+          <XAxis
+            minTickGap={40}
+            tickCount={10}
+            dataKey="timestamp"
+            tickLine={false}
+            tickFormatter={(tick) =>
+              moment
+                .unix(tick)
+                .utc()
+                .format(dateFormat ?? 'D MMM YYYY')
+            }
+            strokeWidth={0.3}
+            {...(hasOnlyStartEndTick ? { tick: <StartEndTick dateformat={dateFormat} /> } : {})}
+            {...(hasOnlyStartEndTick ? { interval: 0 } : {})}
+            {...(chartData.length > 3 ? { scale: 'time' } : {})}
+          />
+
+          <YAxis
+            orientation="right"
+            tickFormatter={(tick) => formatYAxis(tick, currency)}
+            axisLine={false}
+            tickLine={false}
+            tickCount={5}
+          />
+
+          {config.map((chartConfig) => (
+            <Bar
+              dataKey={chartConfig.id}
+              key={chartConfig.id}
+              {...(chartConfig.gradient
+                ? { fill: `url(#${chartConfig.gradient})` }
+                : { fill: 'url(#transparent)' })}
+              {...(chartConfig.stroke ? { stroke: chartConfig.stroke } : {})}
+              {...(chartConfig.fill ? { fill: chartConfig.fill } : {})}
+              {...(chartConfig.strokeDasharray
+                ? { strokeDasharray: chartConfig.strokeDasharray }
+                : {})}
+            >
+              {chartData.map((entry: any, index: number) => (
+                <Cell
+                  fill={
+                    focusBar === index
+                      ? primaryColor
+                      : chartConfig.fill
+                      ? chartConfig.fill
+                      : primaryColor
+                  }
+                  key={index}
+                />
+              ))}
+            </Bar>
+          ))}
+          <Tooltip
+            content={(props) => <CustomTooltip {...props} currency={currency} {...tooltip} />}
+            cursor={false}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default ChartBar;
---
+++ src/sharedComponents/Chart/helpers/getLastDayValue.ts
@@ -0,0 +1,15 @@
+import BigNumber from 'bignumber.js';
+import { ChartDataType } from 'sharedComponents/Chart/helpers/types';
+
+const getLastDayValue = (chartData: ChartDataType[]) => {
+  if (chartData.length >= 2) {
+    const lastDay = chartData[chartData.length - 1].value;
+    const penultimateDay = chartData[chartData.length - 2].value;
+
+    const day = new BigNumber(lastDay).minus(penultimateDay).toFormat(0);
+    return day;
+  }
+  return '...';
+};
+
+export default getLastDayValue;
