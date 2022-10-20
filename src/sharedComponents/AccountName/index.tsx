80877766a454a7f89c0056b3cf48330bff77a369
Radu Mojic <radu.mojic@elrond.com>
2022-10-20 16:09:17 +0300

    [+] style the herotag

--- src/sharedComponents/AccountName/index.tsx
+++ src/sharedComponents/AccountName/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { AssetType } from 'helpers/types';
 import { Trim, Overlay } from 'sharedComponents';
+import { faUserTag } from '@fortawesome/pro-regular-svg-icons/faUserTag';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const AccountName = ({
   address,
@@ -14,22 +16,34 @@ const AccountName = ({
   color?: 'muted' | 'secondary';
 }) => {
   if (assets && assets.name) {
-    const name = assets.name.replaceAll(/[^\p{L}\p{N}\p{P}\p{Z}\n]/gu, '');
-    const description = `${name} (${address})`;
+    const cleanName = assets.name.replaceAll(/[^\p{L}\p{N}\p{P}\p{Z}\n]/gu, '');
+    const name = cleanName.endsWith('.elrond')
+      ? `@${cleanName.replaceAll('.elrond', '')}`
+      : cleanName;
+    const description = `${cleanName} (${address})`;
 
     return (
-      <Overlay title={description} tooltipClassName={'account-name'}>
-        <div
-          className={`text-truncate ${color ? `text-${color}` : ''}`}
-          {...(dataTestId
-            ? {
-                datatestid: dataTestId,
-              }
-            : {})}
-        >
-          {name}
-        </div>
-      </Overlay>
+      <>
+        <>
+          {cleanName.endsWith('.elrond') && (
+            <Overlay title="Maiar Herotag" className="herotag" tooltipClassName="account-name">
+              <FontAwesomeIcon icon={faUserTag} className="mr-1 text-secondary" size="xs" />
+            </Overlay>
+          )}
+        </>
+        <Overlay title={description} tooltipClassName="account-name">
+          <div
+            className={`text-truncate ${color ? `text-${color}` : ''}`}
+            {...(dataTestId
+              ? {
+                  datatestid: dataTestId,
+                }
+              : {})}
+          >
+            {name}
+          </div>
+        </Overlay>
+      </>
     );
   }
 
