22984b727a7aa5b0700894907811b114d2465e5f
Radu Mojic <radu.mojic@elrond.com>
2023-01-07 16:34:37 +0200

    [+] update logo on devnet/testnet

--- src/components/Layout/Navbar/Logo.tsx
+++ src/components/Layout/Navbar/Logo.tsx
@@ -4,6 +4,7 @@ import { useGlobalState } from 'context';
 import { NetworkIdType } from 'helpers/types';
 import { ReactComponent as MultiversXLogo } from 'assets/images/logo-full.svg';
 import { ReactComponent as MultiversXLogoShort } from 'assets/images/logo-short.svg';
+import { ReactComponent as MultiversXSymbol } from 'assets/images/symbol.svg';
 
 const Logo = () => {
   const {
@@ -12,9 +13,17 @@ const Logo = () => {
 
   return (
     <Link to="/" className="d-flex align-items-center navbar-brand mr-2 pr-1">
-      <MultiversXLogo className="main-logo flex-shrink-0 d-none d-md-block" />
-      <MultiversXLogoShort className="main-logo-short flex-shrink-0 d-block d-md-none" />
-      {id !== NetworkIdType.mainnet && <span className="text-capitalize">{id}</span>}
+      {id !== NetworkIdType.mainnet ? (
+        <>
+          <MultiversXSymbol className="main-symbol flex-shrink-0" />
+          <span className="text-capitalize text-truncate">{id} Explorer</span>
+        </>
+      ) : (
+        <>
+          <MultiversXLogo className="main-logo flex-shrink-0 d-none d-md-block" />
+          <MultiversXLogoShort className="main-logo-short flex-shrink-0 d-block d-md-none" />
+        </>
+      )}
     </Link>
   );
 };
