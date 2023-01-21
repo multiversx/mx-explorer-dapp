15d0fa259fb431eeab458ffa7e0c846624ea8ba6
Radu Mojic <radu.mojic@elrond.com>
2023-01-21 12:43:08 +0200

    [+] moved /rawData into /__mocks__ for consistency

--- src/components/Denominate/tests/denominateComponent.test.tsx
+++ src/components/Denominate/tests/denominateComponent.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import config from 'utils/config';
+import config from '__mocks__/config';
 import Denominate from '../index';
 import { GlobalProvider } from 'context';
 import { render } from '@testing-library/react';
