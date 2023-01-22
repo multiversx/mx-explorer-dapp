3f38fc6d01c393f5f60fe42b9ba3283bea0f3b00
Radu Mojic <radu.mojic@elrond.com>
2023-01-23 00:25:02 +0200

    [+] updated configs

--- src/config/sharedConfig.ts
+++ src/config/sharedConfig.ts
@@ -1,18 +1,117 @@
-export const metaChainShardId = 4294967295;
-export const allShardsShardId = 4294967280;
-export const gasLimit = 50000;
-export const gasPrice = 1000000000;
-export const gasPerDataByte = 1500;
-export const refreshRate = 6000;
-export const nrOfShards = 3;
-export const versionNumber = 'v1.1.0.0';
-export const denomination = 18;
-export const decimals = 2;
-export const numInitCharactersForScAddress = 13;
-export const stakePerValidator = 2500000;
-export const extrasApi = '***REMOVED***';
-export const displayedDataLength: number = 1000000;
-export const decodeLimit: number = 60000;
-export const pageSize: number = 25;
+import {
+  DECIMALS,
+  DIGITS,
+  GAS_LIMIT,
+  GAS_PER_DATA_BYTE,
+  GAS_PRICE_MODIFIER,
+  GAS_PRICE as DEFAULT_GAS_PRICE,
+} from '@multiversx/sdk-dapp/constants/index';
+import { NetworkType as NetworkConfigType } from '@multiversx/sdk-dapp/types/network.types';
+import { object, string, boolean } from 'yup';
 
-export const multiversXNodesIdentity = 'multiversx';
+const GAS_PRICE = String(DEFAULT_GAS_PRICE);
+
+export { DECIMALS, DIGITS, GAS_LIMIT, GAS_PER_DATA_BYTE, GAS_PRICE_MODIFIER, GAS_PRICE };
+
+export const links: NetworkUrlType[] = [
+  {
+    id: 'mainnet',
+    name: 'Mainnet',
+    url: 'https://explorer.multiversx.com/',
+  },
+  {
+    id: 'testnet',
+    name: 'Testnet',
+    url: 'https://testnet-explorer.multiversx.com/',
+  },
+  {
+    id: 'devnet',
+    name: 'Devnet',
+    url: 'https://devnet-explorer.multiversx.com/',
+  },
+];
+
+export const allApps = (props: { id: string; name: string; url: string }[]) => [
+  {
+    id: 'main-site',
+    name: 'Main site',
+    url: 'https://multiversx.com/',
+  },
+  ...props,
+  {
+    id: 'xexchange',
+    name: 'xExchange',
+    url: 'https://xexchange.com/',
+  },
+  {
+    id: 'xlaunchpad',
+    name: 'xLaunchpad',
+    url: 'https://xlaunchpad.com/',
+  },
+  {
+    id: 'bridge',
+    name: 'Bridge',
+    url: 'https://ad-astra.multiversx.com/',
+  },
+  {
+    id: 'docs',
+    name: 'Docs',
+    url: 'https://docs.multiversx.com/',
+  },
+];
+
+export const networkBaseSchema = object({
+  default: boolean(),
+  id: string().defined().required(),
+  chainId: string().defined().required(),
+  egldLabel: string().defined().required(),
+  apiAddress: string().defined().required(),
+  name: string().defined().required(),
+  theme: string(),
+  walletAddress: string(),
+  explorerAddress: string(),
+  accessToken: boolean(),
+}).required();
+
+export const adapterSchema = object({
+  extrasApi: string().when('accessToken', {
+    is: true,
+    then: string().required(),
+  }),
+  adapter: string().defined().oneOf(['api', 'elastic']),
+  apiUrl: string().when('adapter', {
+    is: 'api',
+    then: string().required(),
+  }),
+  growthApi: string().when('adapter', {
+    is: 'api',
+    then: string().required(),
+  }),
+  elasticUrl: string().when('adapter', {
+    is: 'elastic',
+    then: string().required(),
+  }),
+  proxyUrl: string().when('adapter', {
+    is: 'elastic',
+    then: string().required(),
+  }),
+}).required();
+
+export const schema = networkBaseSchema.concat(adapterSchema);
+
+export interface NetworkType extends Partial<NetworkConfigType> {
+  adapter: 'api' | 'elastic';
+  theme?: string;
+  default?: boolean;
+  accessToken?: boolean;
+  extrasApi?: string;
+  growthApi?: string;
+  elasticUrl?: string;
+  proxyUrl?: string;
+}
+
+export interface NetworkUrlType {
+  id: string;
+  name: string;
+  url: string;
+}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,3 @@
-export const GAS_PRICE_MODIFIER = 0.01;
-export const GAS_PER_DATA_BYTE = 1_500;
-export const GAS_LIMIT = 50_000;
-export const GAS_PRICE = 1_000_000_000;
-export const DECIMALS = 18;
-export const DIGITS = 4;
-export const VERSION = 1;
-
 export const METACHAIN_SHARD_ID = 4294967295;
 export const ALL_SHARDS_SHARD_ID = 4294967280;
 
