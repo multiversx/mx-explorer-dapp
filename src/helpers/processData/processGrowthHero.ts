96291be1cfaa280160d2819aeb10fd0b27d8e368
Radu Mojic <radu.mojic@elrond.com>
2023-01-27 16:14:30 +0200

    [+] use data from growth

--- src/helpers/processData/processGrowthHero.ts
+++ src/helpers/processData/processGrowthHero.ts
@@ -4,8 +4,10 @@ import { GrowthHeroType } from 'types';
 export const processGrowthHero = (data: GrowthHeroType) => {
   return {
     totalTransactions: new BigNumber(data.totalTransactions).toFormat(0),
-    totalTransactionsToday: new BigNumber(data.totalTransactions).toFormat(0),
-    totalAccounts: new BigNumber(data.totalTransactions).toFormat(0),
-    activeAccountsToday: new BigNumber(data.totalTransactions).toFormat(0)
+    totalTransactionsToday: new BigNumber(data.totalTransactionsToday).toFormat(
+      0
+    ),
+    totalAccounts: new BigNumber(data.totalAccounts).toFormat(0),
+    activeAccountsToday: new BigNumber(data.activeAccountsToday).toFormat(0)
   };
 };
--- src/hooks/growth/index.ts
+++ src/hooks/growth/index.ts
@@ -1,1 +1,6 @@
+export * from './useFetchGrowthHero';
 export * from './useFetchGrowthMostUsed';
+export * from './useFetchGrowthPrice';
+export * from './useFetchGrowthSearch';
+export * from './useFetchGrowthStaking';
+export * from './useFetchGrowthTransactions';
