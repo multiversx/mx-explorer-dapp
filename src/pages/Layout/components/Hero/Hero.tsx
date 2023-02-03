de54fde1d2b1f3d714e08243b5e734bc51543de0
Radu Mojic <radu.mojic@elrond.com>
2023-02-03 15:46:52 +0200

    [+] move page hero logic on separate component, update layout structure

---
+++ src/pages/Layout/components/Hero/Hero.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+
+import { Search } from 'components';
+import { useActiveRoute, useIsMainnet, usePageStats } from 'hooks';
+import { ChartContractsTransactions } from 'pages/Home/ChartContractsTransactions';
+import { activeNetworkSelector, defaultNetworkSelector } from 'redux/selectors';
+import {
+  AccountsStatsCard,
+  BlockHeightStatsCard,
+  TransactionsStatsCard,
+  ValidatorsStatusCard,
+  HeroPills,
+  StatsCard
+} from 'widgets';
+
+import {
+  useShowGlobalStats,
+  useShowCustomStats,
+  useShowTransactionStats
+} from './hooks';
+import { getCustomPageName } from '../../helpers';
+
+export const Hero = () => {
+  const activeRoute = useActiveRoute();
+  const isMainnet = useIsMainnet();
+
+  const { pathname } = useLocation();
+
+  const { id: activeNetworkId } = useSelector(activeNetworkSelector);
+  const { id: defaultNetworkId } = useSelector(defaultNetworkSelector);
+
+  const { pageStats } = usePageStats();
+
+  const isHome = activeRoute('/');
+  const showGlobalStats = useShowGlobalStats();
+  const showCustomStats = useShowCustomStats();
+  const showTransactionsStats = useShowTransactionStats();
+
+  const pathArray = pathname.split('/');
+  const pageClass =
+    activeNetworkId === defaultNetworkId ? pathArray?.[1] : pathArray?.[2];
+  const pageName = getCustomPageName({ pathname, basePage: pageClass });
+
+  return (
+    <>
+      {!isHome && (
+        <div className='main-search-container mt-3'>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-12 col-lg-5 col-xl-6'>
+                <Search className='input-group-black' />
+              </div>
+              <div className='col-12 col-lg-7 col-xl-6'>
+                <HeroPills />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {showGlobalStats && (
+        <div className='container mb-3'>
+          <div className='card card-lg card-black'>
+            <div className='card-header'>
+              <h2 className='title mb-0 text-capitalize'>
+                {pageName !== 'analytics'
+                  ? pageName
+                  : `MultiversX Blockchain ${pageName}`}
+                {pageName === 'analytics' && (
+                  <span className='text-neutral-500'> (Beta)</span>
+                )}
+              </h2>
+            </div>
+
+            {showCustomStats ? (
+              <div className='card-body d-flex flex-row flex-wrap gap-3 custom-stats'>
+                {pageStats?.data.map((item) => (
+                  <StatsCard
+                    key={item.id}
+                    title={item.title}
+                    subTitle={item.subTitle}
+                    icon={item.icon}
+                    value={item.value ? item.value.toString() : ''}
+                    className='card-solitary'
+                  />
+                ))}
+              </div>
+            ) : (
+              <div className='card-body d-flex flex-row flex-wrap gap-3'>
+                <TransactionsStatsCard />
+                <AccountsStatsCard />
+                <BlockHeightStatsCard neutralColors />
+                {isMainnet && <ValidatorsStatusCard isSmall />}
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+
+      {showTransactionsStats && isMainnet && (
+        <div className='container mb-3'>
+          <div className='card card-lg card-black'>
+            <div className='card-header pb-0'>
+              <h2 className='title mb-0 text-capitalize'>{pageName}</h2>
+            </div>
+            <div className='card-body p-0'>
+              <ChartContractsTransactions />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
---
+++ src/pages/Layout/components/Hero/hooks/index.ts
@@ -0,0 +1,94 @@
+import React from 'react';
+
+import { useActiveRoute, useIsMainnet } from 'hooks';
+import {
+  wrappedRoutes,
+  validatorsRoutes,
+  searchRoutes,
+  transactionsRoutes,
+  blocksRoutes,
+  accountsRoutes,
+  tokensRoutes,
+  collectionRoutes
+} from 'routes';
+
+export const useShowGlobalStats = () => {
+  const activeRoute = useActiveRoute();
+  const isMainnet = useIsMainnet();
+
+  const routeExists = wrappedRoutes.some(({ path }) => activeRoute(path));
+
+  switch (true) {
+    case !routeExists:
+    case activeRoute('/'):
+    case activeRoute(searchRoutes.index):
+    case activeRoute(searchRoutes.query):
+    case activeRoute(transactionsRoutes.transactions) && isMainnet:
+    case activeRoute(transactionsRoutes.transactionDetails) && isMainnet:
+    case activeRoute(transactionsRoutes.transactionDetailsLogs) && isMainnet:
+    case activeRoute(validatorsRoutes.identities) && isMainnet:
+    case activeRoute(validatorsRoutes.identityDetails) && isMainnet:
+    case activeRoute(validatorsRoutes.providers) && isMainnet:
+    case activeRoute(validatorsRoutes.providerDetails) && isMainnet:
+    case activeRoute(validatorsRoutes.providerTransactions) && isMainnet:
+    case activeRoute(validatorsRoutes.nodes) && isMainnet:
+    case activeRoute(validatorsRoutes.nodeDetails) && isMainnet:
+    case activeRoute(validatorsRoutes.statistics) && isMainnet:
+    case activeRoute(validatorsRoutes.queue) && isMainnet:
+      return false;
+  }
+
+  return true;
+};
+
+export const useShowCustomStats = () => {
+  const activeRoute = useActiveRoute();
+
+  switch (true) {
+    case activeRoute(blocksRoutes.blocks):
+    case activeRoute(blocksRoutes.blocksDetails):
+    case activeRoute(blocksRoutes.miniBlockDetails):
+    case activeRoute(accountsRoutes.accounts):
+    case activeRoute(accountsRoutes.accountDetails):
+    case activeRoute(accountsRoutes.oldAccountDetails):
+    case activeRoute(accountsRoutes.accountTokens):
+    case activeRoute(accountsRoutes.accountNfts):
+    case activeRoute(accountsRoutes.accountContracts):
+    case activeRoute(accountsRoutes.accountStaking):
+    case activeRoute(accountsRoutes.accountAnalytics):
+    case activeRoute(accountsRoutes.accountCode):
+    case activeRoute(accountsRoutes.accountCodeEndpoints):
+    case activeRoute(accountsRoutes.accountCodeTypes):
+    case activeRoute(accountsRoutes.accountCodeConstructor):
+    case activeRoute(accountsRoutes.accountCodeEvents):
+    case activeRoute(tokensRoutes.tokens):
+    case activeRoute(tokensRoutes.tokenDetails):
+    case activeRoute(tokensRoutes.tokenDetailsAccounts):
+    case activeRoute(tokensRoutes.tokenDetailsLockedAccounts):
+    case activeRoute(tokensRoutes.tokenDetailsRoles):
+    case activeRoute(collectionRoutes.collections):
+    case activeRoute(collectionRoutes.collectionDetails):
+    case activeRoute(collectionRoutes.collectionDetailsRoles):
+    case activeRoute(collectionRoutes.collectionsNft):
+    case activeRoute(collectionRoutes.collectionsSft):
+    case activeRoute(collectionRoutes.collectionDetailsRoles):
+      return true;
+
+    default:
+      return false;
+  }
+};
+
+export const useShowTransactionStats = () => {
+  const activeRoute = useActiveRoute();
+
+  switch (true) {
+    case activeRoute(transactionsRoutes.transactions):
+    case activeRoute(transactionsRoutes.transactionDetails):
+    case activeRoute(transactionsRoutes.transactionDetailsLogs):
+      return true;
+
+    default:
+      return false;
+  }
+};
