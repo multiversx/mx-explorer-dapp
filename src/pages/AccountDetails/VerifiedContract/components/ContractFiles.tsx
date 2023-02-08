684428119e840d973d76c6f14fc1a751d9376448
Radu Mojic <radu.mojic@elrond.com>
2023-02-08 16:41:10 +0200

    [+] collapse the rest of the contract files

--- src/pages/AccountDetails/VerifiedContract/components/ContractFiles.tsx
+++ src/pages/AccountDetails/VerifiedContract/components/ContractFiles.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 
-import { faFileAlt, faLink } from '@fortawesome/pro-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useLocation } from 'react-router-dom';
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 
-import rust from 'react-syntax-highlighter/dist/esm/languages/hljs/rust';
-import androidstudio from 'react-syntax-highlighter/dist/esm/styles/hljs/androidstudio';
-
-import { CopyButton } from 'components';
 import { ContractEntryType } from 'types';
-
-SyntaxHighlighter.registerLanguage('rust', rust);
+import { ContractFile } from './ContractFile';
 
 export const ContractFiles = ({
   entries
@@ -19,11 +11,9 @@ export const ContractFiles = ({
   entries: ContractEntryType[];
 }) => {
   const ref = React.useRef<HTMLDivElement>(null);
-  const { hash, pathname } = useLocation();
+  const { hash } = useLocation();
 
   const formattedHash = hash.replace('#', '');
-
-  const fullPath = `${window.location.protocol}//${window.location.hostname}${pathname}`;
 
   React.useEffect(() => {
     if (ref.current && ref.current !== null) {
@@ -41,44 +31,21 @@ export const ContractFiles = ({
 
   return (
     <div className='contract-files'>
-      {filteredEntries.map(({ path, content }, index) => {
-        const base64Buffer = Buffer.from(content, 'base64');
-        const codeString = base64Buffer.toString();
-
-        const selectedFile = formattedHash === path;
-
+      {filteredEntries.map((entry, index) => {
+        const selectedFile = formattedHash === entry.path;
         return (
           <div
-            key={path}
+            key={entry.path}
             className='d-flex flex-column mb-3'
-            id={path}
+            id={entry.path}
             {...(selectedFile ? { ref: ref } : {})}
           >
-            <div className='d-flex flex-row flex-wrap gap-2 align-items-center mb-3 card card-sm bg-table-header px-3 py-2'>
-              <FontAwesomeIcon icon={faFileAlt} className='me-1 text-primary' />
-              <span className='text-neutral-400'>
-                {index + 1}/{filteredEntries.length}{' '}
-              </span>
-              <code>{path}</code>
-            </div>
-            <div className='code-block'>
-              <div className='button-holder'>
-                <CopyButton text={codeString} className='copy-button' />
-                <CopyButton
-                  icon={faLink}
-                  text={`${fullPath}#${path}`}
-                  className='copy-link-button'
-                />
-              </div>
-              <SyntaxHighlighter
-                language='rust'
-                style={androidstudio}
-                showLineNumbers
-                wrapLongLines
-              >
-                {codeString}
-              </SyntaxHighlighter>
-            </div>
+            <ContractFile
+              entry={entry}
+              index={index}
+              totalEntries={filteredEntries.length}
+              isOpen={index === 0}
+            />
           </div>
         );
       })}
