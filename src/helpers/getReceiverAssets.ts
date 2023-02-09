fd27b390942cf5c94e43a1b60f5338945e074add
Radu Mojic <radu.mojic@elrond.com>
2023-02-09 15:17:45 +0200

    [+] match receiver assets only if present

--- src/helpers/getReceiverAssets.ts
+++ src/helpers/getReceiverAssets.ts
@@ -3,9 +3,10 @@ import { UITransactionType } from 'types';
 export const getReceiverAssets = (transaction: UITransactionType) => {
   if (
     transaction.sender === transaction.receiver &&
+    transaction?.action?.arguments?.receiverAssets &&
     transaction?.action?.arguments?.receiver !== transaction.receiver
   ) {
-    return transaction?.action?.arguments?.receiverAssets;
+    return transaction.action.arguments.receiverAssets;
   }
 
   return transaction.receiverAssets;
