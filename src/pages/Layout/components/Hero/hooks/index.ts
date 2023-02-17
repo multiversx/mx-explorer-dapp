0e2c4d0f29ccde207f33c0b2d9cb425eb6932051
Radu Mojic <radu.mojic@elrond.com>
2023-02-17 17:18:31 +0200

    [+] style fixes

--- src/pages/Layout/components/Hero/hooks/index.ts
+++ src/pages/Layout/components/Hero/hooks/index.ts
@@ -41,6 +41,7 @@ export const useShowGlobalStats = () => {
     case activeRoute(collectionRoutes.collectionDetails):
     case activeRoute(collectionRoutes.collectionDetailsRoles):
     case activeRoute(nftRoutes.nftDetails):
+    case activeRoute(nftRoutes.nftDetailsAccounts):
     case activeRoute(tokensRoutes.tokenDetails):
     case activeRoute(tokensRoutes.tokenDetailsAccounts):
     case activeRoute(tokensRoutes.tokenDetailsLockedAccounts):
