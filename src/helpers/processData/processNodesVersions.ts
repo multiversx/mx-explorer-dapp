017fa73c70a2c7bf530acd79e9a36b3078e154ba
Radu Mojic <radu.mojic@elrond.com>
2023-02-21 12:11:32 +0200

    [+] prepare for increased precision on nodes versions

--- src/helpers/processData/processNodesVersions.ts
+++ src/helpers/processData/processNodesVersions.ts
@@ -3,6 +3,11 @@ import {
   NodesVersionsType
 } from 'types/nodesVersions.types';
 
+const countDecimals = (value: number) => {
+  if (Math.floor(value) === value) return 0;
+  return value.toString().split('.')[1].length || 0;
+};
+
 const prepareNodesVersions = (data: NodesVersionsApiType) => {
   const versions: NodesVersionsType[] = [];
 
@@ -12,7 +17,9 @@ const prepareNodesVersions = (data: NodesVersionsApiType) => {
     if (percent > 0) {
       versions.push({
         name: version,
-        percent: Math.floor(percent * 100)
+        percent: Math.floor(
+          countDecimals(percent) > 2 ? percent * 10000 : percent * 100
+        )
       });
     }
   });
