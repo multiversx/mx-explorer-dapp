5885cc34f9d0063503287d7cd8dc54ee5f140028
Radu Mojic <radu.mojic@elrond.com>
2023-05-09 19:52:23 +0300

    [+] use react-router-dom native scroll to top logic on route changes

--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -1,25 +1,12 @@
 import React, { useEffect, memo } from 'react';
 
-export const ScrollToTop = ({ children }: { children: React.ReactNode }) => {
-  useEffect(() => {
-    setTimeout(() => {
-      window.scrollTo(0, 0);
-    });
-  }, []);
-
-  return <>{children}</>;
-};
-
 export const withPageTitle =
   (title: string, Component: React.ComponentType) => () => {
-    const Memoized = memo(() => (
-      <ScrollToTop>
-        <Component />
-      </ScrollToTop>
-    ));
+    const Memoized = memo(() => <Component />);
 
     useEffect(() => {
       document.title = title;
     }, []);
+
     return <Memoized />;
   };
