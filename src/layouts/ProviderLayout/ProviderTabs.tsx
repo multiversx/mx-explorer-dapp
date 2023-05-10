dcc590c97012ef4b5a73a354e6b7f97a1876aca8
Radu Mojic <radu.mojic@elrond.com>
2023-05-10 17:16:43 +0300

    [+] use the extisting tabs design

--- src/layouts/ProviderLayout/ProviderTabs.tsx
+++ src/layouts/ProviderLayout/ProviderTabs.tsx
@@ -1,33 +1,25 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { NetworkLink } from 'components';
+import { Tabs } from 'components/Tabs';
 import { urlBuilder } from 'helpers';
-import { useActiveRoute } from 'hooks';
-import { validatorsRoutes } from 'routes';
 
 export const ProviderTabs = () => {
   const { hash: address } = useParams() as any;
-  const activeRoute = useActiveRoute();
+
+  const tabs = [
+    {
+      tabLabel: 'Nodes',
+      tabTo: urlBuilder.providerDetails(address)
+    },
+    {
+      tabTo: urlBuilder.providerDetailsTransactions(address),
+      tabLabel: 'Transactions'
+    }
+  ];
 
   return (
-    <div className='provider-tabs tab-links d-flex flex-row flex-wrap'>
-      <NetworkLink
-        to={urlBuilder.providerDetails(address)}
-        className={`tab-link me-3 me-lg-4 ${
-          activeRoute(validatorsRoutes.providerDetails) ? 'active' : ''
-        }`}
-      >
-        <h5>Nodes</h5>
-      </NetworkLink>
-
-      <NetworkLink
-        to={urlBuilder.providerDetailsTransactions(address)}
-        className={`tab-link me-3 me-lg-4 ${
-          activeRoute(validatorsRoutes.providerTransactions) ? 'active' : ''
-        }`}
-      >
-        <h5>Transactions</h5>
-      </NetworkLink>
+    <div className='card-header-item nodes-tabs mb-3'>
+      <Tabs tabs={tabs} />
     </div>
   );
 };
