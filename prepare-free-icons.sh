268de7ca893b16016d0d3fb9d036a35bb857f513
Radu Mojic <radu.mojic@multiversx.com>
2023-08-15 16:05:37 +0300

    [+] base install/config scripts
    Signed-off-by: Radu Mojic <radu.mojic@multiversx.com>

---
+++ prepare-free-icons.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp ./src/icons/duotone/fontawesomeFree.ts ./src/icons/duotone/index.ts
+cp ./src/icons/light/fontawesomeFree.ts ./src/icons/light/index.ts
+cp ./src/icons/regular/fontawesomeFree.ts ./src/icons/regular/index.ts
+cp ./src/icons/solid/fontawesomeFree.ts ./src/icons/solid/index.ts
+
+echo 'Using Fontawesome Free Icons'
