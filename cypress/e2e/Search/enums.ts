d3680441da223e64ffc62ebfe51ddd7b67f0d25b
Radu Mojic <radu.mojic@multiversx.com>
2023-11-03 12:57:06 +0200

    updated cypress config to match the new devnet specs

--- cypress/e2e/Search/enums.ts
+++ cypress/e2e/Search/enums.ts
@@ -1,5 +1,5 @@
 export enum SearchDataEnums {
-  address = 'erd1kyle4p6chlvj0u6fyds7wj47jqwq6kh9h083kw8j76254l8u49dqepfz86',
+  address = 'erd1wgxxclk0v8xnyj0fm6hx830q77za4mf0036sjuxan7h99v4tla9q3n2mz5',
   transactionHash = '59ee93ecd116674d01ff38ec8296c0fba5b801ce5207c8cb3e0f012661019120',
   blockHash = '5b4c94ec196b4d08429c7236bfb83e412594aaaafa7b4ca262ef2fc1e9c2cc79',
   validatorKey = '03c4bdea01ea82f8fdc7a6718b54e274bf88d2268a755c2fce185165632cd4cf085023e6f3aebcf3a1c2246330135703953fa7c158cde66ccd6b3798b0d671e6f01bd6e9698de2c88c7f286b6a3e08814c538063f9e86ba8930154cc08bc9b03',
