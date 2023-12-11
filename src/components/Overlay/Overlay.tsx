d02391e773c9d0b59aa7b7a26c0e4bcd2e3264a6
Radu Mojic <radu.mojic@multiversx.com>
2023-12-11 17:11:23 +0200

    [+] added persistent functionality on Overlays

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -1,29 +1,65 @@
+import { useState, useRef } from 'react';
+import classNames from 'classnames';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+
+export interface OverlayUIType {
+  children: React.ReactNode;
+  title: React.ReactNode | string;
+  className?: string;
+  tooltipClassName?: string;
+  persistent?: boolean;
+}
 
 export const Overlay = ({
   children,
   title,
   className,
-  tooltipClassName
-}: {
-  children: React.ReactNode;
-  title: React.ReactNode | string;
-  className?: string;
-  tooltipClassName?: string;
-}) => (
-  <OverlayTrigger
-    placement='top'
-    delay={{ show: 0, hide: 400 }}
-    overlay={(props: any) => (
-      <Tooltip
-        {...(tooltipClassName ? { className: tooltipClassName } : {})}
-        {...props}
-        show={props.show.toString()}
+  tooltipClassName,
+  persistent = false
+}: OverlayUIType) => {
+  const [show, setShow] = useState(false);
+  const handleOnMouseEnter = () => {
+    setShow(true);
+  };
+  const handleOnMouseLeave = () => {
+    setShow(false);
+  };
+  const ref = useRef(null);
+
+  return (
+    <OverlayTrigger
+      placement='top'
+      delay={{ show: 0, hide: 400 }}
+      overlay={(props: any) => (
+        <Tooltip
+          {...(tooltipClassName ? { className: tooltipClassName } : {})}
+          {...props}
+          {...(persistent
+            ? {
+                onMouseEnter: handleOnMouseEnter,
+                onMouseLeave: handleOnMouseLeave
+              }
+            : { show: props.show.toString() })}
+        >
+          {title}
+        </Tooltip>
+      )}
+      {...(persistent ? { show } : {})}
+    >
+      <div
+        ref={ref}
+        className={classNames(className, {
+          'text-truncate': !Boolean(className)
+        })}
+        {...(persistent
+          ? {
+              onMouseEnter: handleOnMouseEnter,
+              onMouseLeave: handleOnMouseLeave
+            }
+          : {})}
       >
-        {title}
-      </Tooltip>
-    )}
-  >
-    <div className={className ?? 'text-truncate'}>{children}</div>
-  </OverlayTrigger>
-);
+        {children}
+      </div>
+    </OverlayTrigger>
+  );
+};
