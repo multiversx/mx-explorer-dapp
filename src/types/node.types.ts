152c4342be6eb31a14dc2730f56dd167e0a22cc3
Radu Mojic <radu.mojic@multiversx.com>
2024-02-11 19:22:28 +0200

    - moved node types from general

--- src/types/node.types.ts
+++ src/types/node.types.ts
@@ -1,3 +1,53 @@
+export interface NodeType {
+  bls: string;
+  name: string;
+  type: 'observer' | 'validator';
+  status?:
+    | 'waiting'
+    | 'eligible'
+    | 'new'
+    | 'jailed'
+    | 'leaving'
+    | 'inactive'
+    | 'queued';
+  online: boolean;
+  rating: number;
+  tempRating: number;
+  ratingModifier: number;
+  shard: number;
+  nonce: number;
+  instances: number;
+  version: string;
+  owner: string;
+  stake: string;
+  topUp: string;
+  uptime: number;
+  uptimeSec: number;
+  downtime: number;
+  downtimeSec: number;
+  locked: string;
+  identity?: string;
+  provider?: string;
+  issues?: string[];
+  syncProgress?: number;
+  leaderSuccess?: number;
+  leaderFailure?: number;
+  validatorSuccess?: number;
+  validatorFailure?: number;
+  validatorIgnoredSignatures?: number;
+  position?: number;
+  fullHistory?: boolean;
+
+  auctionPosition?: number;
+  auctionTopUp?: string;
+  auctionQualified?: boolean;
+  isInDangerZone?: boolean;
+
+  // TODO check if used
+  receivedShardID?: number;
+  computedShardID?: number;
+}
+
 export enum NodeQualificationStatusEnum {
   qualified = 'Qualified',
   notQualified = 'Not Qualified',
