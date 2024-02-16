9e0f4f9c98c6ef1db70d09659fb458cbffb76071
Radu Mojic <radu.mojic@multiversx.com>
2024-02-16 15:14:29 +0200

    fetch shards only once

--- src/hooks/fetch/useFetchShards.ts
+++ src/hooks/fetch/useFetchShards.ts
@@ -1,24 +1,46 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { useAdapter } from 'hooks';
-import { interfaceSelector } from 'redux/selectors';
 import { setShards } from 'redux/slices/interface';
+
+let currentRequest: any = null;
 
 export const useFetchShards = () => {
   const dispatch = useDispatch();
   const { getShards } = useAdapter();
-  const { shards } = useSelector(interfaceSelector);
 
-  const fetchShards = () => {
-    if (shards.length === 0) {
-      getShards().then(({ data, success }) => {
-        if (data && success) {
-          dispatch(setShards(data));
-        }
-      });
+  const getShardsOnce = () => {
+    if (currentRequest) {
+      return currentRequest;
     }
+
+    const requestPromise = new Promise(async (resolve, reject) => {
+      try {
+        const response = await getShards();
+        resolve(response);
+      } catch (error) {
+        reject(error);
+      } finally {
+        currentRequest = null;
+      }
+    });
+
+    currentRequest = requestPromise;
+    return requestPromise;
   };
 
-  useEffect(fetchShards, []);
+  const fetchShards = async () => {
+    const { data, success } = await getShardsOnce();
+
+    if (data && success) {
+      dispatch(setShards(data));
+    }
+
+    return { data, success };
+  };
+
+  useEffect(() => {
+    fetchShards();
+  }, []);
 };
