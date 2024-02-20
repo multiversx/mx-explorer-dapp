139567ebc88b5d0d2cca984e6c98db162420c344
Radu Mojic <radu.mojic@multiversx.com>
2024-02-20 13:14:12 +0200

    temporarily remove expand row functionality

--- src/components/NodesTable/components/NodesTableBody/Rows/AuctionListRow.tsx
+++ src/components/NodesTable/components/NodesTableBody/Rows/AuctionListRow.tsx
@@ -2,7 +2,6 @@ import { useGetSort } from 'hooks';
 import { NodeType, SortOrderEnum, WithClassnameType } from 'types';
 
 import { AuctionListBaseRow } from './AuctionListBaseRow';
-import { AuctionListExpandRow } from './AuctionListExpandRow';
 import { AuctionListTresholdRow } from './AuctionListTresholdRow';
 
 export interface AuctionListRowUIType extends WithClassnameType {
@@ -13,53 +12,17 @@ export interface AuctionListRowUIType extends WithClassnameType {
 
 export const AuctionListRow = ({
   nodeData,
-  showTresholdRow,
-  expandRowDetails
+  showTresholdRow
 }: AuctionListRowUIType) => {
   const { sort, order } = useGetSort();
   const isSortDesc = sort === 'auctionPosition' && order === SortOrderEnum.desc;
-
-  const hasExpand =
-    expandRowDetails?.qualifiedExpandPosition ||
-    expandRowDetails?.dangerZoneExpandPosition ||
-    expandRowDetails?.notQualifiedExpandPosition;
 
   return (
     <>
       {isSortDesc && showTresholdRow && (
         <AuctionListTresholdRow key={nodeData.bls} isSortDesc />
       )}
-      {expandRowDetails?.qualifiedExpandPosition && (
-        <AuctionListExpandRow
-          nodeData={nodeData}
-          expandPosition={expandRowDetails.qualifiedExpandPosition}
-          closePosition={expandRowDetails.qualifiedExpandClosePosition}
-          remainingQualifiedValidators={
-            expandRowDetails?.remainingQualifiedValidators
-          }
-        />
-      )}
-      {expandRowDetails?.dangerZoneExpandPosition && (
-        <AuctionListExpandRow
-          nodeData={nodeData}
-          expandPosition={expandRowDetails.dangerZoneExpandPosition}
-          closePosition={expandRowDetails.dangerZoneExpandClosePosition}
-          remainingDangerZoneValidators={
-            expandRowDetails?.remainingDangerZoneValidators
-          }
-        />
-      )}
-      {expandRowDetails?.notQualifiedExpandPosition && (
-        <AuctionListExpandRow
-          nodeData={nodeData}
-          expandPosition={expandRowDetails.notQualifiedExpandPosition}
-          closePosition={expandRowDetails.notQualifiedExpandClosePosition}
-          remainingNotQualifiedValidators={
-            expandRowDetails?.remainingNotQualifiedValidators
-          }
-        />
-      )}
-      {!hasExpand && <AuctionListBaseRow nodeData={nodeData} />}
+      <AuctionListBaseRow nodeData={nodeData} />
       {!isSortDesc && showTresholdRow && (
         <AuctionListTresholdRow key={nodeData.bls} />
       )}
