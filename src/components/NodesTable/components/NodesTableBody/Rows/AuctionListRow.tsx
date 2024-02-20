f4647e0561f45f20dbb03a91665fe770d32464a6
Radu Mojic <radu.mojic@multiversx.com>
2024-02-20 17:00:50 +0200

    temporarily remove expand row functionality

--- src/components/NodesTable/components/NodesTableBody/Rows/AuctionListRow.tsx
+++ src/components/NodesTable/components/NodesTableBody/Rows/AuctionListRow.tsx
@@ -3,7 +3,6 @@ import { useGetSort } from 'hooks';
 import { NodeType, SortOrderEnum, WithClassnameType } from 'types';
 
 import { AuctionListBaseRow } from './AuctionListBaseRow';
-import { AuctionListExpandRow } from './AuctionListExpandRow';
 import { AuctionListTresholdRow } from './AuctionListTresholdRow';
 
 export interface AuctionListRowUIType extends WithClassnameType {
@@ -16,32 +15,17 @@ export interface AuctionListRowUIType extends WithClassnameType {
 export const AuctionListRow = ({
   nodeData,
   showTresholdRow,
-  index,
-  expandRowDetails
+  index
 }: AuctionListRowUIType) => {
   const { sort, order } = useGetSort();
   const isSortDesc = sort === 'auctionPosition' && order === SortOrderEnum.desc;
-
-  const hasExpand = Boolean(
-    expandRowDetails?.qualifiedExpandPosition ||
-      expandRowDetails?.dangerZoneExpandPosition ||
-      expandRowDetails?.notQualifiedExpandPosition
-  );
 
   return (
     <>
       {isSortDesc && showTresholdRow && (
         <AuctionListTresholdRow key={nodeData.bls} isSortDesc />
       )}
-      {hasExpand && index && expandRowDetails ? (
-        <AuctionListExpandRow
-          nodeData={nodeData}
-          expandRowDetails={expandRowDetails}
-          index={index}
-        />
-      ) : (
-        <AuctionListBaseRow nodeData={nodeData} index={index} />
-      )}
+      <AuctionListBaseRow nodeData={nodeData} index={index} />
       {!isSortDesc && showTresholdRow && (
         <AuctionListTresholdRow key={nodeData.bls} />
       )}
