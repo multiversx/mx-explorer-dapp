3c4b2c259edd96718a4a6519a494b92cdb6fa659
Radu Mojic <radu.mojic@multiversx.com>
2024-02-20 20:13:41 +0200

    use the ExpandRow funtionality

--- src/components/NodesTable/components/NodesTableBody/Rows/AuctionListRow.tsx
+++ src/components/NodesTable/components/NodesTableBody/Rows/AuctionListRow.tsx
@@ -3,6 +3,7 @@ import { useGetSort } from 'hooks';
 import { NodeType, SortOrderEnum, WithClassnameType } from 'types';
 
 import { AuctionListBaseRow } from './AuctionListBaseRow';
+import { AuctionListExpandRow } from './AuctionListExpandRow';
 import { AuctionListTresholdRow } from './AuctionListTresholdRow';
 
 export interface AuctionListRowUIType extends WithClassnameType {
@@ -10,22 +11,44 @@ export interface AuctionListRowUIType extends WithClassnameType {
   showTresholdRow?: boolean;
   expandRowDetails?: ExpandRowDetailsType;
   index?: number;
+  showPosition?: boolean;
 }
 
 export const AuctionListRow = ({
   nodeData,
   showTresholdRow,
-  index
+  index,
+  showPosition,
+  expandRowDetails
 }: AuctionListRowUIType) => {
   const { sort, order } = useGetSort();
   const isSortDesc = sort === 'auctionPosition' && order === SortOrderEnum.desc;
+
+  const hasExpand = Boolean(
+    expandRowDetails?.qualifiedExpandPosition ||
+      expandRowDetails?.dangerZoneExpandPosition ||
+      expandRowDetails?.notQualifiedExpandPosition
+  );
 
   return (
     <>
       {isSortDesc && showTresholdRow && (
         <AuctionListTresholdRow key={nodeData.bls} isSortDesc />
       )}
-      <AuctionListBaseRow nodeData={nodeData} index={index} />
+      {hasExpand && index && expandRowDetails ? (
+        <AuctionListExpandRow
+          nodeData={nodeData}
+          expandRowDetails={expandRowDetails}
+          showPosition={showPosition}
+          index={index}
+        />
+      ) : (
+        <AuctionListBaseRow
+          nodeData={nodeData}
+          index={index}
+          showPosition={showPosition}
+        />
+      )}
       {!isSortDesc && showTresholdRow && (
         <AuctionListTresholdRow key={nodeData.bls} />
       )}
