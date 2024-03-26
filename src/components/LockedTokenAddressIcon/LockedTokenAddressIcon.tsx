667fecb3b1bb2c5a3015d1450fc5a3fb4c077e59
Radu Mojic <radu.mojic@multiversx.com>
2024-03-26 19:11:13 +0200

    - use the existing Overlay component instead of building one with react-bootstrap

--- src/components/LockedTokenAddressIcon/LockedTokenAddressIcon.tsx
+++ src/components/LockedTokenAddressIcon/LockedTokenAddressIcon.tsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 
+import { Overlay } from 'components';
 import { addressIsBech32 } from 'helpers';
 import { faLock } from 'icons/regular';
 import { tokenSelector } from 'redux/selectors';
@@ -27,21 +27,13 @@ export const LockedTokenAddressIcon = ({ address }: { address: string }) => {
     const lockedAccountName = lockedAccounts?.[validLockedAccounts[0]];
 
     return lockedAccountName ? (
-      <OverlayTrigger
-        placement='top'
-        delay={{ show: 0, hide: 400 }}
-        overlay={(props: any) => (
-          <Tooltip {...props} show={props.show.toString()}>
-            {lockedAccountName}
-          </Tooltip>
-        )}
-      >
+      <Overlay title={lockedAccountName}>
         <FontAwesomeIcon
           icon={faLock}
           size='xs'
           className='me-1 text-primary'
         />
-      </OverlayTrigger>
+      </Overlay>
     ) : null;
   }
 
