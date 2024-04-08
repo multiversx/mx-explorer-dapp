69a25c0a4ce1e2e4e1311b98eaa5851711e92388
Radu Mojic <radu.mojic@multiversx.com>
2024-04-08 20:25:53 +0300

    updated LockedTokenAddressIcon display params

--- src/components/LockedTokenAddressIcon/LockedTokenAddressIcon.tsx
+++ src/components/LockedTokenAddressIcon/LockedTokenAddressIcon.tsx
@@ -1,12 +1,21 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
 import { useSelector } from 'react-redux';
 
 import { Overlay } from 'components';
 import { addressIsBech32 } from 'helpers';
 import { faLock } from 'icons/regular';
 import { tokenSelector } from 'redux/selectors';
+import { WithClassnameType } from 'types';
 
-export const LockedTokenAddressIcon = ({ address }: { address: string }) => {
+export interface LockedTokenAddressIconUIType extends WithClassnameType {
+  address: string;
+}
+
+export const LockedTokenAddressIcon = ({
+  address,
+  className
+}: LockedTokenAddressIconUIType) => {
   const { token } = useSelector(tokenSelector);
   const { assets } = token;
 
@@ -31,7 +40,7 @@ export const LockedTokenAddressIcon = ({ address }: { address: string }) => {
         <FontAwesomeIcon
           icon={faLock}
           size='xs'
-          className='me-1 text-primary'
+          className={classNames('text-primary ms-1', className)}
         />
       </Overlay>
     ) : null;
