9255161aa6f4a1d2687ad2ddf6660ca337651e72
Radu Mojic <radu.mojic@multiversx.com>
2024-04-10 18:16:11 +0300

    updated postcss' purgecsss config/safelist

--- postcss.config.js
+++ postcss.config.js
@@ -9,20 +9,29 @@ module.exports = {
         './src/**/*.tsx',
         './node_modules/react-bootstrap/**/*.js',
         './node_modules/react-datepicker/**/*.js',
-        './node_modules/react-select/**/*.js'
+        './node_modules/react-select/**/*.js',
+        './node_modules/react-simple-maps/**/*.js',
+        './node_modules/recharts/**/*.js',
+        './node_modules/@multiversx/sdk-dapp/**/*.js',
+        './node_modules/@multiversx/sdk-dapp-sc-explorer/**/*.js'
       ],
-      variables: true,
       safelist: {
-        standard: ['html', 'body', 'tooltip', 'popover', 'modal'],
         deep: [
           /^dropdown/,
           /^tooltip/,
           /^popover/,
           /^modal/,
+          /^nav/,
           /^styled-select/,
-          /^tabs/
-        ]
+          /^tabs/,
+          /^mx-sdk-sc/,
+          /^styled-select/
+        ],
+        greedy: [/^bs-/, /^mx-sdk-sc-/]
       }
     })
   ]
 };
+
+// TODO - remove / update / safelist dynamic classes
+// TODO - styled components
