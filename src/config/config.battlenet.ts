4b7d0cca9560f2e955a6b0c81f8eabf828dd7d6e
Radu Mojic <radu.mojic@multiversx.com>
2024-08-07 12:05:52 +0300

    added battlenet setup

---
+++ src/config/config.battlenet.ts
@@ -0,0 +1,35 @@
+import { NetworkType } from 'types/network.types';
+import { allApps, schema } from './sharedConfig';
+export * from './sharedConfig';
+
+export const networks: NetworkType[] = [
+  {
+    default: true,
+    id: 'battlenet',
+    name: 'Battlenet',
+    chainId: 'B',
+    adapter: 'api',
+    theme: 'testnet',
+    egldLabel: 'EGLD',
+    walletAddress: 'https://battlenet-wallet.multiversx.com',
+    explorerAddress: 'https://battlenet-explorer.multiversx.com',
+    apiAddress: 'https://express-api-shadowfork-two.elrond.ro'
+  }
+];
+
+export const multiversxApps = allApps([
+  {
+    id: 'wallet',
+    url: 'https://battlenet-wallet.multiversx.com'
+  },
+  {
+    id: 'explorer',
+    url: 'http://battlenet-explorer.multiversx.com'
+  }
+]);
+
+networks.forEach((network) => {
+  schema.validate(network, { strict: true }).catch(({ errors }) => {
+    console.error(`Config invalid format for ${network.id}`, errors);
+  });
+});
