412136ccf200ef2dea9989b0e7d258644149c82d
Radu Mojic <radu.mojic@multiversx.com>
2024-10-08 13:54:56 +0300

    handle the display for the native token identifier

--- src/components/TransactionActionBlock/TransactionActionToken.tsx
+++ src/components/TransactionActionBlock/TransactionActionToken.tsx
@@ -1,3 +1,4 @@
+import { NATIVE_TOKEN_IDENTIFIER } from 'appConstants';
 import { FormatAmount, FormatUSD, NetworkLink } from 'components';
 import { DECIMALS } from 'config';
 import { urlBuilder } from 'helpers';
@@ -14,45 +15,56 @@ export const TransactionActionToken = ({
 }) => {
   const decimals = token.decimals !== undefined ? token.decimals : DECIMALS;
 
+  if (!token?.token) {
+    return null;
+  }
+
+  if (token.token === NATIVE_TOKEN_IDENTIFIER) {
+    return (
+      <div className='d-flex align-items-center symbol text-truncate text-neutral-100'>
+        <FormatAmount
+          value={token.value}
+          showLastNonZeroDecimal={showLastNonZeroDecimal}
+          showUsdValue={false}
+          className='me-1'
+        />
+      </div>
+    );
+  }
+
   return (
     <div className='token-action-block d-contents'>
-      {token && token.token && (
-        <>
-          {!noValue && token.value && (
-            <FormatAmount
-              value={token.value}
-              showLabel={false}
-              showSymbol={false}
-              decimals={decimals}
-              showLastNonZeroDecimal={showLastNonZeroDecimal}
-              showUsdValue={false}
-              className='me-1'
+      {!noValue && token.value && (
+        <FormatAmount
+          value={token.value}
+          showLabel={false}
+          showSymbol={false}
+          decimals={decimals}
+          showLastNonZeroDecimal={showLastNonZeroDecimal}
+          showUsdValue={false}
+          className='me-1'
+        />
+      )}
+      <NetworkLink
+        to={urlBuilder.tokenDetails(token.token)}
+        className={`d-flex text-truncate ${token.svgUrl ? 'side-link' : ''}`}
+      >
+        <div className='d-flex align-items-center symbol text-truncate'>
+          {token.svgUrl && (
+            <img
+              src={token.svgUrl}
+              className='side-icon me-1'
+              alt=''
+              role='presentation'
             />
           )}
-          <NetworkLink
-            to={urlBuilder.tokenDetails(token.token)}
-            className={`d-flex text-truncate ${
-              token.svgUrl ? 'side-link' : ''
-            }`}
-          >
-            <div className='d-flex align-items-center symbol text-truncate'>
-              {token.svgUrl && (
-                <img
-                  src={token.svgUrl}
-                  className='side-icon me-1'
-                  alt=''
-                  role='presentation'
-                />
-              )}
-              <span className='text-truncate'>{token.ticker}</span>
-            </div>
-          </NetworkLink>
-          {token?.valueUSD && (
-            <div className='me-1 text-truncate text-neutral-400 ms-1 text-lh-24'>
-              (<FormatUSD value={token.valueUSD} usd={1} />)
-            </div>
-          )}
-        </>
+          <span className='text-truncate'>{token.ticker}</span>
+        </div>
+      </NetworkLink>
+      {token.valueUSD && (
+        <div className='me-1 text-truncate text-neutral-400 ms-1 text-lh-24'>
+          (<FormatUSD value={token.valueUSD} usd={1} />)
+        </div>
       )}
     </div>
   );
