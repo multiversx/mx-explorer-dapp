fda17d62a3f8deb3db34d47b87447c75efceebce
Radu Mojic <radu.mojic@multiversx.com>
2025-01-10 17:17:47 +0200

    test out relayed marking

--- src/components/TransactionIcons/TransactionRelayedIcon/TransactionRelayedIcon.tsx
+++ src/components/TransactionIcons/TransactionRelayedIcon/TransactionRelayedIcon.tsx
@@ -1,7 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useSelector } from 'react-redux';
 
-import { Overlay } from 'components';
-import { faHandshake } from 'icons/regular';
+import { AccountLink, Overlay } from 'components';
+import { faHandshake, faHandHoldingHand } from 'icons/regular';
+import { accountSelector } from 'redux/selectors';
 import { UITransactionType } from 'types';
 
 export const TransactionRelayedIcon = ({
@@ -9,15 +11,48 @@ export const TransactionRelayedIcon = ({
 }: {
   transaction: UITransactionType;
 }) => {
-  if (
-    transaction?.isRelayed ||
-    transaction?.relayerSignature ||
-    transaction?.relayer
-  ) {
-    const relayedText = transaction?.relayedVersion
-      ? `Relayed Tx Version: ${transaction.relayedVersion}`
-      : 'Relayed Transaction';
+  const { account } = useSelector(accountSelector);
 
+  const relayedText = transaction?.relayedVersion
+    ? `Relayed Tx Version: ${transaction.relayedVersion}`
+    : 'Relayed Transaction';
+
+  if (transaction?.isRelayed) {
+    const isRelayedByCurrentAccount =
+      account?.address && transaction?.relayer === account.address;
+
+    return (
+      <>
+        <Overlay title={relayedText} className='relayed-icon' persistent>
+          <FontAwesomeIcon icon={faHandshake} className='text-primary me-1' />
+        </Overlay>
+
+        {transaction?.relayer && (
+          <Overlay
+            title={
+              <>
+                Transaction relayed by{' '}
+                {isRelayedByCurrentAccount ? (
+                  'current account'
+                ) : (
+                  <AccountLink address={transaction.relayer} />
+                )}
+              </>
+            }
+            className='relayed-icon'
+            persistent
+          >
+            <FontAwesomeIcon
+              icon={faHandHoldingHand}
+              className='text-primary me-1'
+            />
+          </Overlay>
+        )}
+      </>
+    );
+  }
+
+  if (transaction?.relayer) {
     return (
       <Overlay title={relayedText} className='relayed-icon' persistent>
         <FontAwesomeIcon icon={faHandshake} className='text-primary me-1' />
