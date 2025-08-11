5f2cc1fdf901081f18090985586a53680c00fe14
Radu Mojic <radu.mojic@multiversx.com>
2025-08-11 15:45:15 +0300

    update fontawesome pro packages

--- prepare-pro-icons.sh
+++ prepare-pro-icons.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-packages='@fortawesome/pro-duotone-svg-icons@6.5.1 @fortawesome/pro-light-svg-icons@6.5.1 @fortawesome/pro-regular-svg-icons@6.5.1 @fortawesome/pro-solid-svg-icons@6.5.1'
+packages='@fortawesome/pro-duotone-svg-icons@6.7.2 @fortawesome/pro-light-svg-icons@6.7.2 @fortawesome/pro-regular-svg-icons@6.7.2 @fortawesome/pro-solid-svg-icons@6.7.2'
 if [[ "$(npm list $packages)" =~ "empty" ]]; then
     echo "Installing $packages ..."
     yarn add $packages --registry https://npm.fontawesome.com/ --peer --no-lockfile
