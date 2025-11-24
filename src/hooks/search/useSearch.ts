5810cf4f649e4b5429418418610984bc5ae9ce7f
Radu Mojic <radu.mojic@multiversx.com>
2025-11-24 18:32:06 +0200

    cancel existing calls

--- src/hooks/search/useSearch.ts
+++ src/hooks/search/useSearch.ts
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { formatHerotag } from 'helpers';
@@ -8,6 +9,7 @@ import { SearchResponseType, SortOrderEnum } from 'types';
 import { useSearchSingleResponse } from './useSearchSingleResponse';
 
 export const useSearch = (hash: string) => {
+  const controllerRef = useRef<any | null>(null);
   const dispatch = useDispatch();
   const isMainnet = useIsMainnet();
   const { searchQuery, isDataReady } = useSelector(searchSelector);
@@ -38,61 +40,84 @@ export const useSearch = (hash: string) => {
       return singleResponse;
     }
 
-    const [
-      tokensResponse,
-      collectionsResponse,
-      nftsResponse,
-      nodesResponse,
-      accountsResponse,
-      usernameResponse
-    ] = await Promise.all([
-      getTokens({
-        search: searchHash,
-        includeMetaESDT: true,
-        ...defaultQueryParams
-      }),
-      getCollections({
-        search: searchHash,
-        excludeMetaESDT: true,
-        ...defaultQueryParams
-      }),
-      getNfts({
-        search: searchHash,
-        excludeMetaESDT: true,
-        ...defaultQueryParams
-      }),
-      getNodes({
-        search: searchHash,
-        withIdentityInfo: true,
-        ...defaultQueryParams
-      }),
-      getAccounts({
-        search: searchHash,
-        withAssets: true,
-        withTxCount: true,
-        ...(isMainnet ? accountSorting : {})
-      }),
-      getUsername(formatHerotag(searchHash))
-    ]);
+    if (controllerRef.current) {
+      controllerRef.current.abort();
+    }
 
-    const foundTokens = tokensResponse?.data && tokensResponse.data.length > 0;
-    const foundCollections =
-      collectionsResponse?.data && collectionsResponse.data.length > 0;
-    const foundNfts = nftsResponse?.data && nftsResponse.data.length > 0;
-    const foundNodes = nodesResponse?.data && nodesResponse.data.length > 0;
-    const foundAccounts =
-      accountsResponse?.data && accountsResponse.data.length > 0;
+    controllerRef.current = new AbortController();
 
-    return {
-      ...(foundTokens ? { tokens: tokensResponse?.data } : {}),
-      ...(foundCollections ? { collections: collectionsResponse?.data } : {}),
-      ...(foundNfts ? { nfts: nftsResponse?.data } : {}),
-      ...(foundNodes ? { nodes: nodesResponse?.data } : {}),
-      ...(foundAccounts ? { accounts: accountsResponse?.data } : {}),
-      ...(usernameResponse?.data?.address
-        ? { account: usernameResponse?.data }
-        : {})
-    };
+    try {
+      const signal = controllerRef.current.signal;
+      const [
+        tokensResponse,
+        collectionsResponse,
+        nftsResponse,
+        nodesResponse,
+        accountsResponse,
+        usernameResponse
+      ] = await Promise.all([
+        getTokens({
+          signal,
+          search: searchHash,
+          includeMetaESDT: true,
+          ...defaultQueryParams
+        }),
+        getCollections({
+          signal,
+          search: searchHash,
+          excludeMetaESDT: true,
+          ...defaultQueryParams
+        }),
+        getNfts({
+          signal,
+          search: searchHash,
+          excludeMetaESDT: true,
+          ...defaultQueryParams
+        }),
+        getNodes({
+          signal,
+          search: searchHash,
+          withIdentityInfo: true,
+          ...defaultQueryParams
+        }),
+        getAccounts({
+          signal,
+          search: searchHash,
+          withAssets: true,
+          withTxCount: true,
+          ...(isMainnet ? accountSorting : {})
+        }),
+        getUsername(formatHerotag(searchHash), { signal })
+      ]);
+
+      const foundTokens =
+        tokensResponse?.data && tokensResponse.data.length > 0;
+      const foundCollections =
+        collectionsResponse?.data && collectionsResponse.data.length > 0;
+      const foundNfts = nftsResponse?.data && nftsResponse.data.length > 0;
+      const foundNodes = nodesResponse?.data && nodesResponse.data.length > 0;
+      const foundAccounts =
+        accountsResponse?.data && accountsResponse.data.length > 0;
+
+      return {
+        ...(foundTokens ? { tokens: tokensResponse?.data } : {}),
+        ...(foundCollections ? { collections: collectionsResponse?.data } : {}),
+        ...(foundNfts ? { nfts: nftsResponse?.data } : {}),
+        ...(foundNodes ? { nodes: nodesResponse?.data } : {}),
+        ...(foundAccounts ? { accounts: accountsResponse?.data } : {}),
+        ...(usernameResponse?.data?.address
+          ? { account: usernameResponse?.data }
+          : {})
+      };
+    } catch (err: any) {
+      if (err.name === 'CanceledError') {
+        console.info('Search cancelled');
+        return {};
+      } else {
+        console.error('Encountered an error searching for: ', hash);
+        return {};
+      }
+    }
   };
 
   const search = async () => {
