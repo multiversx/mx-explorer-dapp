31b3cb797c4584a54e65e88fa0a34a723005ee4d
Radu Mojic <radu.mojic@multiversx.com>
2026-01-20 16:56:34 +0200

    remove unused memo

--- src/components/Search/components/SearchSuggestions/SearchSuggestions.tsx
+++ src/components/Search/components/SearchSuggestions/SearchSuggestions.tsx
@@ -1,4 +1,3 @@
-import { memo } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Loader } from 'components';
@@ -6,27 +5,29 @@ import { searchSelector } from 'redux/selectors';
 import { SearchMostUsed } from './SearchMostUsed';
 import { SearchResults } from './SearchResults';
 
-export const SearchSuggestions = memo(
-  ({ searchHash = '' }: { searchHash?: string }) => {
-    const { searchQuery, isDataReady } = useSelector(searchSelector);
-    const trimmedSearchHash = String(searchHash).trim();
+export const SearchSuggestions = ({
+  searchHash = ''
+}: {
+  searchHash?: string;
+}) => {
+  const { searchQuery, isDataReady } = useSelector(searchSelector);
+  const trimmedSearchHash = String(searchHash).trim();
 
-    if (trimmedSearchHash.length < 3) {
-      return (
-        <div className='search-suggestions'>
-          <SearchMostUsed />
-        </div>
-      );
-    }
-
-    if (isDataReady === false && searchQuery) {
-      return <Loader />;
-    }
-
+  if (trimmedSearchHash.length < 3) {
     return (
       <div className='search-suggestions'>
-        {searchQuery ? <SearchResults /> : <SearchMostUsed />}
+        <SearchMostUsed />
       </div>
     );
   }
-);
+
+  if (isDataReady === false && searchQuery) {
+    return <Loader />;
+  }
+
+  return (
+    <div className='search-suggestions'>
+      {searchQuery ? <SearchResults /> : <SearchMostUsed />}
+    </div>
+  );
+};
