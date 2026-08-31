70e96ba3082fa6e2fbc74c90dab0934b204e9d7d
Radu Mojic <radu.mojic@multiversx.com>
2026-08-31 18:23:18 +0300

    lazy load smart contract explorer

--- src/lib/sdkDappSc.ts
+++ src/lib/sdkDappSc.ts
@@ -1,4 +1,3 @@
-export { ScExplorerContainer } from '@multiversx/sdk-dapp-sc-explorer/out/containers/ScExplorerContainer';
 export { VerifiedContractTabsEnum } from '@multiversx/sdk-dapp-sc-explorer/out/types/base.types';
 
 export type { RawAbiType } from '@multiversx/sdk-dapp-sc-explorer/out/types/abi.types';
